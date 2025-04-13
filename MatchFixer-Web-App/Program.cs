a11565668f1e4f0fe9b7d11bed6f0919c795631d
I-M-Marinov <lcfrrr@gmail.com>
2025-04-13 19:37:40 +0300

    Using the new DB Context from the Infrastructure project and ApplicationUser instead of IdentityUser
    -- Added RazorPages in the pipeline
    -- all secrets to user secrets

--- MatchFixer-Web-App/Program.cs
+++ MatchFixer-Web-App/Program.cs
@@ -1,18 +1,30 @@
-using MatchFixer_Web_App.Data;
+
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using MatchFixer.Infrastructure;
+using MatchFixer.Infrastructure.Entities;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
+
+builder.Services.AddDbContext<MatchFixerDbContext>(options =>
     options.UseSqlServer(connectionString));
+
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole<Guid>>(options =>
+	{
+		options.SignIn.RequireConfirmedAccount = true;
+	})
+	.AddEntityFrameworkStores<MatchFixerDbContext>()
+	.AddDefaultTokenProviders();
+
 builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+
 
 var app = builder.Build();
 
