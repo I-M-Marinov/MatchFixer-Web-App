08cc1d2032a076c4abd6f1e7daa0d3b6f4833d38
I-M-Marinov <lcfrrr@gmail.com>
2025-04-15 21:23:43 +0300

    Created DTOs for Image File Upload and Delete Image Result to serve in the Image Service ( upload image and result of deleting an image )

---
+++ MatchFixer.Infrastructure/Models/Image/ImageFileUploadModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http;
+using MatchFixer.Infrastructure.ValidationAttributes;
+
+
+namespace MatchFixer.Infrastructure.Models.Image
+{
+
+	public class ImageFileUploadModel
+	{
+		[Required(ErrorMessage = "Image must be selected !")]
+		[AllowedExtensions(ErrorMessage = "Invalid file extension. Only JPG, JPEG and PNG files are allowed.")]
+		[FileSize(MaxSizeInBytes = 10485760, ErrorMessage = "File size must not exceed 10 MegaBytes.")]
+		public IFormFile FormFile { get; set; } = null!;
+
+	}
+}
---
+++ MatchFixer.Infrastructure/Models/Image/﻿DeleteImageResult﻿.cs
@@ -0,0 +1,8 @@
+﻿namespace MatchFixer.Infrastructure.Models.Image
+{
+	public class DeleteImageResult
+	{
+		public bool IsSuccess { get; set; }
+		public string Message { get; set; } = null!;
+	}
+}
