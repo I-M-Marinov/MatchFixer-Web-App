2686b1c85552a52e31f29da4f8d8a77d86d8a325
I-M-Marinov <lcfrrr@gmail.com>
2025-04-15 21:21:42 +0300

    Created the Validation Attributes:
    -- Allowed Extensions Attribute to validate if the extension of the image file uploaded is the correct
    
    -- File Size Attribute to verify images uploaded are not more than 10MB ( the maximum size allowed on a free subsription in Cloudinary )

---
+++ MatchFixer.Infrastructure/ValidationAttributes/AllowedExtensions.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace MatchFixer.Infrastructure.ValidationAttributes
+{
+	public class AllowedExtensions : ValidationAttribute
+	{
+		private readonly string[] _validExtensions;
+
+		private static readonly string[] ValidImageExtensions = new[] { ".jpg", ".jpeg", ".png" };
+		private static readonly string[] AdminValidImageExtensions = new[] { ".jpg", ".jpeg", ".png", ".gif" };
+
+		public AllowedExtensions()
+		{
+			_validExtensions = ValidImageExtensions;
+		}
+		public AllowedExtensions(bool isAdmin)
+		{
+			_validExtensions = isAdmin ? AdminValidImageExtensions : ValidImageExtensions;
+		}
+
+		protected override ValidationResult IsValid(object? value, ValidationContext validationContext)
+		{
+			var file = value as IFormFile;
+
+			if (file != null)
+			{
+				var extension = Path.GetExtension(file.FileName).ToLower();
+				if (!_validExtensions.Contains(extension))
+				{
+					return new ValidationResult(ErrorMessage ?? "Invalid file extension.");
+				}
+			}
+
+			return ValidationResult.Success!;
+		}
+	}
+}
---
+++ MatchFixer.Infrastructure/ValidationAttributes/FileSize.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace MatchFixer.Infrastructure.ValidationAttributes
+{
+	public class FileSize : ValidationAttribute
+	{
+		public int MaxSizeInBytes { get; set; }
+
+		protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+		{
+			var file = value as IFormFile;
+
+			if (file != null && file.Length > MaxSizeInBytes)
+			{
+				return new ValidationResult(ErrorMessage ?? $"File size should not exceed {MaxSizeInBytes / (1024 * 1024)} MB.");
+			}
+
+			return ValidationResult.Success;
+		}
+	}
+}
