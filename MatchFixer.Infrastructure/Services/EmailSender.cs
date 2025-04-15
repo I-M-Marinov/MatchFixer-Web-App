e7f766e62a6c6b4c08036cc35129a6d9f2c7b4f9
I-M-Marinov <lcfrrr@gmail.com>
2025-04-15 21:25:06 +0300

    Created the EmailSender service, using Gmail SMTP to send out emails ( confirmation emails after a user registers, resend confirmation emails upon request of a user )

---
+++ MatchFixer.Infrastructure/Services/EmailSender.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Configuration;
+using System.Net;
+using System.Net.Mail;
+
+public class EmailSender : IEmailSender
+{
+	private readonly IConfiguration _configuration;
+
+	public EmailSender(IConfiguration configuration)
+	{
+		_configuration = configuration;
+	}
+
+	public Task SendEmailAsync(string email, string subject, string htmlMessage)
+	{
+		var smtpClient = new SmtpClient("smtp.gmail.com")
+		{
+			Port = 587,
+			Credentials = new NetworkCredential(
+				_configuration["GmailSmtp:User"],
+				_configuration["GmailSmtp:Pass"]
+			),
+			EnableSsl = true
+		};
+
+		var mailMessage = new MailMessage
+		{
+			From = new MailAddress(_configuration["GmailSmtp:User"]),
+			Subject = subject,
+			Body = htmlMessage,
+			IsBodyHtml = true
+		};
+
+		mailMessage.To.Add(email);
+
+		return smtpClient.SendMailAsync(mailMessage);
+	}
+}
