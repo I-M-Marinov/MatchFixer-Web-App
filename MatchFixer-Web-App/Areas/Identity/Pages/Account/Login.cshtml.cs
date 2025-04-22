dbe5103cb7e2cd2796bc0e07065a49ad821ea8cc
I-M-Marinov <lcfrrr@gmail.com>
2025-04-22 22:59:02 +0300

    Updated the message shown to the user when an invalid email or password is given.

--- MatchFixer-Web-App/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ MatchFixer-Web-App/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -129,7 +129,7 @@ namespace MatchFixer_Web_App.Areas.Identity.Pages.Account
                 }
                 else
                 {
-                    ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                    ModelState.AddModelError(string.Empty, "Login failed. Please check your email and password.");
                     return Page();
                 }
             }
