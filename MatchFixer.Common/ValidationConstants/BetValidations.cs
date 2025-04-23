79b6ec1074f501571398af1c7042083780c49b10
I-M-Marinov <lcfrrr@gmail.com>
2025-04-23 21:11:41 +0300

    Created the MatchFixer.Common project to hold the Validation Constants, Enums and helper methods

---
+++ MatchFixer.Common/ValidationConstants/BetValidations.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace MatchFixer.Common.ValidationConstants
+{
+	public static class BetValidations
+	{
+		public const string AmountIsRequired = "Amount of the bet is required.";
+		public const string AmountMustBeBetweenOneAndFiftyThousand = "Amount of the bet must be between $1.00 and $50,000.";
+		public const string PickIsRequired = "Pick is required.";
+
+	
+
+	}
+}
---
+++ MatchFixer.Common/ValidationConstants/UserValidations.cs
@@ -0,0 +1,27 @@
+﻿namespace MatchFixer.Common.ValidationConstants
+{
+	public static class UserValidations
+	{
+
+		// First Name of the user
+
+		public const byte FirstNameMinLength = 2;
+		public const byte FirstNameMaxLength = 25;
+
+		// Last Name of the user
+
+		public const byte LastNameMinLength = 3;
+		public const byte LastNameMaxLength = 25;
+
+		// Country of the user 
+
+		public const byte CountryNameMinLength = 3;
+		public const byte CountryNameMaxLength = 25;
+
+		// Profile Picture Validations
+
+		public const int UserImageMaxLength = 1000;
+		public const int UserImagePublicIdMaxLength = 1000;
+
+	}
+}
