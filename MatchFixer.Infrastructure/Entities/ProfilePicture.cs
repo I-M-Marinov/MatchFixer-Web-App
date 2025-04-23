fda50f44370c8aca27b508bc245fc227e4e7b24d
I-M-Marinov <lcfrrr@gmail.com>
2025-04-23 21:16:42 +0300

    Created the Profile Picture entity to hold the user images ( account pictures ) that the user would have a way of uploading from the Profile View

---
+++ MatchFixer.Infrastructure/Entities/ProfilePicture.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+using static MatchFixer.Common.ValidationConstants.UserValidations;
+
+namespace MatchFixer.Infrastructure.Entities
+{
+	public class ProfilePicture
+	{
+		[Key]
+		[Comment("Unique identifier for the User Image")]
+		public Guid Id { get; set; }
+
+		[StringLength(UserImageMaxLength)]
+		[Comment("Url address pointing to the User Image")]
+		public string? ImageUrl { get; set; }
+
+		[StringLength(UserImagePublicIdMaxLength)]
+		[Required]
+		[Comment("Key used in Cloudinary to determine validity of the User Image")]
+		public string PublicId { get; set; } = null!;
+		public virtual ICollection<ApplicationUser> Users { get; set; } = new HashSet<ApplicationUser>();
+
+	}
+
+}
