977fc04bf254a2e1d55fab05ff2134ef1596ae1d
I-M-Marinov <lcfrrr@gmail.com>
2025-04-23 20:57:41 +0300

    Created the Profile Controller

---
+++ MatchFixer-Web-App/Controllers/ProfileController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MatchFixer_Web_App.Controllers
+{
+	public class ProfileController : Controller
+	{
+		public IActionResult Profile()
+		{
+			return View();
+		}
+	}
+}
