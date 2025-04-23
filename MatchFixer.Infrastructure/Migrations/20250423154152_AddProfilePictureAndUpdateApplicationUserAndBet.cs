44792715848daf5a1ce6fb77566d805c2df33df4
I-M-Marinov <lcfrrr@gmail.com>
2025-04-23 21:18:43 +0300

    Added a migration to the DB to update all the changes added and updated to all entities

---
+++ MatchFixer.Infrastructure/Migrations/20250423154152_AddProfilePictureAndUpdateApplicationUserAndBet.cs
@@ -0,0 +1,305 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MatchFixer.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProfilePictureAndUpdateApplicationUserAndBet : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "UserId",
+                table: "Bets",
+                type: "uniqueidentifier",
+                nullable: false,
+                comment: "Id of the user that placed the bet",
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Pick",
+                table: "Bets",
+                type: "int",
+                nullable: false,
+                comment: "The chosen outcome the user will choose for the bet",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "MatchEventId",
+                table: "Bets",
+                type: "uniqueidentifier",
+                nullable: false,
+                comment: "Id of match event user is placing a bet for",
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "BetTime",
+                table: "Bets",
+                type: "datetime2",
+                nullable: false,
+                comment: "The exact time the bet was placed",
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Amount",
+                table: "Bets",
+                type: "decimal(10,2)",
+                precision: 10,
+                scale: 2,
+                nullable: false,
+                comment: "Amount of the Bet",
+                oldClrType: typeof(decimal),
+                oldType: "decimal(10,2)",
+                oldPrecision: 10,
+                oldScale: 2);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "Bets",
+                type: "uniqueidentifier",
+                nullable: false,
+                comment: "Unique identifier for a Bet",
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsSettled",
+                table: "Bets",
+                type: "bit",
+                nullable: false,
+                defaultValue: false,
+                comment: "Signifies if the bet has been settled or not");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "WinAmount",
+                table: "Bets",
+                type: "decimal(10,2)",
+                precision: 10,
+                scale: 2,
+                nullable: true,
+                comment: "Amount that would be won on the bet");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Country",
+                table: "AspNetUsers",
+                type: "nvarchar(25)",
+                maxLength: 25,
+                nullable: false,
+                defaultValue: "",
+                comment: "Country of origin of the application user");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedOn",
+                table: "AspNetUsers",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                comment: "Date user was created");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateOfBirth",
+                table: "AspNetUsers",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                comment: "Date of birth of the application user");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "AspNetUsers",
+                type: "nvarchar(25)",
+                maxLength: 25,
+                nullable: false,
+                defaultValue: "",
+                comment: "First name of the application user");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "AspNetUsers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false,
+                comment: "Indicates whether the user is marked as deleted");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "AspNetUsers",
+                type: "nvarchar(25)",
+                maxLength: 25,
+                nullable: false,
+                defaultValue: "",
+                comment: "Last name of the application user");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedOn",
+                table: "AspNetUsers",
+                type: "datetime2",
+                nullable: true,
+                comment: "Date user was last updated");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "ProfilePictureId",
+                table: "AspNetUsers",
+                type: "uniqueidentifier",
+                nullable: true,
+                comment: "Id of the profile picture of the application user");
+
+            migrationBuilder.AddColumn<string>(
+                name: "TimeZone",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                comment: "Time Zone of the application user");
+
+            migrationBuilder.CreateTable(
+                name: "ProfilePictures",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false, comment: "Unique identifier for the User Image"),
+                    ImageUrl = table.Column<string>(type: "nvarchar(1000)", maxLength: 1000, nullable: true, comment: "Url address pointing to the User Image"),
+                    PublicId = table.Column<string>(type: "nvarchar(1000)", maxLength: 1000, nullable: false, comment: "Key used in Cloudinary to determine validity of the User Image")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProfilePictures", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_ProfilePictureId",
+                table: "AspNetUsers",
+                column: "ProfilePictureId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_ProfilePictures_ProfilePictureId",
+                table: "AspNetUsers",
+                column: "ProfilePictureId",
+                principalTable: "ProfilePictures",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_ProfilePictures_ProfilePictureId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropTable(
+                name: "ProfilePictures");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_ProfilePictureId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "IsSettled",
+                table: "Bets");
+
+            migrationBuilder.DropColumn(
+                name: "WinAmount",
+                table: "Bets");
+
+            migrationBuilder.DropColumn(
+                name: "Country",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedOn",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "DateOfBirth",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedOn",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "ProfilePictureId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "TimeZone",
+                table: "AspNetUsers");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "UserId",
+                table: "Bets",
+                type: "uniqueidentifier",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldComment: "Id of the user that placed the bet");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Pick",
+                table: "Bets",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldComment: "The chosen outcome the user will choose for the bet");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "MatchEventId",
+                table: "Bets",
+                type: "uniqueidentifier",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldComment: "Id of match event user is placing a bet for");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "BetTime",
+                table: "Bets",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldComment: "The exact time the bet was placed");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Amount",
+                table: "Bets",
+                type: "decimal(10,2)",
+                precision: 10,
+                scale: 2,
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(10,2)",
+                oldPrecision: 10,
+                oldScale: 2,
+                oldComment: "Amount of the Bet");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "Bets",
+                type: "uniqueidentifier",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldComment: "Unique identifier for a Bet");
+        }
+    }
+}
