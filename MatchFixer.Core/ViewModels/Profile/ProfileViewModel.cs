231efce59be787bec0adb38087e29a76f6c52ba3
I-M-Marinov <lcfrrr@gmail.com>
2025-04-24 22:00:15 +0300

    Moved the ProfileViewModel in the ViewModels folder in the newly created MatchFixer.Core projec

---
+++ MatchFixer.Core/ViewModels/Profile/ProfileViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace MatchFixer_Web_App.Models.Profile
+{
+	public class ProfileViewModel
+	{
+		public string Id { get; set; } = null!;
+		public string FirstName { get; set; } = null!;
+		public string LastName { get; set; } = null!;
+		public string FullName => $"{FirstName} {LastName}";
+		public DateTime DateOfBirth { get; set; }
+		public string Country { get; set; } = null!;
+		public string? TimeZone { get; set; }
+		public string Email { get; set; } = null!;
+		public string? ProfileImageUrl { get; set; } 
+		public DateTime CreatedOn { get; set; }
+	}
+}
