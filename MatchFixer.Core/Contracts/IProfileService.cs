03e49bccc55ad96a66c0692b3875f7e4423ee4f5
I-M-Marinov <lcfrrr@gmail.com>
2025-04-24 21:56:29 +0300

    Created the IProfileService Interface

---
+++ MatchFixer.Core/Contracts/IProfileService.cs
@@ -0,0 +1,11 @@
+﻿using MatchFixer_Web_App.Models.Profile;
+
+
+namespace MatchFixer.Core.Contracts
+{
+	public interface IProfileService
+	{
+		Task<ProfileViewModel> GetProfileAsync(string userId);
+		Task<(bool Success, string Message)> UpdateProfileAsync(ProfileViewModel model);
+	}
+}
