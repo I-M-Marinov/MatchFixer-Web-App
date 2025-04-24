525484b8275f1ec4e9ab6093bd1b262a356f2599
I-M-Marinov <lcfrrr@gmail.com>
2025-04-25 00:04:49 +0300

    Added UpdateEmailAsync and ConfirmEmailAsync to the Interface

--- MatchFixer.Core/Contracts/IProfileService.cs
+++ MatchFixer.Core/Contracts/IProfileService.cs
@@ -7,5 +7,7 @@ namespace MatchFixer.Core.Contracts
 	{
 		Task<ProfileViewModel> GetProfileAsync(string userId);
 		Task<(bool Success, string Message)> UpdateProfileAsync(ProfileViewModel model);
+		Task<(bool Success, string ErrorMessage)> UpdateEmailAsync(string userId, string newEmail, string scheme);
+		Task<(bool Success, string Message)> ConfirmEmailAsync(string userId, string code);
 	}
 }
