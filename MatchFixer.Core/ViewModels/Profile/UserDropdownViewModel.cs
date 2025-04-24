528433d20f8cc3f45aa21609b9ac7c786402aea9
I-M-Marinov <lcfrrr@gmail.com>
2025-04-24 22:01:00 +0300

    Move the UserDropDownViewModel to the ViewModels in the newly created MatchFixer.Core project

---
+++ MatchFixer.Core/ViewModels/Profile/UserDropdownViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace MatchFixer_Web_App.Models.Profile
+{
+	public class UserDropdownViewModel
+	{
+		public Guid Id { get; set; }
+		public string FirstName { get; set; } = null!;
+		public string LastName { get; set; } = null!;
+		public string FullName => $"{FirstName} {LastName}";
+		public string? ProfileImageUrl { get; set; }
+	}
+
+}
