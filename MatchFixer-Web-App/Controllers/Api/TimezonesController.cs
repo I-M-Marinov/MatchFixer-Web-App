0343dd7febf33d198cd9d1380a05fd08924a0d98
I-M-Marinov <lcfrrr@gmail.com>
2025-04-26 14:22:32 +0300

    Created the TimezonesController ( API CONTROLLER ) using the Timezone Service to get the timezones for a certain country

---
+++ MatchFixer-Web-App/Controllers/Api/TimezonesController.cs
@@ -0,0 +1,25 @@
+﻿using MatchFixer.Infrastructure.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MatchFixer_Web_App.Controllers.Api
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class TimezonesController : ControllerBase
+	{
+		private readonly ITimezoneService _timezoneService;
+
+		public TimezonesController(ITimezoneService timezoneService)
+		{
+			_timezoneService = timezoneService;
+		}
+
+		[HttpGet("{countryCode}")]
+		public IActionResult Get(string countryCode)
+		{
+			var zones = _timezoneService.GetTimezonesForCountry(countryCode);
+			return Ok(zones);
+		}
+	}
+
+}
