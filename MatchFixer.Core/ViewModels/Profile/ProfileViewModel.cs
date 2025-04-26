3f1a91ff74de4d98eaec89ec6f29d33ec6bb7fc9
I-M-Marinov <lcfrrr@gmail.com>
2025-04-26 14:35:35 +0300

    Updated the namespace of the ProfileViewModel and:
    -- Added validation to the TimeZone and Email fields
    -- Added the List<SelectListItem> CountryOptions

--- MatchFixer.Core/ViewModels/Profile/ProfileViewModel.cs
+++ MatchFixer.Core/ViewModels/Profile/ProfileViewModel.cs
@@ -1,16 +1,27 @@
-﻿namespace MatchFixer_Web_App.Models.Profile
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace MatchFixer.Core.ViewModels.Profile
 {
 	public class ProfileViewModel
 	{
 		public string Id { get; set; } = null!;
+		[Required(ErrorMessage = "First name is required.")]
 		public string FirstName { get; set; } = null!;
+		[Required(ErrorMessage = "Last name is required.")]
 		public string LastName { get; set; } = null!;
 		public string FullName => $"{FirstName} {LastName}";
 		public DateTime DateOfBirth { get; set; }
 		public string Country { get; set; } = null!;
-		public string? TimeZone { get; set; }
+		[Required(ErrorMessage = "Time Zone is required. Please select the time zone.")]
+		public string TimeZone { get; set; } = null!;
+
+		[Required(ErrorMessage = "Email address is required.")]
 		public string Email { get; set; } = null!;
-		public string? ProfileImageUrl { get; set; } 
+		public string? ProfileImageUrl { get; set; }
 		public DateTime CreatedOn { get; set; }
+		public List<SelectListItem> CountryOptions { get; set; } = new();
+
+
 	}
 }
