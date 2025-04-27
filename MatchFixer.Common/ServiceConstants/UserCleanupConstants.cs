996fd237182181dca16e3f1eae9666cef230669f
I-M-Marinov <lcfrrr@gmail.com>
2025-04-27 15:54:37 +0300

    Created UserCleanupConstants static class in the Common project to hold constants and read-only data for the UserCleanupService

---
+++ MatchFixer.Common/ServiceConstants/UserCleanupConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace MatchFixer.Common.ServiceConstants
+{
+	public static class UserCleanupConstants
+	{
+		public static readonly TimeSpan CleanupInterval = TimeSpan.FromHours(24);
+		public static readonly TimeSpan AccountExpiration = TimeSpan.FromHours(1);
+	}
+}
