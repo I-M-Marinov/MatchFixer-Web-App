552e232789a01b133bee1a55373683cf31969e26
I-M-Marinov <lcfrrr@gmail.com>
2025-04-29 23:00:27 +0300

    Created the GeneralConstants folder to hold constants about certain services
    -- Added the ProfilePictureConstants that gold the path, publicId and default image guid

---
+++ MatchFixer.Common/GeneralConstants/ProfilePictureConstants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatchFixer.Common.GeneralConstants
+{
+	public static  class ProfilePictureConstants
+	{
+		public const string DefaultImagePath = "/images/default-user-image.png";
+		public const string DefaultPublicId = "default-public-id";
+		public static readonly Guid DefaultImageId = new Guid("00000000-0000-0000-0000-000000000001");
+	}
+}
