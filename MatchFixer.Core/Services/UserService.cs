0ba0402acb134a5dd4c6ce603eea10d8b113e146
I-M-Marinov <lcfrrr@gmail.com>
2025-04-29 23:01:51 +0300

    Created the UserService
    -- Created GetOrCreateDefaultImageAsync ---> Method that would get the default profile picture from the DB ( it is not available it will create it first )

---
+++ MatchFixer.Core/Services/UserService.cs
@@ -0,0 +1,42 @@
+﻿using MatchFixer.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+using static MatchFixer.Common.GeneralConstants.ProfilePictureConstants;
+using MatchFixer.Infrastructure.Entities;
+using MatchFixer.Core.Contracts;
+
+namespace MatchFixer.Core.Services
+{
+	public class UserService : IUserService
+	{
+
+		private readonly MatchFixerDbContext _context;
+
+		public UserService(MatchFixerDbContext context)
+		{
+			_context = context;
+		}
+
+
+		public async Task<Guid> GetOrCreateDefaultImageAsync()
+		{
+			var defaultImage = await _context.ProfilePictures
+				.FirstOrDefaultAsync(img => img.Id == DefaultImageId);
+
+			if (defaultImage == null)
+			{
+				defaultImage = new ProfilePicture
+				{
+					Id = DefaultImageId,
+					ImageUrl = DefaultImagePath,
+					PublicId = DefaultPublicId
+				};
+
+				_context.ProfilePictures.Add(defaultImage);
+				await _context.SaveChangesAsync();
+			}
+
+			return defaultImage.Id;
+		}
+	}
+}
