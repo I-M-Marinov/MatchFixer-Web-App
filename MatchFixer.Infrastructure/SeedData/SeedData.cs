3707f13aa1b294d05a90f03286e5dcf1047c1317
I-M-Marinov <lcfrrr@gmail.com>
2025-04-29 22:53:42 +0300

    Created the SeedData class to seed data into the application
    -- Added the SeedDefaultProfilePicture method that seeds the default profile picture in the ProfilePictures table in the DB

---
+++ MatchFixer.Infrastructure/SeedData/SeedData.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+
+using MatchFixer.Infrastructure.Entities;
+using MatchFixer.Infrastructure.Models;
+
+using static MatchFixer.Common.GeneralConstants.ProfilePictureConstants;
+
+
+namespace MatchFixer.Infrastructure.SeedData
+{
+	public class SeedData
+	{
+		public static async Task SeedDefaultProfilePicture(UserManager<ApplicationUser> userManager, IServiceProvider serviceProvider)
+		{
+			var dbContext = serviceProvider.GetRequiredService<MatchFixerDbContext>();
+
+			var defaultImage = await dbContext.ProfilePictures
+				.FirstOrDefaultAsync(img => img.Id == DefaultImageId); // check if it already exists in the DB
+
+			if (defaultImage == null) // if it does not exist create it 
+			{
+				defaultImage = new ProfilePicture
+				{
+					Id = DefaultImageId,
+					ImageUrl = DefaultImagePath,
+					PublicId = DefaultPublicId
+				};
+
+				dbContext.ProfilePictures.Add(defaultImage); 
+				await dbContext.SaveChangesAsync();
+			}
+
+			await dbContext.SaveChangesAsync();
+		}
+	}
+}
