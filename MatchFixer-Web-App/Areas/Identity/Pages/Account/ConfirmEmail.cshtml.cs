38a1b426a39584f971cdaf84f7c51605a45b336b
I-M-Marinov <lcfrrr@gmail.com>
2025-04-29 23:14:12 +0300

    Refactored the ConfirmEmail View logic:
    -- OnGetAsync method would now redirect to the Login page passing a success or an error message as well

--- MatchFixer-Web-App/Areas/Identity/Pages/Account/ConfirmEmail.cshtml.cs
+++ MatchFixer-Web-App/Areas/Identity/Pages/Account/ConfirmEmail.cshtml.cs
@@ -2,12 +2,9 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 #nullable disable
 
-using System;
-using System.Linq;
+
 using System.Text;
-using System.Threading.Tasks;
 using MatchFixer.Infrastructure.Entities;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -30,23 +27,30 @@ namespace MatchFixer_Web_App.Areas.Identity.Pages.Account
         /// </summary>
         [TempData]
         public string StatusMessage { get; set; }
-        public async Task<IActionResult> OnGetAsync(string userId, string code)
-        {
-            if (userId == null || code == null)
-            {
-                return RedirectToPage("/Index");
-            }
+		public async Task<IActionResult> OnGetAsync(string userId, string code, string? returnUrl = null)
+		{
+			if (userId == null || code == null)
+			{
+				return RedirectToPage("/Index");
+			}
 
-            var user = await _userManager.FindByIdAsync(userId);
-            if (user == null)
-            {
-                return NotFound($"Unable to load user with ID '{userId}'.");
-            }
+			var user = await _userManager.FindByIdAsync(userId);
 
-            code = Encoding.UTF8.GetString(WebEncoders.Base64UrlDecode(code));
-            var result = await _userManager.ConfirmEmailAsync(user, code);
-            StatusMessage = result.Succeeded ? "Thank you for confirming your email." : "Error confirming your email.";
-            return Page();
-        }
-    }
+			if (user == null)
+			{
+				return NotFound($"Unable to load user with ID '{userId}'.");
+			}
+
+			var decodedCode = Encoding.UTF8.GetString(WebEncoders.Base64UrlDecode(code));
+			var result = await _userManager.ConfirmEmailAsync(user, decodedCode);
+			if (!result.Succeeded)
+			{
+				StatusMessage = "Error confirming your email.";
+				return RedirectToPage("/Account/Login");
+			}
+
+			TempData["Success"] = "Your email has been successfully confirmed. You can now log in.";
+			return RedirectToPage("/Account/Login");
+		}
+	}
 }
