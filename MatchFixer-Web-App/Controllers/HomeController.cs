98493ef5831c51ef9a8f2b19ecad0f7bb7aff24a
I-M-Marinov <lcfrrr@gmail.com>
2025-05-02 23:13:56 +0300

    Updated the Home Controller:
    --    Added Headers to ensure the browser is not going to store a cached version of the Home page and if you hit back it will show it to the logged-in users

--- MatchFixer-Web-App/Controllers/HomeController.cs
+++ MatchFixer-Web-App/Controllers/HomeController.cs
@@ -1,10 +1,12 @@
 using System.Diagnostics;
 using MatchFixer_Web_App.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MatchFixer_Web_App.Controllers
 {
-    public class HomeController : Controller
+	[AllowAnonymous]
+	public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
 
@@ -15,7 +17,17 @@ namespace MatchFixer_Web_App.Controllers
 
         public IActionResult Index()
         {
-            return View();
+	        if (User.Identity.IsAuthenticated) // if the user is already logged in redirect him to the profile page, not the home page 
+	        {
+				return RedirectToAction("Profile", "Profile");
+	        }
+
+			// Ensures the browser is not going to store a cached version of the Home page and if you hit back it will show it to the logged-in users 
+			Response.Headers["Cache-Control"] = "no-cache, no-store, must-revalidate";
+			Response.Headers["Pragma"] = "no-cache";
+			Response.Headers["Expires"] = "0";
+
+			return View();
         }
 
         public IActionResult Privacy()
