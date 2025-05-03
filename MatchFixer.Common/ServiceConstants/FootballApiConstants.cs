a17dfe696056c2614b993e58d47af3a9257071fa
I-M-Marinov <lcfrrr@gmail.com>
2025-05-04 00:35:11 +0300

    Created the FootballApiConstants static class to hold the ids and season code that are used in the FootballApiService

---
+++ MatchFixer.Common/ServiceConstants/FootballApiConstants.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace MatchFixer.Common.ServiceConstants
+{
+	public static class FootballApiConstants
+	{
+		public const int PremierLeagueId = 39;
+		public const int LaLigaId = 140;
+		public const int BundesligaId = 78;
+		public const int Season = 2023;
+	}
+}
