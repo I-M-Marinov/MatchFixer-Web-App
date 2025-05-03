5c29df8cdb3d98dd1f0ece1ae93bca0a2b69381e
I-M-Marinov <lcfrrr@gmail.com>
2025-05-04 00:34:24 +0300

    Created a new migration and updated the DB with the new entity

---
+++ MatchFixer.Infrastructure/Migrations/20250503205608_AddMatchResultEntity.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MatchFixer.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMatchResultEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MatchResults",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ApiFixtureId = table.Column<int>(type: "int", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LeagueName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Season = table.Column<int>(type: "int", nullable: false),
+                    HomeTeam = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    HomeTeamLogo = table.Column<string>(type: "nvarchar(300)", maxLength: 300, nullable: false),
+                    AwayTeam = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    AwayTeamLogo = table.Column<string>(type: "nvarchar(300)", maxLength: 300, nullable: false),
+                    HomeScore = table.Column<int>(type: "int", nullable: true),
+                    AwayScore = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MatchResults", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MatchResults");
+        }
+    }
+}
