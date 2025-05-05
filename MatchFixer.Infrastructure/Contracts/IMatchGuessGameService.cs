f9e8a28a2b47281e12e7dd4b9aacf1871ed51d7d
I-M-Marinov <lcfrrr@gmail.com>
2025-05-05 23:24:37 +0300

    Created the IMatchGuessGameService interface

---
+++ MatchFixer.Infrastructure/Contracts/IMatchGuessGameService.cs
@@ -0,0 +1,11 @@
+﻿using MatchFixer.Infrastructure.Entities;
+
+namespace MatchFixer.Infrastructure.Contracts
+{
+	public interface IMatchGuessGameService
+	{
+		Task<MatchResult?> GetRandomMatchAsync();
+		bool CheckAnswer(MatchResult match, int userHomeScore, int userAwayScore);
+		Task<MatchResult?> GetMatchByIdAsync(int id);
+	}
+}
