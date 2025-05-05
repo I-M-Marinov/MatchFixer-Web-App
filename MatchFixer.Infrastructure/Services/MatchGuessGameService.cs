ce3443067f9d10cb3c971a8ad04cbc68c38a8ed5
I-M-Marinov <lcfrrr@gmail.com>
2025-05-05 23:25:52 +0300

    Created the MatchGuessGameService that would:
    --- Get a random match from the MatchResults table
    --- Verify if an answer that the user gave is correct or not
    --- Get a match via it's Id from the MatchResults table

---
+++ MatchFixer.Infrastructure/Services/MatchGuessGameService.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using MatchFixer.Infrastructure.Entities;
+using MatchFixer.Infrastructure.Contracts;
+
+namespace MatchFixer.Infrastructure.Services
+{
+	public class MatchGuessGameService  : IMatchGuessGameService
+	{
+		private readonly MatchFixerDbContext _context;
+		private readonly Random _random = new();
+
+		public MatchGuessGameService(MatchFixerDbContext context)
+		{
+			_context = context;
+		}
+
+		public async Task<MatchResult?> GetRandomMatchAsync()
+		{
+			var totalMatches = await _context.MatchResults.CountAsync();
+			if (totalMatches == 0) return null;
+
+			int skip = _random.Next(0, totalMatches);
+			return await _context.MatchResults.Skip(skip).Take(1).FirstOrDefaultAsync();
+		}
+
+		public bool CheckAnswer(MatchResult match, int userHomeScore, int userAwayScore)
+		{
+			return match.HomeScore == userHomeScore && match.AwayScore == userAwayScore;
+		}
+		public async Task<MatchResult?> GetMatchByIdAsync(int id)
+		{
+			return await _context.MatchResults.FindAsync(id);
+		}
+	}
+
+}
