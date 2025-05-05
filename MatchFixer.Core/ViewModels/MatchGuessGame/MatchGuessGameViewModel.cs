092c0a656f4de95e58d6a21e04095c6ae3a3e437
I-M-Marinov <lcfrrr@gmail.com>
2025-05-05 23:22:41 +0300

    Created the MatchGuessGameViewModel that would hold general information needed in the Game Views

---
+++ MatchFixer.Core/ViewModels/MatchGuessGame/MatchGuessGameViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatchFixer.Core.ViewModels.MatchGuessGame
+{
+	public class MatchGuessGameViewModel
+	{
+		public int QuestionNumber { get; set; }
+		public int TotalQuestions { get; set; }
+		public int Score { get; set; }
+
+		public int MatchId { get; set; }
+
+		public string HomeTeam { get; set; }
+		public string AwayTeam { get; set; }
+
+		public string HomeTeamLogo { get; set; }
+		public string AwayTeamLogo { get; set; }
+		public int? ActualHomeScore { get; set; }
+		public int? ActualAwayScore { get; set; }
+		public bool IsAnswered { get; set; }
+		public int? UserHomeGuess { get; set; }
+		public int? UserAwayGuess { get; set; }
+
+		public bool? IsCorrect { get; set; }
+	}
+
+}
