83eabb6c8de405498ea0fdf096f5ccce9f4c1990
I-M-Marinov <lcfrrr@gmail.com>
2025-05-06 22:53:48 +0300

    Created the GameSessionState to hold the question number, score, total questions and a boolen if the last question is answered or not

---
+++ MatchFixer.Core/ViewModels/GameSessionState/GameSessionState.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace MatchFixer.Core.ViewModels.GameSessionState
+{
+	public class GameSessionState
+	{
+		public int QuestionNumber { get; set; } = 1;
+		public int Score { get; set; } = 0;
+		public int TotalQuestions { get; set; } = 5;
+		public bool LastQuestionAnswered { get; set; } = false;
+	}
+}
