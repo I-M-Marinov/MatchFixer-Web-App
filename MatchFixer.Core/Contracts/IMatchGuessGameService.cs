ac1e87fc197e0c6850aff1c32dd6fea45b317cd7
I-M-Marinov <lcfrrr@gmail.com>
2025-05-06 22:58:15 +0300

    Updated the IMatchGuessGameService interface:
    -- Added the ProcessAnswerAsync and PrepareNextQuestionAsync methods

---
+++ MatchFixer.Core/Contracts/IMatchGuessGameService.cs
@@ -0,0 +1,17 @@
+﻿using MatchFixer.Core.ViewModels.MatchGuessGame;
+using MatchFixer.Infrastructure.Entities;
+using Microsoft.AspNetCore.Http;
+
+namespace MatchFixer.Core.Contracts
+{
+	public interface IMatchGuessGameService
+	{
+		Task<MatchResult?> GetRandomMatchAsync();
+		bool CheckAnswer(MatchResult match, int userHomeScore, int userAwayScore);
+		Task<MatchResult?> GetMatchByIdAsync(int id);
+		Task<(MatchGuessGameViewModel ViewModel, bool IsGameOver)> PrepareNextQuestionAsync(ISession session);
+
+		Task<(MatchGuessGameViewModel ViewModel, bool IsGameOver)> ProcessAnswerAsync(ISession session,
+			MatchGuessGameViewModel submittedModel);
+	}
+}
