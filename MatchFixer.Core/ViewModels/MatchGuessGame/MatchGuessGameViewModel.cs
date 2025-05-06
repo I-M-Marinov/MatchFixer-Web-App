d685df41d58ff5cea43234c007e1df76d064646f
I-M-Marinov <lcfrrr@gmail.com>
2025-05-06 22:55:27 +0300

    Updated the MatchGuessGameViewModel -- removed unused usings

--- MatchFixer.Core/ViewModels/MatchGuessGame/MatchGuessGameViewModel.cs
+++ MatchFixer.Core/ViewModels/MatchGuessGame/MatchGuessGameViewModel.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace MatchFixer.Core.ViewModels.MatchGuessGame
 {
 	public class MatchGuessGameViewModel
