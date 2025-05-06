79b51facfbc903494aef45f0dade1e2e66c19c82
I-M-Marinov <lcfrrr@gmail.com>
2025-05-06 22:52:53 +0300

    Created the SessionExtensions

---
+++ MatchFixer.Core/Extensions/SessionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+
+namespace MatchFixer.Core.Extensions
+{
+	public static class SessionExtensions
+	{
+		public static void SetObject(this ISession session, string key, object value)
+		{
+			var json = JsonConvert.SerializeObject(value);
+			session.SetString(key, json);
+		}
+
+		public static T GetObject<T>(this ISession session, string key)
+		{
+			var value = session.GetString(key);
+			if (string.IsNullOrEmpty(value)) return default(T);
+			return JsonConvert.DeserializeObject<T>(value);
+		}
+
+	}
+}
