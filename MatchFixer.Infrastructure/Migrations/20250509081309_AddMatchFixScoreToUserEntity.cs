af74c17cc6a23ae2bdc15937ecde48e35a3c154d
I-M-Marinov <lcfrrr@gmail.com>
2025-05-09 21:51:25 +0300

    Migrated the newly added property in the ApplicationUser to the DB

---
+++ MatchFixer.Infrastructure/Migrations/20250509081309_AddMatchFixScoreToUserEntity.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MatchFixer.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMatchFixScoreToUserEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "MatchFixScore",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                comment: "Indicates the number of points the user has in the MatchFix Game");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MatchFixScore",
+                table: "AspNetUsers");
+        }
+    }
+}
