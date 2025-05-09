a38ea3247a611acd57a9597520c0c413d84abef4
I-M-Marinov <lcfrrr@gmail.com>
2025-05-09 21:40:06 +0300

    Added the UserId to the GameSessionState

--- MatchFixer.Core/ViewModels/GameSessionState/GameSessionState.cs
+++ MatchFixer.Core/ViewModels/GameSessionState/GameSessionState.cs
@@ -4,6 +4,7 @@ namespace MatchFixer.Core.ViewModels.GameSessionState
 {
 	public class GameSessionState
 	{
+		public string UserId { get; set; } = null!;
 		public int QuestionNumber { get; set; } = 1;
 		public int Score { get; set; } = 0;
 		public int TotalQuestions { get; set; } = 10;
