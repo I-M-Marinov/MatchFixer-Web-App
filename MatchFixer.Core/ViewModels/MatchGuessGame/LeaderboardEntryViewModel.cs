9e8000acd3af06f5ded4294abc46134ac3312e5f
I-M-Marinov <lcfrrr@gmail.com>
2025-05-09 21:47:58 +0300

    Created the LeaderboardEntryViewModel

---
+++ MatchFixer.Core/ViewModels/MatchGuessGame/LeaderboardEntryViewModel.cs
@@ -0,0 +1,11 @@
+﻿using MatchFixer.Infrastructure.Entities;
+
+namespace MatchFixer.Core.ViewModels.MatchGuessGame
+{
+	public class LeaderboardEntryViewModel
+	{
+		public string UserName { get; set; } = null!;
+		public string Picture { get; set; } = null!;
+		public int Score { get; set; }
+	}
+}
