d219901eecb3d363d312fb69e0ef15be97c76bb4
I-M-Marinov <lcfrrr@gmail.com>
2025-05-09 21:47:43 +0300

    Created the MatchFixScoreService and interface to get the top 10 players from the DB  via the GetTopPlayersAsync method and map them to the LeaderboardEntryViewModel and pass them to the GameController and in turn the controller will pass them to the newly created entry point view GameStartScreen

---
+++ MatchFixer.Core/Contracts/IMatchFixScoreService.cs
@@ -0,0 +1,11 @@
+﻿
+using MatchFixer.Core.ViewModels.MatchGuessGame;
+
+namespace MatchFixer.Core.Contracts
+{
+	public interface IMatchFixScoreService
+	{
+		Task<List<LeaderboardEntryViewModel>> GetTopPlayersAsync(int count = 10);
+
+	}
+}
---
+++ MatchFixer.Core/Services/MatchFixScoreService.cs
@@ -0,0 +1,33 @@
+﻿using MatchFixer.Core.ViewModels.MatchGuessGame;
+
+using MatchFixer.Core.Contracts;
+using MatchFixer.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+namespace MatchFixer.Core.Services
+{
+	public class MatchFixScoreService : IMatchFixScoreService
+	{
+		private readonly MatchFixerDbContext _context;
+
+		public MatchFixScoreService(MatchFixerDbContext context)
+		{
+			_context = context;
+		}
+
+		public async Task<List<LeaderboardEntryViewModel>> GetTopPlayersAsync(int count = 10)
+		{
+			return await _context.Users
+				.OrderByDescending(u => u.MatchFixScore)
+				.Take(count)
+				.Select(u => new LeaderboardEntryViewModel
+				{
+					UserName = u.FullName!,
+					Picture = u.ProfilePicture!.ImageUrl ?? string.Empty,
+					Score = u.MatchFixScore
+				})
+				.ToListAsync();
+		}
+	}
+
+}
