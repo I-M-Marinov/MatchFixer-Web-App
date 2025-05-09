b0681d2363319bd2585a398906428076d52e38e8
I-M-Marinov <lcfrrr@gmail.com>
2025-05-09 21:41:09 +0300

    Created the SessionService and Interface:
    -- GetSessionState method
    -- SetSessionState method
    -- InitializeSessionState method
    -- ClearSession method

---
+++ MatchFixer.Core/Contracts/ISessionService.cs
@@ -0,0 +1,12 @@
+﻿using MatchFixer.Core.ViewModels.GameSessionState;
+
+namespace MatchFixer.Core.Contracts
+{
+	public interface ISessionService
+	{
+		GameSessionState? GetSessionState();
+		void SetSessionState(GameSessionState state);
+		void InitializeSessionState(string userId);
+		void ClearSession();
+	}
+}
---
+++ MatchFixer.Core/Services/SessionService.cs
@@ -0,0 +1,50 @@
+﻿using MatchFixer.Core.Contracts;
+using MatchFixer.Core.Extensions;
+using MatchFixer.Core.ViewModels.GameSessionState;
+using Microsoft.AspNetCore.Http;
+
+using static MatchFixer.Common.GeneralConstants.SessionConstants;
+
+namespace MatchFixer.Core.Services
+{
+	public class SessionService : ISessionService
+	{
+		private const string SessionKey = GameSessionName;
+		private readonly IHttpContextAccessor _httpContextAccessor;
+
+		public SessionService(IHttpContextAccessor httpContextAccessor)
+		{
+			_httpContextAccessor = httpContextAccessor;
+		}
+
+		public GameSessionState? GetSessionState()
+		{
+			return _httpContextAccessor.HttpContext?.Session.GetObject<GameSessionState>(SessionKey);
+		}
+
+		public void SetSessionState(GameSessionState state)
+		{
+			_httpContextAccessor.HttpContext?.Session.SetObject(SessionKey, state);
+		}
+
+		public void InitializeSessionState(string userId)
+		{
+			var state = new GameSessionState
+			{
+				UserId = userId,
+				Score = 0,
+				QuestionNumber = 1,
+				TotalQuestions = 10,
+				LastQuestionAnswered = false
+			};
+
+			SetSessionState(state);
+		}
+
+		public void ClearSession()
+		{
+			_httpContextAccessor.HttpContext?.Session.Remove(SessionKey);
+		}
+	}
+
+}
