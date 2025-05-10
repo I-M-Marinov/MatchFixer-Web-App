2ecf8b22198d7722c4aa5dabee67b1321e141b73
I-M-Marinov <lcfrrr@gmail.com>
2025-05-10 16:30:23 +0300

    Created the SessionInitializationMiddleware:
    -- gets the user id and sets it in the session

---
+++ MatchFixer.Core/Middlewares/SessionInitializationMiddleware.cs
@@ -0,0 +1,31 @@
+﻿using MatchFixer.Infrastructure.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+
+
+namespace MatchFixer.Core.Middlewares
+{
+	public class SessionInitializationMiddleware
+	{
+		private readonly RequestDelegate _next;
+
+		public SessionInitializationMiddleware(RequestDelegate next)
+		{
+			_next = next;
+		}
+
+		public async Task InvokeAsync(HttpContext context, UserManager<ApplicationUser> userManager)
+		{
+			if (context.User.Identity.IsAuthenticated && context.Session.GetString("UserId") == null)
+			{
+				var user = await userManager.GetUserAsync(context.User);
+				if (user != null)
+				{
+					context.Session.SetString("UserId", user.Id.ToString());
+				}
+			}
+
+			await _next(context);
+		}
+	}
+}
