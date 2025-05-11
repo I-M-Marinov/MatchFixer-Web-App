92044272b1d3695fa41c0a504b5f6622db6f6ce3
I-M-Marinov <lcfrrr@gmail.com>
2025-05-11 23:02:06 +0300

    Added the Serie A Id to the FootballAPIConstants class
    added all leagues logos urls as well

--- MatchFixer.Common/ServiceConstants/FootballApiConstants.cs
+++ MatchFixer.Common/ServiceConstants/FootballApiConstants.cs
@@ -1,12 +1,16 @@
-﻿
-
-namespace MatchFixer.Common.ServiceConstants
+﻿namespace MatchFixer.Common.ServiceConstants
 {
 	public static class FootballApiConstants
 	{
 		public const int PremierLeagueId = 39;
 		public const int LaLigaId = 140;
 		public const int BundesligaId = 78;
+		public const int SeriaAId = 135;
 		public const int Season = 2023;
+
+		public const string PremierLeagueLogoUrl = "https://media.api-sports.io/football/leagues/39.png";
+		public const string LaLigaLogoUrl = "https://media.api-sports.io/football/leagues/140.png";
+		public const string BundesligaLogoUrl = "https://media.api-sports.io/football/leagues/78.png";
+		public const string SeriaALogoUrl = "https://media.api-sports.io/football/leagues/135.png";
 	}
 }
