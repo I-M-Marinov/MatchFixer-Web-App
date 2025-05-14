90455d1fe444e94e4322f5df9d961b463fdf2d32
I-M-Marinov <lcfrrr@gmail.com>
2025-05-14 21:46:33 +0300

    Added the constants for the delete-pictur-image in the GeneralConstants

--- MatchFixer.Common/GeneralConstants/ProfilePictureConstants.cs
+++ MatchFixer.Common/GeneralConstants/ProfilePictureConstants.cs
@@ -11,5 +11,9 @@ namespace MatchFixer.Common.GeneralConstants
 		public const string DefaultImagePath = "/images/default-user-image.png";
 		public const string DefaultPublicId = "default-public-id";
 		public static readonly Guid DefaultImageId = new Guid("00000000-0000-0000-0000-000000000001");
+
+		public const string DeletedUserImagePath = "/images/deleted-user-image.png";
+		public const string DeletedUserImagePublicId = "deleted-user-public-id";
+		public static readonly Guid DeletedUserImageId = new Guid("00000000-0000-0000-0000-000000000002");
 	}
 }
