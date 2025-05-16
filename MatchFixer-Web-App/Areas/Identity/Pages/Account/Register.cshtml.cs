42289dc5656223239ef8b775ddb30bf0210e2866
I-M-Marinov <lcfrrr@gmail.com>
2025-05-16 18:47:31 +0300

    Updated the Register View's logic:
    -- Cleaned usings
    -- Added LogoUrl from the GeneralConstants.ProfilePictureConstants

--- MatchFixer-Web-App/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ MatchFixer-Web-App/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -1,28 +1,23 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 #nullable disable
-
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
 using System.Text;
 using System.Text.Encodings.Web;
-using System.Threading;
-using System.Threading.Tasks;
+
 using ISO3166;
 using MatchFixer.Core.Contracts;
-using MatchFixer.Core.Services;
 using MatchFixer.Infrastructure.Entities;
 using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.WebUtilities;
-using Microsoft.Extensions.Logging;
+
+using static MatchFixer.Common.GeneralConstants.ProfilePictureConstants;
+
 
 namespace MatchFixer_Web_App.Areas.Identity.Pages.Account
 {
@@ -191,7 +186,7 @@ namespace MatchFixer_Web_App.Areas.Identity.Pages.Account
 
                     _logger.LogInformation("Sending confirmation to: " + Input.Email);
 
-					var logoUrl = "https://res.cloudinary.com/doorb7d6i/image/upload/v1744732462/matchFixer-logo_kj93zj.png"; 
+                    var logoUrl = LogoUrl;
 
 					var emailBody = $@"
 					<!DOCTYPE html>
