79b18d7f75bc15f3387147d830573c8224bfb6ac
I-M-Marinov <lcfrrr@gmail.com>
2025-05-16 18:58:10 +0300

    Added the LogoUrl constant in the static class ProfilePictureConstants

--- MatchFixer.Common/GeneralConstants/ProfilePictureConstants.cs
+++ MatchFixer.Common/GeneralConstants/ProfilePictureConstants.cs
@@ -15,5 +15,10 @@ namespace MatchFixer.Common.GeneralConstants
 		public const string DeletedUserImagePath = "/images/deleted-user-image.png";
 		public const string DeletedUserImagePublicId = "deleted-user-public-id";
 		public static readonly Guid DeletedUserImageId = new Guid("00000000-0000-0000-0000-000000000002");
+
+		// MatchFixer Logo 
+
+		public const string LogoUrl =
+			"https://res.cloudinary.com/doorb7d6i/image/upload/v1747399160/matchFixer-logo_yfzk57.png";
 	}
 }
