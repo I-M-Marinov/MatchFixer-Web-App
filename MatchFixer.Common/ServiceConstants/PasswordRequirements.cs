b2f09e7ec7c3edf2f62b54374502d0a9e35451a0
I-M-Marinov <lcfrrr@gmail.com>
2025-05-17 21:26:29 +0300

    Created the PasswordRequirements static class to hold of all the constants regarded to the passwords in the application  + regex pattern to match + error message

---
+++ MatchFixer.Common/ServiceConstants/PasswordRequirements.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace MatchFixer.Common.ServiceConstants
+{
+	public static class PasswordRequirements
+	{
+		public const int MinLength = 8;
+		public const bool RequireDigit = true;
+		public const bool RequireLowercase = true;
+		public const bool RequireUppercase = true;
+		public const bool RequireNonAlphanumeric = true;
+		public const string RegexPattern = @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,}$";
+		public const string ErrorMessage = "Password must be at least 8 characters and contain uppercase, lowercase, number, and special character.";
+	}
+}
