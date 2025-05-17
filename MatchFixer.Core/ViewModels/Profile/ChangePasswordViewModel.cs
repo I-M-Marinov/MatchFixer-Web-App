26983e3361005723a6f39295a5cc01705e6f0c37
I-M-Marinov <lcfrrr@gmail.com>
2025-05-17 21:27:22 +0300

    Created the ChangePasswordViewModel to be used in the DangerZone View to pass the passwords to the Controller + validations

---
+++ MatchFixer.Core/ViewModels/Profile/ChangePasswordViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static MatchFixer.Common.ServiceConstants.PasswordRequirements;
+
+namespace MatchFixer.Core.ViewModels.Profile
+{
+	public class ChangePasswordViewModel
+	{
+		[Required(ErrorMessage = "Current password is required.")]
+		[DataType(DataType.Password)]
+		public string CurrentPassword { get; set; } = null!;
+
+		[Required(ErrorMessage = "The new password field is required.")]
+		[DataType(DataType.Password)]
+		[RegularExpression(RegexPattern, ErrorMessage = ErrorMessage)]
+		public string NewPassword { get; set; } = null!;
+
+		[Required(ErrorMessage = "The confirmation password field is required.")]
+		[DataType(DataType.Password)]
+		[Compare("NewPassword", ErrorMessage = "The passwords do not match.")]
+		public string ConfirmPassword { get; set; } = null!;
+	}
+}
