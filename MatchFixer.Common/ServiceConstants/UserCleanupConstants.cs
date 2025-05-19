c4a9428527e7e09e97cd830d6c1dc0b190a7ba76
I-M-Marinov <lcfrrr@gmail.com>
2025-05-19 23:22:52 +0300

    Updated the CleanupInterval to 30 miunutes on the UserCleanupService

--- MatchFixer.Common/ServiceConstants/UserCleanupConstants.cs
+++ MatchFixer.Common/ServiceConstants/UserCleanupConstants.cs
@@ -2,7 +2,7 @@
 {
 	public static class UserCleanupConstants
 	{
-		public static readonly TimeSpan CleanupInterval = TimeSpan.FromHours(24);
+		public static readonly TimeSpan CleanupInterval = TimeSpan.FromMinutes(30);
 		public static readonly TimeSpan AccountExpiration = TimeSpan.FromHours(1);
 	}
 }
