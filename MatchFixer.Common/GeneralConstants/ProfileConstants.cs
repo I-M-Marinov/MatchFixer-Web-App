575f6dd9b8367c581390016ec7887aaf9b2be5b4
I-M-Marinov <lcfrrr@gmail.com>
2025-05-19 21:03:11 +0300

    Created the ProfileConstants static class to hold the messages and validations for the Profile Service

---
+++ MatchFixer.Common/GeneralConstants/ProfileConstants.cs
@@ -0,0 +1,26 @@
+﻿
+namespace MatchFixer.Common.GeneralConstants
+{
+	public static class ProfileConstants
+	{
+		// VALIDATIONS
+		public const string TimeZoneMissingOrIncorrect = "Time Zone is missing or incorrect !";
+		public const string FirstAndLastNameAreRequired = "First name and Last name are required.";
+
+
+
+
+		// MESSAGES
+		public const string NoChangesMadeToProfile = "No changes were made to the profile.";
+		public const string FailedToUpdateProfile = "Failed to update profile.";
+		public const string ProfileUpdatedSuccessfully = "Your profile was updated successfully. You have made changes to:";
+		public const string NameUpdatedSuccessfully = "Your name was updated successfully !";
+		public const string UserNotFound = "User not found.";
+		public const string FailedToUpdateUsersEmail = "Failed to update user email.";
+		public const string EmailUpdatedSuccessfully = "Email updated and confirmation sent.";
+		public const string NewAndCurrentEmailAreTheSame = "The email is the same as the current one.";
+		public const string EmailConfirmedSuccessfully = "Your email has been confirmed.";
+		public const string EmailConfirmationFailed = "Email confirmation failed.";
+
+	}
+}
