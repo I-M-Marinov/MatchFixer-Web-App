403bf5dc654878579299437e97b8ae7a9c83e2cd
I-M-Marinov <lcfrrr@gmail.com>
2025-05-19 21:04:52 +0300

    Created the AnonymizationConstants static class to hold the anonymized first, last names and coutry and timezone for the Profile Service's AnonymizeUserAsync method

---
+++ MatchFixer.Common/GeneralConstants/AnonymizationConstants.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace MatchFixer.Common.GeneralConstants
+{
+	public static class AnonymizationConstants
+	{
+		public const string AnonymizedFirstName = "Deleted";
+		public const string AnonymizedLastName = "User";
+		public const string AnonymizedCountry = "Unknown";
+		public const string AnonymizedTimeZone = "Unknown";
+	}
+}
