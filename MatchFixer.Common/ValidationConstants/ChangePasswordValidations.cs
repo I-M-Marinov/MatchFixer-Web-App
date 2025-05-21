b16e7d237f584b4675381b093a3994bdbb3ad165
I-M-Marinov <lcfrrr@gmail.com>
2025-05-21 13:00:29 +0300

    Created the ChangePasswordValidations to hold the validation messages for the  ChangePasswordViewModel

---
+++ MatchFixer.Common/ValidationConstants/ChangePasswordValidations.cs
@@ -0,0 +1,11 @@
+﻿namespace MatchFixer.Common.ValidationConstants
+{
+	public static class ChangePasswordValidations
+	{
+		public const string CurrentPasswordIsRequired = "Current password is required.";
+		public const string NewPasswordIsRequired = "The new password field is required.";
+		public const string ConfirmationPasswordIsRequired = "The confirmation password field is required.";
+
+		public const string PasswordsDoNotMatch = "The passwords do not match.";
+	}
+}
