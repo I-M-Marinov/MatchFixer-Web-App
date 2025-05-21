44e33c0871efc6e5e3bdcf02e5d779d2356d8b11
I-M-Marinov <lcfrrr@gmail.com>
2025-05-21 12:45:09 +0300

    Removed magic strings and numbers and instead moved to constants from the newly created MatchEventValidation static class

--- MatchFixer.Infrastructure/Entities/MatchEvent.cs
+++ MatchFixer.Infrastructure/Entities/MatchEvent.cs
@@ -1,26 +1,32 @@
 ﻿
 using System.ComponentModel.DataAnnotations;
 
+using static MatchFixer.Common.GeneralConstants.MatchEventValidation;
+
 namespace MatchFixer.Infrastructure.Entities
 {
 	public class MatchEvent
 	{
 		[Key]
 		public Guid Id { get; set; }
+
 		[Required]
-		public string HomeTeam { get; set; }
+		[StringLength(HomeTeamMaxLength, ErrorMessage = HomeTeamCannotExceed100Characters)]
+		public string HomeTeam { get; set; } = null!;
+
 		[Required]
-		public string AwayTeam { get; set; }
+		[StringLength(AwayTeamMaxLength, ErrorMessage = AwayTeamCannotExceed100Characters)]
+		public string AwayTeam { get; set; } = null!;
 		[Required]
 		public DateTime MatchDate { get; set; }
 
-		[Range(1.01, 1000)] 
+		[Range(OddsMinValue, OddsMaxValue)] 
 		public decimal? HomeOdds { get; set; }
 
-		[Range(1.01, 1000)]
+		[Range(OddsMinValue, OddsMaxValue)]
 		public decimal? DrawOdds { get; set; }
 
-		[Range(1.01, 1000)]
+		[Range(OddsMinValue, OddsMaxValue)]
 		public decimal? AwayOdds { get; set; }
 
 		public ICollection<Bet> Bets { get; set; } = new List<Bet>();
