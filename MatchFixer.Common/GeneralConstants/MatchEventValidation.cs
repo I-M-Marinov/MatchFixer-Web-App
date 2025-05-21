f11c60eb70ad94ca7947757b0946abab02e344fd
I-M-Marinov <lcfrrr@gmail.com>
2025-05-21 12:44:26 +0300

    Created the MatchEventValidation to hold all the validations and messages for the MatchEvent entity

---
+++ MatchFixer.Common/GeneralConstants/MatchEventValidation.cs
@@ -0,0 +1,14 @@
+﻿namespace MatchFixer.Common.GeneralConstants
+{
+	public static class MatchEventValidation
+	{
+		public const string HomeTeamCannotExceed100Characters = $"The home team name cannot exceed 100 characters.";
+		public const int HomeTeamMaxLength = 100;	
+		
+		public const string AwayTeamCannotExceed100Characters = "The away team name cannot exceed 100 characters.";
+		public const int AwayTeamMaxLength = 100;
+
+		public const double OddsMinValue = 1.01;
+		public const double OddsMaxValue = 1000;
+	}
+}
