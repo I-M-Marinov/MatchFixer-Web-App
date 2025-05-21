a64d9448850a15d474d46ad79676534ed8583e47
I-M-Marinov <lcfrrr@gmail.com>
2025-05-21 13:01:29 +0300

    Updated the ChangePasswordViewModel and removed all the magic strings ( now using constants from the newly createad static class ChangePasswordValidations )

--- MatchFixer.Core/ViewModels/Profile/ChangePasswordViewModel.cs
+++ MatchFixer.Core/ViewModels/Profile/ChangePasswordViewModel.cs
@@ -1,22 +1,23 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using static MatchFixer.Common.ServiceConstants.PasswordRequirements;
+using static MatchFixer.Common.ValidationConstants.ChangePasswordValidations;
 
 namespace MatchFixer.Core.ViewModels.Profile
 {
 	public class ChangePasswordViewModel
 	{
-		[Required(ErrorMessage = "Current password is required.")]
+		[Required(ErrorMessage = CurrentPasswordIsRequired)]
 		[DataType(DataType.Password)]
 		public string CurrentPassword { get; set; } = null!;
 
-		[Required(ErrorMessage = "The new password field is required.")]
+		[Required(ErrorMessage = NewPasswordIsRequired)]
 		[DataType(DataType.Password)]
 		[RegularExpression(RegexPattern, ErrorMessage = ErrorMessage)]
 		public string NewPassword { get; set; } = null!;
 
-		[Required(ErrorMessage = "The confirmation password field is required.")]
+		[Required(ErrorMessage = ConfirmationPasswordIsRequired)]
 		[DataType(DataType.Password)]
-		[Compare("NewPassword", ErrorMessage = "The passwords do not match.")]
+		[Compare(nameof(NewPassword), ErrorMessage = PasswordsDoNotMatch)]
 		public string ConfirmPassword { get; set; } = null!;
 	}
 }
