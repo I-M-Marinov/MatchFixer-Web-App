ed1e5bd30f11b914caff71f2c8ad8c5fcab3547a
I-M-Marinov <lcfrrr@gmail.com>
2025-05-22 12:26:24 +0300

    Created the MatchEventFormModel

---
+++ MatchFixer.Core/ViewModels/LiveEvents/MatchEventFormModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MatchFixer.Core.ViewModels.LiveEvents
+{
+	public class MatchEventFormModel
+	{
+		[Required] 
+		public string HomeTeam { get; set; } = null!;
+
+		[Required]
+		public string AwayTeam { get; set; } = null!;
+
+		[Required]
+		[DataType(DataType.DateTime)]
+		public DateTime MatchDate { get; set; }
+
+		[Range(1.01, 100)]
+		public decimal? HomeOdds { get; set; }
+
+		[Range(1.01, 100)]
+		public decimal? DrawOdds { get; set; }
+
+		[Range(1.01, 100)]
+		public decimal? AwayOdds { get; set; }
+	}
+
+}
