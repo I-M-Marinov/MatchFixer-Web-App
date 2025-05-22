f830179100189a076e41b69b21bdb060465d529e
I-M-Marinov <lcfrrr@gmail.com>
2025-05-22 12:26:41 +0300

    Created the LiveEventViewModel

---
+++ MatchFixer.Core/ViewModels/LiveEvents/LiveEventViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MatchFixer.Core.ViewModels.LiveEvents
+{
+	public class LiveEventViewModel
+	{
+		public Guid Id { get; set; }
+		public string HomeTeam { get; set; }
+		public string AwayTeam { get; set; }
+		public DateTime KickoffTime { get; set; }
+		public decimal HomeWinOdds { get; set; }
+		public decimal DrawOdds { get; set; }
+		public decimal AwayWinOdds { get; set; }
+	}
+
+}
