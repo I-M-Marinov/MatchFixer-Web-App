2102466c072a7ea87e1a2f98f8bd1af017ecf313
I-M-Marinov <lcfrrr@gmail.com>
2025-05-23 23:29:42 +0300

    Created the MatchEventService and Interface:
    ----> GetLiveEventsAsync ---> pull all events from the MatchEvents table ( all posted live events )
    
    ----> AddEventAsync ----> Add an event to the MatchEvents table ( essentially post it on the Events Board )
    
    ----> GetTeamLogo ( kind of a helper method, but keeping it public for now ) ----> Query the Teams table and pulls the logo url for a team by NAME

---
+++ MatchFixer.Core/Contracts/IMatchEventService.cs
@@ -0,0 +1,16 @@
+﻿using MatchFixer.Core.ViewModels.LiveEvents;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatchFixer.Core.Contracts
+{
+	public interface IMatchEventService
+	{
+		Task<List<LiveEventViewModel>> GetLiveEventsAsync();
+		Task AddEventAsync(MatchEventFormModel model);
+		Task<string> GetTeamLogo(string name);
+	}
+}
---
+++ MatchFixer.Core/Services/MatchEventService.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using MatchFixer.Core.Contracts;
+using MatchFixer.Core.ViewModels.LiveEvents;
+using MatchFixer.Infrastructure;
+using MatchFixer.Infrastructure.Entities;
+
+
+namespace MatchFixer.Core.Services
+{
+	public class MatchEventService : IMatchEventService
+	{
+		private readonly MatchFixerDbContext _dbContext;
+
+		public MatchEventService(MatchFixerDbContext dbContext)
+		{
+			_dbContext = dbContext;
+		}
+
+		public async Task<List<LiveEventViewModel>> GetLiveEventsAsync()
+		{
+			var events = await _dbContext.MatchEvents
+				.Include(e => e.HomeTeam)
+				.Include(e => e.AwayTeam)
+				.OrderBy(e => e.MatchDate)
+				.Select(e => new LiveEventViewModel
+				{
+					Id = Guid.NewGuid(),
+					HomeTeam = e.HomeTeam.Name,
+					AwayTeam = e.AwayTeam.Name,
+					KickoffTime = e.MatchDate,
+					HomeWinOdds = e.HomeOdds ?? 0,
+					DrawOdds = e.DrawOdds ?? 0,
+					AwayWinOdds = e.AwayOdds ?? 0,
+					HomeTeamLogoUrl = e.HomeTeam.LogoUrl,
+					AwayTeamLogoUrl = e.AwayTeam.LogoUrl
+				})
+				.AsNoTracking()
+				.ToListAsync();
+
+			return events;
+		}
+
+		public async Task AddEventAsync(MatchEventFormModel model)
+		{
+			var homeTeam = await _dbContext.Teams
+				.FirstOrDefaultAsync(t => t.Name == model.HomeTeam);
+
+			var awayTeam = await _dbContext.Teams
+				.FirstOrDefaultAsync(t => t.Name == model.AwayTeam);
+
+			if (homeTeam == null || awayTeam == null)
+			{
+				throw new Exception("Home or Away team does not exist!");
+			}
+
+			var matchEvent = new MatchEvent
+			{
+				Id = Guid.NewGuid(),
+				HomeTeamId = homeTeam!.Id,
+				AwayTeamId = awayTeam!.Id,
+				MatchDate = model.MatchDate,
+				HomeOdds = model.HomeOdds,
+				DrawOdds = model.DrawOdds,
+				AwayOdds = model.AwayOdds
+			};
+
+			await _dbContext.MatchEvents.AddAsync(matchEvent);
+			await _dbContext.SaveChangesAsync();
+		}
+
+		public async Task<string> GetTeamLogo(string name)
+		{
+			var logo = await _dbContext.Teams
+				.Where(t => t.Name == name)
+				.Select(t => t.LogoUrl.ToString())
+				.AsNoTracking()
+				.FirstOrDefaultAsync();
+
+			return logo;
+		}
+
+
+
+	}
+}
