8b6f2cc6581d30f9aca734b58f06c9531e9aa8e9
I-M-Marinov <lcfrrr@gmail.com>
2025-05-23 23:20:45 +0300

    Created the Team entity in the DB

---
+++ MatchFixer.Infrastructure/Entities/Team.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatchFixer.Infrastructure.Entities
+{
+	public class Team
+	{
+		[Key]
+		public Guid Id { get; set; }
+
+		[Required]
+		[StringLength(100)]
+		public string Name { get; set; }
+
+		[Required]
+		[Url]
+		public string LogoUrl { get; set; }
+
+		// Navigation properties for matches
+		public ICollection<MatchResult> HomeMatches { get; set; } = new List<MatchResult>();
+		public ICollection<MatchResult> AwayMatches { get; set; } = new List<MatchResult>();
+	}
+}
