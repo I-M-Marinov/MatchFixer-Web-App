cee2a6932760f771c2dd40a278b54e94c060c432
I-M-Marinov <lcfrrr@gmail.com>
2025-05-23 23:23:21 +0300

    Added the Ligue 1, Eredevisie, Liga Portugal, Croatian League and Swiss League's IDs to the static class FootballApiConstants

--- MatchFixer.Common/ServiceConstants/FootballApiConstants.cs
+++ MatchFixer.Common/ServiceConstants/FootballApiConstants.cs
@@ -2,11 +2,18 @@
 {
 	public static class FootballApiConstants
 	{
+		public const int Season = 2023;
+
 		public const int PremierLeagueId = 39;
 		public const int LaLigaId = 140;
 		public const int BundesligaId = 78;
 		public const int SeriaAId = 135;
-		public const int Season = 2023;
+		
+		public const int Ligue1Id = 61;
+		public const int EredivisieId = 88;
+		public const int LigaPortugalId = 94;
+		public const int CroatianLeagueId = 106;
+		public const int SwissLeagueId = 207;
 
 		public const string PremierLeagueLogoUrl = "https://media.api-sports.io/football/leagues/39.png";
 		public const string LaLigaLogoUrl = "https://media.api-sports.io/football/leagues/140.png";
