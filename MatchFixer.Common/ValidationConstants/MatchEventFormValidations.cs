ef7d4780c8391b316cec9799e2ef67b8d473a492
I-M-Marinov <lcfrrr@gmail.com>
2025-05-24 23:23:21 +0300

    Created the MatchEventFormValidations to hold all the validation error messages for the MatchEventFormView

---
+++ MatchFixer.Common/ValidationConstants/MatchEventFormValidations.cs
@@ -0,0 +1,14 @@
+﻿namespace MatchFixer.Common.ValidationConstants
+{
+	public static class MatchEventFormValidations
+	{
+		public const string ChooseAValidTeam = "Please choose a valid team !";
+		public const string ChooseAValidDateAndTime = "Please enter a valid date for the event !";
+		public const string ChooseValidHomeTeamOdds = "Please enter valid home team odds!";
+		public const string ChooseValidAwayTeamOdds = "Please enter valid away team odds!";
+		public const string ChooseValidDrawOdds = "Please enter valid draw odds!";
+
+
+
+	}
+}
