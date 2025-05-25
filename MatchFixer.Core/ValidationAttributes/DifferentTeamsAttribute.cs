a499a883154fab6800f02c2515b9f76f9bd19c6d
I-M-Marinov <lcfrrr@gmail.com>
2025-05-25 13:50:11 +0300

    Created the Different Teams Validation Attribute to validate if the user is putting the same team for the Home and Away Team in the AddMatchEvent View

---
+++ MatchFixer.Core/ValidationAttributes/DifferentTeamsAttribute.cs
@@ -0,0 +1,21 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using MatchFixer.Core.ViewModels.LiveEvents;
+
+namespace MatchFixer.Core.ValidationAttributes
+{
+	public class DifferentTeamsAttribute : ValidationAttribute
+	{
+		protected override ValidationResult IsValid(object? value, ValidationContext validationContext)
+		{
+			var model = (MatchEventFormModel)validationContext.ObjectInstance;
+
+			if (model.HomeTeamId == model.AwayTeamId && model.HomeTeamId != Guid.Empty)
+			{
+				return new ValidationResult("The home and away team cannot be the same.");
+			}
+
+			return ValidationResult.Success!;
+		}
+	}
+}
