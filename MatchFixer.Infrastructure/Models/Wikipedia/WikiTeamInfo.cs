1c43dfd20b4849420d846a0166098cf3667e0049
I-M-Marinov <lcfrrr@gmail.com>
2025-05-26 22:29:46 +0300

    Created the WikiTeamInfo

---
+++ MatchFixer.Infrastructure/Models/Wikipedia/WikiTeamInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace MatchFixer.Infrastructure.Models.Wikipedia
+{
+	public class WikiTeamInfo
+	{
+		public string Name { get; set; } = string.Empty;
+		public string Summary { get; set; } = string.Empty;
+		public string? ImageUrl { get; set; }
+		public string WikipediaUrl { get; set; } = string.Empty;
+		public List<string> Players { get; set; } = new List<string>();
+
+	}
+}
