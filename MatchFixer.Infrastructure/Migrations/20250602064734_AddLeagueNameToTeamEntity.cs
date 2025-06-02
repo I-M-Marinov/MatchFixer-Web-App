faf2f3b587a67a4ce95fa98ab907ce6d86fbdd92
I-M-Marinov <lcfrrr@gmail.com>
2025-06-02 11:39:02 +0300

    Created a migration and updated the DB with the new property for the Team Entity

---
+++ MatchFixer.Infrastructure/Migrations/20250602064734_AddLeagueNameToTeamEntity.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MatchFixer.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLeagueNameToTeamEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "LogoUrl",
+                table: "Teams",
+                type: "nvarchar(250)",
+                maxLength: 250,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LeagueName",
+                table: "Teams",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LeagueName",
+                table: "Teams");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LogoUrl",
+                table: "Teams",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(250)",
+                oldMaxLength: 250);
+        }
+    }
+}
