c9bf60d39b7f1c3b3233151e0cc74eac858a0b8b
I-M-Marinov <lcfrrr@gmail.com>
2025-06-02 11:35:51 +0300

    Added all League Names in the FootballApiConstants static class

--- MatchFixer.Common/ServiceConstants/FootballApiConstants.cs
+++ MatchFixer.Common/ServiceConstants/FootballApiConstants.cs
@@ -15,6 +15,17 @@
 		public const int CroatianLeagueId = 106;
 		public const int SwissLeagueId = 207;
 
+
+		public const string PremierLeagueName = "Premier League";
+		public const string LaLigaName = "La Liga";
+		public const string BundesligaName = "Bundesliga";
+		public const string SeriaAName = "Serie A";
+		public const string Ligue1Name = "Ligue 1";
+		public const string EredivisieName = "Eredivisie";
+		public const string LigaPortugalName = "Liga Portugal";
+		public const string CroatianLeagueName = "Croatian League";
+		public const string SwissLeagueName = "Swiss League";
+
 		public const string PremierLeagueLogoUrl = "https://media.api-sports.io/football/leagues/39.png";
 		public const string LaLigaLogoUrl = "https://media.api-sports.io/football/leagues/140.png";
 		public const string BundesligaLogoUrl = "https://media.api-sports.io/football/leagues/78.png";
