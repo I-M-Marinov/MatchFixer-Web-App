a765bf05ecc1342f639e2b282c940afdc5d7a8bd
I-M-Marinov <lcfrrr@gmail.com>
2025-06-06 23:51:53 +0300

    Created a migration and updated the DB with the new property of the MatchEvent Entity

---
+++ MatchFixer.Infrastructure/Migrations/20250606105152_AddedIsDerbyPropertyToMatchEventEntity.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MatchFixer.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedIsDerbyPropertyToMatchEventEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDerby",
+                table: "MatchEvents",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDerby",
+                table: "MatchEvents");
+        }
+    }
+}
