9b5032d7075d725a7dbcf97766a66947084dfa3e
I-M-Marinov <lcfrrr@gmail.com>
2025-06-08 23:20:21 +0300

    Created the BetController

---
+++ MatchFixer-Web-App/Controllers/BetController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using MatchFixer.Core.DTOs.Bets;
+using MatchFixer.Core.Contracts;
+
+namespace MatchFixer_Web_App.Controllers
+{
+	public class BetController : Controller
+	{
+
+		private readonly IBettingService _bettingService;
+
+		public BetController(IBettingService bettingService)
+		{
+			_bettingService = bettingService;
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> PlaceBet(BetSlipDto model)
+		{
+			var userId = Guid.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier)!);
+			var (message, success) = await _bettingService.PlaceBetAsync(userId, model);
+
+			if (!success)
+			{
+				TempData["ErrorMessage"] = message;
+				return RedirectToAction("LiveEvents", "Event");
+			}
+
+			TempData["SuccessMessage"] = message;
+			return RedirectToAction("LiveEvents", "Event");
+		}
+
+	}
+}
