7807ac2cb86a886b238fbdd35fd79f19ccf86ed2
I-M-Marinov <lcfrrr@gmail.com>
2025-06-08 23:18:46 +0300

    Created the BetSlipDto

---
+++ MatchFixer.Core/DTOs/Bets/BetSlipDto.cs
@@ -0,0 +1,9 @@
+﻿namespace MatchFixer.Core.DTOs.Bets
+{
+	public class BetSlipDto
+	{
+		public decimal Amount { get; set; }
+		public decimal TotalOdds { get; set; }
+		public List<SingleBetDto> Bets { get; set; }
+	}
+}
