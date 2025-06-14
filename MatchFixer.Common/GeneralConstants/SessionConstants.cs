3fb973eb1f46ade6f7661b4be2dc27dbc7d453d8
I-M-Marinov <lcfrrr@gmail.com>
2025-06-14 22:22:06 +0300

    Added the BetSlipSessionName constant to the SessionConstants static class

--- MatchFixer.Common/GeneralConstants/SessionConstants.cs
+++ MatchFixer.Common/GeneralConstants/SessionConstants.cs
@@ -3,5 +3,6 @@
 	public static class SessionConstants
 	{
 		public const string GameSessionName = "MatchFixGuessGameSessionKey";
+		public const string BetSlipSessionName = "BetSlipSession"; 
 	}
 }
