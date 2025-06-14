423fbb4d021ae9b040ed172610a10fc350f8dff3
I-M-Marinov <lcfrrr@gmail.com>
2025-06-14 22:22:58 +0300

    Created the  BetSlipState

---
+++ MatchFixer.Core/DTOs/Bets/BetSlipState.cs
@@ -0,0 +1,10 @@
+﻿namespace MatchFixer.Core.DTOs.Bets
+{
+	public class BetSlipState
+	{
+		public string UserId { get; set; } // optional for logged-in
+		public List<BetSlipItem> Bets { get; set; } = new List<BetSlipItem>();
+		public decimal TotalOdds { get; set; }
+		public decimal StakeAmount { get; set; }
+	}
+}
