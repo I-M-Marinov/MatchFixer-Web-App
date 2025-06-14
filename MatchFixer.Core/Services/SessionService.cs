564b0aaa8673fb242bd61975504cac7867765cf3
I-M-Marinov <lcfrrr@gmail.com>
2025-06-14 22:21:13 +0300

    Updated the SessionService:
    -- Added the SetBetSlipState method that would set the state of the Bet Slip in the session
    
    -- Added the ClearBetSlip method that would clear(delete) the session state for the Bet Slip
    
    -- Added the GetBetSlipState method that would get the state of the Bet Slip from the session
    
    -- Added the AddBetToSlip method that would add bets to the Bet Slip's session state

--- MatchFixer.Core/Services/SessionService.cs
+++ MatchFixer.Core/Services/SessionService.cs
@@ -1,4 +1,6 @@
-﻿using MatchFixer.Core.Contracts;
+﻿using System.Text.Json;
+using MatchFixer.Core.Contracts;
+using MatchFixer.Core.DTOs.Bets;
 using MatchFixer.Core.Extensions;
 using MatchFixer.Core.ViewModels.GameSessionState;
 using Microsoft.AspNetCore.Http;
@@ -44,6 +46,43 @@ namespace MatchFixer.Core.Services
 		public void ClearSession()
 		{
 			_httpContextAccessor.HttpContext?.Session.Remove(SessionKey);
+		}
+
+		public void SetBetSlipState(BetSlipState state)
+		{
+			_httpContextAccessor.HttpContext?.Session.SetObject(BetSlipSessionName, state);
+		}
+
+		public void ClearBetSlip()
+		{
+			_httpContextAccessor.HttpContext?.Session.Remove(BetSlipSessionName);
+		}
+
+		public BetSlipState? GetBetSlipState()
+		{
+			var stateJson = _httpContextAccessor.HttpContext?.Session.GetString(BetSlipSessionName);
+			return stateJson == null ? null : JsonSerializer.Deserialize<BetSlipState>(stateJson);
+		}
+
+		public void AddBetToSlip(BetSlipItem item)
+		{
+			var betSlip = GetBetSlipState() ?? new BetSlipState();
+
+			var existingBet = betSlip.Bets.FirstOrDefault(b =>
+				b.MatchId == item.MatchId &&
+				b.SelectedOption == item.SelectedOption);
+
+			if (existingBet != null)
+			{
+				existingBet.Odds = item.Odds;
+				existingBet.SelectedOption = item.SelectedOption;
+			}
+			else
+			{
+				betSlip.Bets.Add(item);
+			}
+
+			SetBetSlipState(betSlip);
 		}
 	}
 
