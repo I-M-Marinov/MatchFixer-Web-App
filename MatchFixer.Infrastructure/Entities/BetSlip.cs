50b1a9e3922962f40d1b8d0be8e448ff85b3ba29
I-M-Marinov <lcfrrr@gmail.com>
2025-06-15 22:06:40 +0300

    Created the Bet Slip Entity

---
+++ MatchFixer.Infrastructure/Entities/BetSlip.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+using static MatchFixer.Common.ValidationConstants.BetValidations;
+
+
+namespace MatchFixer.Infrastructure.Entities
+{
+	public class BetSlip
+	{
+		[Key]
+		[Comment("Unique identifier for a BetSlip")]
+		public Guid Id { get; set; }
+
+		[Required(ErrorMessage = AmountIsRequired)]
+		[Range(1.00, 50000, ErrorMessage = AmountMustBeBetweenOneAndFiftyThousand)]
+		[Comment("Amount of the Bet")]
+		public decimal Amount { get; set; }
+
+		[Required]
+		[Comment("Id of the user who placed the bet slip")]
+		public Guid UserId { get; set; }
+
+		[ForeignKey(nameof(UserId))]
+		public ApplicationUser User { get; set; }
+
+		[Required]
+		[Comment("Time the bet slip was placed")]
+		public DateTime BetTime { get; set; } = DateTime.UtcNow;
+
+		[Comment("Indicates whether the bet slip has been settled")]
+		public bool IsSettled { get; set; }
+
+		[Comment("The total win amount for the bet slip if successful")]
+		public decimal? WinAmount { get; set; }
+
+		// Navigation property for all bets
+		public ICollection<Bet> Bets { get; set; } = new List<Bet>();
+	}
+
+}
