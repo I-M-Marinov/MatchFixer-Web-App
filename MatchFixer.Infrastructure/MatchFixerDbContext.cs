7ef2e4ac45354c377e73b8912c02ab73bfa317c8
I-M-Marinov <lcfrrr@gmail.com>
2025-06-15 22:07:22 +0300

    Added the BetSlips to the MatchFixerDbContext, as well as some foreign keys with the FLUENT Api

--- MatchFixer.Infrastructure/MatchFixerDbContext.cs
+++ MatchFixer.Infrastructure/MatchFixerDbContext.cs
@@ -17,6 +17,7 @@ namespace MatchFixer.Infrastructure
  		public virtual DbSet<ProfilePicture> ProfilePictures { get; set; }
 		public virtual DbSet<MatchEvent> MatchEvents { get; set; }
 		public virtual DbSet<Bet> Bets { get; set; }
+		public virtual DbSet<BetSlip> BetSlips { get; set; }
 		public virtual DbSet<Team> Teams { get; set; }
 		public virtual DbSet<MatchResult> MatchResults { get; set; }
 
@@ -25,22 +26,38 @@ namespace MatchFixer.Infrastructure
 		{
 			base.OnModelCreating(builder);
 
+			builder.Entity<BetSlip>()
+				.HasOne(bs => bs.User)
+				.WithMany(u => u.BetSlips)
+				.HasForeignKey(bs => bs.UserId);
+
 			builder.Entity<Bet>()
-				.HasOne(b => b.User)
-				.WithMany(u => u.Bets) 
-				.HasForeignKey(b => b.UserId);
+				.HasOne(b => b.BetSlip)
+				.WithMany(bs => bs.Bets)
+				.HasForeignKey(b => b.BetSlipId)
+				.OnDelete(DeleteBehavior.Cascade); // when a slip is deleted, delete its bets
+
+			builder.Entity<Bet>()
+				.HasOne(b => b.MatchEvent)
+				.WithMany(me => me.Bets)  
+				.HasForeignKey(b => b.MatchEventId);
 
 			builder.Entity<MatchEvent>(entity =>
-			 {
-			 	entity.Property(e => e.HomeOdds).HasPrecision(7, 2);
-			 	entity.Property(e => e.DrawOdds).HasPrecision(7, 2);
-			 	entity.Property(e => e.AwayOdds).HasPrecision(7, 2);
-			 });
+			{
+				entity.Property(e => e.HomeOdds).HasPrecision(7, 2);
+				entity.Property(e => e.DrawOdds).HasPrecision(7, 2);
+				entity.Property(e => e.AwayOdds).HasPrecision(7, 2);
+			});
 
 			builder.Entity<Bet>(entity =>
 			{
-				entity.Property(e => e.Amount).HasPrecision(10, 2); 
-				entity.Property(e => e.WinAmount).HasPrecision(10, 2); 
+				entity.Property(e => e.Odds).HasPrecision(7, 2);  // new property for odds per bet
+			});
+
+			builder.Entity<BetSlip>(entity =>
+			{
+				entity.Property(e => e.Amount).HasPrecision(10, 2);
+				entity.Property(e => e.WinAmount).HasPrecision(10, 2);
 			});
 
 			builder.Entity<Team>()
