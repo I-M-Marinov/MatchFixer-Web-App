1c5e30d8175791bb9d7f9ef637ab3b6c31a12dbb
I-M-Marinov <lcfrrr@gmail.com>
2025-06-16 13:18:04 +0300

    Added the GetBetsByUserAsync to the BettingService Interface

--- MatchFixer.Core/Contracts/IBettingService.cs
+++ MatchFixer.Core/Contracts/IBettingService.cs
@@ -6,5 +6,6 @@ namespace MatchFixer.Core.Contracts
 	public interface IBettingService
 	{
 		Task<(string Message, bool IsSuccess)> PlaceBetAsync(Guid userId, BetSlipDto betSlipDto);
+		Task<IEnumerable<UserBetSlipDTO>> GetBetsByUserAsync(Guid userId);
 	}
 }
