9ebfe0b5c338549d073b837fc2f83f53db120e4d
I-M-Marinov <lcfrrr@gmail.com>
2025-06-16 13:17:05 +0300

    Created the UserBetsController

---
+++ MatchFixer-Web-App/Controllers/UserBetsController.cs
@@ -0,0 +1,33 @@
+﻿using MatchFixer.Core.Contracts;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace MatchFixer_Web_App.Controllers
+{
+	[Authorize]
+	public class UserBetsController : Controller
+	{
+
+		private readonly IBettingService _bettingService;
+
+		public UserBetsController(IBettingService bettingService)
+		{
+			_bettingService = bettingService;
+		}
+
+		public async Task<IActionResult> UserBets()
+		{
+			var userIdString = User.FindFirstValue(ClaimTypes.NameIdentifier);
+			if (!Guid.TryParse(userIdString, out var userId))
+			{
+				return Unauthorized();
+			}
+
+			var userBets = await _bettingService.GetBetsByUserAsync(userId);
+
+			return View(userBets);
+		}
+		
+	}
+}
