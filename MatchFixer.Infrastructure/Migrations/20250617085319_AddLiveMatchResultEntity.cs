ca9072aab053fa46b568fee0da9bc7cf15b58432
I-M-Marinov <lcfrrr@gmail.com>
2025-06-17 12:52:24 +0300

    Created a migration adding the LiveMatchResult entity and updated the DB

---
+++ MatchFixer.Infrastructure/Migrations/20250617085319_AddLiveMatchResultEntity.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MatchFixer.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLiveMatchResultEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "LiveMatchResult",
+                columns: table => new
+                {
+                    MatchEventId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    HomeScore = table.Column<int>(type: "int", nullable: false),
+                    AwayScore = table.Column<int>(type: "int", nullable: false),
+                    RecordedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Notes = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LiveMatchResult", x => x.MatchEventId);
+                    table.ForeignKey(
+                        name: "FK_LiveMatchResult_MatchEvents_MatchEventId",
+                        column: x => x.MatchEventId,
+                        principalTable: "MatchEvents",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LiveMatchResult");
+        }
+    }
+}
