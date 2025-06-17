4d5c95763b1f0030e59824e8b05f7223d4ae6407
I-M-Marinov <lcfrrr@gmail.com>
2025-06-17 12:51:18 +0300

    Created the LiveMatchResult entity to hold the actual result outcome of every MatchEvent

---
+++ MatchFixer.Infrastructure/Entities/LiveMatchResult.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace MatchFixer.Infrastructure.Entities
+{
+	public class LiveMatchResult
+	{
+		[Key] [ForeignKey(nameof(MatchEvent))] 
+		public Guid MatchEventId { get; set; } // Same as MatchEvent.Id
+
+		public MatchEvent MatchEvent { get; set; }
+
+		[Required] [Range(0, 20)] public int HomeScore { get; set; }
+
+		[Required] [Range(0, 20)] public int AwayScore { get; set; }
+
+		[Required] public DateTime RecordedAt { get; set; } = DateTime.UtcNow;
+
+		public string? Notes { get; set; }
+	}
+}
