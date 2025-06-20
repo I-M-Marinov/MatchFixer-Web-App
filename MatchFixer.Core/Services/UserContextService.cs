1e62de45c88c3e9a94ef3babf859c20cb229599e
I-M-Marinov <lcfrrr@gmail.com>
2025-06-20 14:43:22 +0300

    Created the UserContextService to handle retrieval of the userId and user timezone where needed in the application's services.

---
+++ MatchFixer.Core/Services/UserContextService.cs
@@ -0,0 +1,28 @@
+﻿using MatchFixer.Core.Contracts;
+using Microsoft.AspNetCore.Http;
+using System.Security.Claims;
+
+
+namespace MatchFixer.Core.Services
+{
+	public class UserContextService : IUserContextService
+	{
+		private readonly IHttpContextAccessor _httpContextAccessor;
+
+		public UserContextService(IHttpContextAccessor httpContextAccessor)
+		{
+			_httpContextAccessor = httpContextAccessor;
+		}
+
+		public Guid GetUserId()
+		{
+			var userId = _httpContextAccessor.HttpContext?.User?.FindFirstValue(ClaimTypes.NameIdentifier);
+			return Guid.TryParse(userId, out var guid) ? guid : Guid.Empty;
+		}
+
+		public string GetUserTimeZone()
+		{
+			return _httpContextAccessor.HttpContext?.User?.FindFirst("TimeZone")?.Value ?? "UTC";
+		}
+	}
+}
