ef9436b5279791e29aec64b922070dc6c8b907db
Ivan Marinov <134225981+I-M-Marinov@users.noreply.github.com>
2025-06-21 20:46:20 +0300

    Created the WalletViewModel

---
+++ MatchFixer.Core/ViewModels/Wallet/WalletViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace MatchFixer.Core.ViewModels.Wallet
+{
+	public class WalletViewModel
+	{
+		public decimal Balance { get; set; }
+		public string Currency { get; set; }
+
+		public List<WalletTransactionViewModel> Transactions { get; set; }
+	}
+}
