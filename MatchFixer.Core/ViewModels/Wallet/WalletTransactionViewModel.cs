f6e268d62fe147afa2ce138301dcc3cd77853f0a
Ivan Marinov <134225981+I-M-Marinov@users.noreply.github.com>
2025-06-21 20:46:08 +0300

    Created the WalletTransactionViewModel

---
+++ MatchFixer.Core/ViewModels/Wallet/WalletTransactionViewModel.cs
@@ -0,0 +1,12 @@
+﻿using MatchFixer.Common.Enums;
+
+namespace MatchFixer.Core.ViewModels.Wallet
+{
+	public class WalletTransactionViewModel
+	{
+		public DateTime CreatedAt { get; set; }
+		public decimal Amount { get; set; }
+		public WalletTransactionType TransactionType { get; set; }
+		public string Description { get; set; }
+	}
+}
