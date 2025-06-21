172eaa63cc11c8ed80112c9a823e899007356ce4
Ivan Marinov <134225981+I-M-Marinov@users.noreply.github.com>
2025-06-21 20:44:03 +0300

    Created the Wallet Entity

---
+++ MatchFixer.Infrastructure/Entities/Wallet.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MatchFixer.Infrastructure.Entities
+{
+	public class Wallet
+	{
+		[Key]
+		public Guid Id { get; set; }
+
+		[Required]
+		public Guid UserId { get; set; }
+
+		[ForeignKey("UserId")]
+		public ApplicationUser User { get; set; }
+
+		[Column(TypeName = "decimal(18,2)")]
+		public decimal Balance { get; set; } = 0m;		
+
+		[MaxLength(3)]
+		public string Currency { get; set; } = "EUR"; // default currently Euro
+
+		public DateTime CreatedAt { get; set; } = DateTime.UtcNow; // create time in UTC 
+
+		public DateTime UpdatedAt { get; set; } = DateTime.UtcNow; // updated time in UTC
+
+		public ICollection<WalletTransaction> Transactions { get; set; }
+	}
+}
