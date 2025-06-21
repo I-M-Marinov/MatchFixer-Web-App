03bd87654b53932d62b19a971693349de791bd5f
Ivan Marinov <134225981+I-M-Marinov@users.noreply.github.com>
2025-06-21 20:38:41 +0300

    Created the IWalletService

---
+++ MatchFixer.Core/Contracts/IWalletService.cs
@@ -0,0 +1,11 @@
+﻿using MatchFixer.Infrastructure.Entities;
+
+namespace MatchFixer.Core.Contracts
+{
+	public interface IWalletService
+	{
+		Task<bool> HasWalletAsync(Guid userId);
+		Task<Wallet> GetWalletAsync(Guid userId);
+		Task<Wallet> CreateWalletAsync(Guid userId);
+	}
+}
