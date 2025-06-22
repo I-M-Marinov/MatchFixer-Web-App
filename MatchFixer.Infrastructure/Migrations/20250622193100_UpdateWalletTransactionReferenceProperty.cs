059c034c1e7eacb102588ce328658183e195c3c0
Ivan Marinov <134225981+I-M-Marinov@users.noreply.github.com>
2025-06-22 23:23:20 +0300

    Created a new migration ( updating the max length of the Reference property of the WalletTransaction entity. Updated the DB.

---
+++ MatchFixer.Infrastructure/Migrations/20250622193100_UpdateWalletTransactionReferenceProperty.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MatchFixer.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateWalletTransactionReferenceProperty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Reference",
+                table: "WalletTransactions",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Reference",
+                table: "WalletTransactions",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(255)",
+                oldMaxLength: 255);
+        }
+    }
+}
