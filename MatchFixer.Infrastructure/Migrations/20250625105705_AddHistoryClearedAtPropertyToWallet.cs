43e63a48977ad9222395b33c4c2fcd50f3430279
Ivan Marinov <134225981+I-M-Marinov@users.noreply.github.com>
2025-06-25 15:26:32 +0300

    Added a new migration, adding the HistoryClearedAt property to the Wallet entity and updated the DB

---
+++ MatchFixer.Infrastructure/Migrations/20250625105705_AddHistoryClearedAtPropertyToWallet.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MatchFixer.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddHistoryClearedAtPropertyToWallet : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "HistoryClearedAt",
+                table: "Wallets",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HistoryClearedAt",
+                table: "Wallets");
+        }
+    }
+}
