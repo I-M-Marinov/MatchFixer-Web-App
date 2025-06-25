8d5b78586fd946e1fcc84a2f539af78df711adb6
Ivan Marinov <134225981+I-M-Marinov@users.noreply.github.com>
2025-06-25 15:27:49 +0300

    Updated the IWalletService interface:
    -- Added the ClearTransactionHistoryAsync and GetWalletViewModelAsync method signatures

--- MatchFixer.Core/Contracts/IWalletService.cs
+++ MatchFixer.Core/Contracts/IWalletService.cs
@@ -1,13 +1,16 @@
-﻿using MatchFixer.Infrastructure.Entities;
+﻿using MatchFixer.Core.ViewModels.Wallet;
+using MatchFixer.Infrastructure.Entities;
 
 namespace MatchFixer.Core.Contracts
 {
 	public interface IWalletService
 	{
 		Task<bool> HasWalletAsync();
-		Task<Wallet> GetWalletAsync();
+		Task<Wallet> GetWalletAsync(); 
+		Task<WalletViewModel> GetWalletViewModelAsync();
 		Task<Wallet> CreateWalletAsync();
 		Task DepositAsync(decimal amount, string description = null);
 		Task<bool> WithdrawAsync(decimal amount, string description = null);
+		Task<(bool Success, string Message)> ClearTransactionHistoryAsync();
 	}
 }
