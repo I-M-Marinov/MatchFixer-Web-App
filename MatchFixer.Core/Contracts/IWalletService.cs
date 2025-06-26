ee9856c8288e02fdc79b0cf67ba2a390a001dd7f
Ivan Marinov <134225981+I-M-Marinov@users.noreply.github.com>
2025-06-26 14:30:43 +0300

    Updated the SessionService Interface:
    -- Added the ClearTransactionHistoryAsync and DeductForBetAsync method signatures

--- MatchFixer.Core/Contracts/IWalletService.cs
+++ MatchFixer.Core/Contracts/IWalletService.cs
@@ -6,11 +6,14 @@ namespace MatchFixer.Core.Contracts
 	public interface IWalletService
 	{
 		Task<bool> HasWalletAsync();
-		Task<Wallet> GetWalletAsync(); 
-		Task<WalletViewModel> GetWalletViewModelAsync();
+		Task<Wallet> GetWalletAsync();
+		Task<WalletViewModel> GetWalletViewModelAsync(string timeZoneId);
 		Task<Wallet> CreateWalletAsync();
 		Task DepositAsync(decimal amount, string description = null);
 		Task<bool> WithdrawAsync(decimal amount, string description = null);
 		Task<(bool Success, string Message)> ClearTransactionHistoryAsync();
+		Task<(bool Success, string Message)> DeductForBetAsync(Guid userId, decimal amount);
+
+
 	}
 }
