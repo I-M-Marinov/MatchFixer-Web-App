f82a186792b5ba7315a0bf4950a3b1274f4e4d01
Ivan Marinov <134225981+I-M-Marinov@users.noreply.github.com>
2025-06-26 14:58:46 +0300

    Added the UserTimezoneKey in the SessionConstants static class

--- MatchFixer.Common/GeneralConstants/SessionConstants.cs
+++ MatchFixer.Common/GeneralConstants/SessionConstants.cs
@@ -3,6 +3,7 @@
 	public static class SessionConstants
 	{
 		public const string GameSessionName = "MatchFixGuessGameSessionKey";
-		public const string BetSlipSessionName = "BetSlipSession"; 
+		public const string BetSlipSessionName = "BetSlipSession";
+		public const string UserTimezoneKey = "UserTimezone";
 	}
 }
