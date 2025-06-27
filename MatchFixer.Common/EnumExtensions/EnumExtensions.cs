f1a325e151453f9535ec6549014a31d36f20885c
I-M-Marinov <lcfrrr@gmail.com>
2025-06-27 19:57:07 +0300

    Created the EnumExtensions static class with method ToDisplayName to format Enums ( specifically WalletTransactionType enums and more specifically "BetPlaced" )

---
+++ MatchFixer.Common/EnumExtensions/EnumExtensions.cs
@@ -0,0 +1,21 @@
+﻿
+
+namespace MatchFixer.Common.EnumExtensions
+{
+	public static class EnumExtensions
+	{
+		public static string ToDisplayName(this Enum value)
+		{
+			var text = value.ToString();
+			if (string.IsNullOrWhiteSpace(text))
+				return string.Empty;
+
+			return System.Text.RegularExpressions.Regex.Replace(
+				text,
+				"([a-z])([A-Z])",
+				"$1 $2"
+			);
+		}
+	}
+
+}
