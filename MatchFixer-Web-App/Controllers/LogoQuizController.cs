9774a3e7b68482e760950aea3bd7e4f48b3ce606
I-M-Marinov <lcfrrr@gmail.com>
2025-06-27 23:24:16 +0300

    Created the LogoQuizController

---
+++ MatchFixer-Web-App/Controllers/LogoQuizController.cs
@@ -0,0 +1,31 @@
+﻿using MatchFixer.Core.Contracts;
+using MatchFixer.Core.ViewModels.LogoQuiz;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MatchFixer_Web_App.Controllers
+{
+	public class LogoQuizController : Controller
+	{
+		private readonly ILogoQuizService _logoQuizService;
+
+		public LogoQuizController(ILogoQuizService logoQuizService)
+		{
+			_logoQuizService = logoQuizService;
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> LogoQuiz()
+		{
+			var question = await _logoQuizService.GenerateQuestionAsync();
+			return View(question);
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> LogoQuiz(LogoQuizQuestionViewModel model)
+		{
+			model.IsCorrect = model.SelectedAnswer == model.CorrectAnswer;
+			return View(model); // Show feedback
+		}
+	}
+}
