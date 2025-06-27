2d8b4b42da711e7cb36c8d734ff16485303b94a6
I-M-Marinov <lcfrrr@gmail.com>
2025-06-27 23:25:23 +0300

    Created the ILogoQuizService interface

---
+++ MatchFixer.Core/Contracts/ILogoQuizService.cs
@@ -0,0 +1,9 @@
+﻿using MatchFixer.Core.ViewModels.LogoQuiz;
+
+namespace MatchFixer.Core.Contracts
+{
+	public interface ILogoQuizService
+	{
+		Task<LogoQuizQuestionViewModel> GenerateQuestionAsync();
+	}
+}
