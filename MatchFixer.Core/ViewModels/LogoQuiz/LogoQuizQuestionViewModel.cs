5f154488b664e6e81d3b5b9e1e56c431b93bad0b
I-M-Marinov <lcfrrr@gmail.com>
2025-06-27 23:26:03 +0300

    Created the LogoQuizQuestionViewModel

---
+++ MatchFixer.Core/ViewModels/LogoQuiz/LogoQuizQuestionViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace MatchFixer.Core.ViewModels.LogoQuiz
+{
+	public class LogoQuizQuestionViewModel
+	{
+		public string LogoUrl { get; set; } = null!;
+		public string CorrectAnswer { get; set; } = null!;
+		public List<string> Options { get; set; } = new();
+		public string? SelectedAnswer { get; set; } 
+		public bool? IsCorrect { get; set; }
+	}
+
+}
