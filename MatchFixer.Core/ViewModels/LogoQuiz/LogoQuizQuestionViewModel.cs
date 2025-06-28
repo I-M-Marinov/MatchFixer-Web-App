d3fde129f5c76ecfc7773e344d74de79c144dad7
I-M-Marinov <lcfrrr@gmail.com>
2025-06-28 19:51:09 +0300

    Added the OriginalOptions list to the LogoQuizQuestionViewModel

--- MatchFixer.Core/ViewModels/LogoQuiz/LogoQuizQuestionViewModel.cs
+++ MatchFixer.Core/ViewModels/LogoQuiz/LogoQuizQuestionViewModel.cs
@@ -4,6 +4,7 @@
 	{
 		public string LogoUrl { get; set; } = null!;
 		public string CorrectAnswer { get; set; } = null!;
+		public List<string> OriginalOptions { get; set; } = new();
 		public List<string> Options { get; set; } = new();
 		public string? SelectedAnswer { get; set; } 
 		public bool? IsCorrect { get; set; }
