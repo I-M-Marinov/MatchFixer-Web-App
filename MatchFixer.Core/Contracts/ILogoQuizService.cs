74fd785c95c5f277586f4e3adb993ce7d954a0f9
I-M-Marinov <lcfrrr@gmail.com>
2025-06-28 23:47:09 +0300

    -- Updated the signature of the GenerateQuestionAsync method to accept currentScore integer
    -- Added the UpdateLogoQuizScoreAsync, accepting the userId and a boolean signifying if the answer given by the user is correct or not

--- MatchFixer.Core/Contracts/ILogoQuizService.cs
+++ MatchFixer.Core/Contracts/ILogoQuizService.cs
@@ -4,8 +4,8 @@ namespace MatchFixer.Core.Contracts
 {
 	public interface ILogoQuizService
 	{
-		Task<LogoQuizQuestionViewModel> GenerateQuestionAsync();
-
+		Task<LogoQuizQuestionViewModel> GenerateQuestionAsync(int currentScore);
 		LogoQuizQuestionViewModel BuildAnsweredModel(string selectedAnswer, string correctAnswer, string logoUrl, List<string> originalOptions);
+		Task<(string Message, int NewScore)> UpdateLogoQuizScoreAsync(Guid userId, bool isCorrect);
 	}
 }
