730e8284bea77d39d5769901b6e5e77dcf0fd2ce
I-M-Marinov <lcfrrr@gmail.com>
2025-06-28 23:50:09 +0300

    Updated the LogoQuizController:
    -- Injected the userContextService through the constructor
    
    -- Updated the LogoQuiz POST method

--- MatchFixer-Web-App/Controllers/LogoQuizController.cs
+++ MatchFixer-Web-App/Controllers/LogoQuizController.cs
@@ -6,24 +6,40 @@ namespace MatchFixer_Web_App.Controllers
 	public class LogoQuizController : Controller
 	{
 		private readonly ILogoQuizService _logoQuizService;
+		private readonly IUserContextService _userContextService;
 
-		public LogoQuizController(ILogoQuizService logoQuizService)
+		public LogoQuizController(ILogoQuizService logoQuizService, IUserContextService userContextService)
 		{
 			_logoQuizService = logoQuizService;
+			_userContextService = userContextService;
 		}
 
 		[HttpGet]
-		public async Task<IActionResult> LogoQuiz()
+		public async Task<IActionResult> LogoQuiz(int currentScore = 0)
 		{
-			var question = await _logoQuizService.GenerateQuestionAsync();
+			var question = await _logoQuizService.GenerateQuestionAsync(currentScore);
 			return View(question);
 		}
 
 		[HttpPost]
 		[ValidateAntiForgeryToken]
-		public IActionResult LogoQuiz(string SelectedAnswer, string CorrectAnswer, string LogoUrl, List<string> OriginalOptions)
+		public async Task<IActionResult> LogoQuiz(string SelectedAnswer, string CorrectAnswer, string LogoUrl, List<string> OriginalOptions)
 		{
+			var userId = _userContextService.GetUserId();
 			var model = _logoQuizService.BuildAnsweredModel(SelectedAnswer, CorrectAnswer, LogoUrl, OriginalOptions);
+
+			var (pointsMessage, updatedScore) = await _logoQuizService.UpdateLogoQuizScoreAsync(userId, model.IsCorrect == true);
+			model.CurrentScore = updatedScore;
+
+			if (model.IsCorrect == true)
+			{
+				TempData["SuccessMessage"] = pointsMessage; // Store success message
+			}
+			else
+			{
+				TempData["ErrorMessage"] = pointsMessage; // Store error or no-change
+			}
+
 			return View(model);
 		}
 	}
