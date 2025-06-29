39df23bd7b2df046cec14d757da73e00e41e4b54
I-M-Marinov <lcfrrr@gmail.com>
2025-06-29 21:32:53 +0300

    Updated the SessionInitializationMiddleware:
    -- Skip middleware for login, register, logout, static files
    
    -- Check if the session contains the UserTimezone, if not that means the session expired, logout the user and redirect him to the login page

--- MatchFixer.Core/Middlewares/SessionInitializationMiddleware.cs
+++ MatchFixer.Core/Middlewares/SessionInitializationMiddleware.cs
@@ -1,7 +1,9 @@
 ﻿using MatchFixer.Infrastructure.Entities;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 
+using static MatchFixer.Common.GeneralConstants.SessionConstants;
 
 namespace MatchFixer.Core.Middlewares
 {
@@ -16,6 +18,23 @@ namespace MatchFixer.Core.Middlewares
 
 		public async Task InvokeAsync(HttpContext context, UserManager<ApplicationUser> userManager)
 		{
+			// Skip middleware for login, register, logout, static files, etc.
+			var path = context.Request.Path.Value;
+
+			if (string.IsNullOrEmpty(path) ||
+			    (path.StartsWith("/Identity/Account/Login", StringComparison.OrdinalIgnoreCase) && context.Request.Method.Equals("POST", StringComparison.OrdinalIgnoreCase)) ||
+			    path.StartsWith("/Identity/Account/Register", StringComparison.OrdinalIgnoreCase) ||
+			    path.StartsWith("/Identity/Account/Logout", StringComparison.OrdinalIgnoreCase) ||
+			    path.StartsWith("/favicon", StringComparison.OrdinalIgnoreCase) ||
+			    path.StartsWith("/lib", StringComparison.OrdinalIgnoreCase) ||
+			    path.StartsWith("/css", StringComparison.OrdinalIgnoreCase) ||
+			    path.StartsWith("/js", StringComparison.OrdinalIgnoreCase) ||
+			    path.StartsWith("/images", StringComparison.OrdinalIgnoreCase))
+			{
+				await _next(context);
+				return;
+			}
+
 			if (context.User.Identity.IsAuthenticated)
 			{
 				var user = await userManager.GetUserAsync(context.User);
@@ -24,20 +43,29 @@ namespace MatchFixer.Core.Middlewares
 					var currentSessionUserId = context.Session.GetString("UserId");
 
 					// reset the currently set currentSessionUserId if it is different from the user id of the user logged in now 
-					if (currentSessionUserId == null || currentSessionUserId != user.Id.ToString())
+					if (string.IsNullOrEmpty(currentSessionUserId) || currentSessionUserId != user.Id.ToString())
 					{
 						context.Session.SetString("UserId", user.Id.ToString());
+					}
+
+					if (string.IsNullOrEmpty(context.Session.GetString(UserTimezoneKey)))
+					{
+						// session expired
+						await context.SignOutAsync(IdentityConstants.ApplicationScheme);
+						context.Response.Redirect("/Identity/Account/Login?sessionExpired=true");
+						return;
 					}
 				}
 			}
 			else
 			{
-				// Clear session on logout
-				context.Session.Remove("UserId");
+				// clear on logout
+				context.Session.Clear();
 			}
 
 			await _next(context);
 		}
+
 
 	}
 }
