a4a0ed811075a1baeddf95a4428e9a30e6e723fc
I-M-Marinov <lcfrrr@gmail.com>
2025-06-29 21:34:50 +0300

    Added the GetCurrentUserAsync signature to the IUserContextService interface

--- MatchFixer.Core/Contracts/IUserContextService.cs
+++ MatchFixer.Core/Contracts/IUserContextService.cs
@@ -1,7 +1,10 @@
-﻿namespace MatchFixer.Core.Contracts
+﻿using MatchFixer.Infrastructure.Entities;
+
+namespace MatchFixer.Core.Contracts
 {
 	public interface IUserContextService
 	{
 		Guid GetUserId();
+		Task<ApplicationUser?> GetCurrentUserAsync();
 	}
 }
