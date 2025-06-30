a04f778fb9620ac9dad61510857abd3b640a7a61
I-M-Marinov <lcfrrr@gmail.com>
2025-06-30 11:38:10 +0300

    Created the MatchResultInputViewModel

---
+++ MatchFixer.Core/ViewModels/MatchResults/MatchResultInputViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MatchFixer.Core.ViewModels.MatchResults
+{
+	public class MatchResultInputViewModel
+	{
+		public Guid MatchId { get; set; }
+
+		public string HomeTeam { get; set; }
+		public string AwayTeam { get; set; }
+
+		public DateTime MatchDate { get; set; }
+
+		[Required]
+		[Range(0, 20)]
+		public int HomeScore { get; set; }
+
+		[Required]
+		[Range(0, 20)]
+		public int AwayScore { get; set; }
+
+		public string? Notes { get; set; }
+	}
+}
