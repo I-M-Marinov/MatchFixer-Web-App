a0457b39278d18f768367ea103e5e3483f843787
I-M-Marinov <lcfrrr@gmail.com>
2025-06-30 11:42:43 +0300

    Created the ILiveMatchResultService interface
    -- AddMatchResultAsync and GetUnresolvedMatchResultsAsync ( signatures )

---
+++ MatchFixer.Core/Contracts/ILiveMatchResultService.cs
@@ -0,0 +1,11 @@
+﻿using MatchFixer.Core.ViewModels.MatchResults;
+
+namespace MatchFixer.Core.Contracts
+{
+	public interface ILiveMatchResultService
+	{
+		Task<bool> AddMatchResultAsync(Guid matchEventId, int homeScore, int awayScore, string? notes = null);
+		Task<List<MatchResultInputViewModel>> GetUnresolvedMatchResultsAsync();
+	}
+
+}
