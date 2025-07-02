ad0a70be80323d4b45c607113361dd0fa856f81b
I-M-Marinov <lcfrrr@gmail.com>
2025-07-02 12:53:07 +0300

    Created the MatchEventConstants static class to hold all the string constants that are needed in the MatchEvent Service

---
+++ MatchFixer.Common/GeneralConstants/MatchEventConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace MatchFixer.Common.GeneralConstants
+{
+	public static class MatchEventConstants
+	{
+		public const string TeamDoesNotExist = "Home or Away team does not exist!";
+
+	}
+}
