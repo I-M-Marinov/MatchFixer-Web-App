99c5b254da9a924f31e2b39f70173c56f5c6e044
Ivan Marinov <134225981+I-M-Marinov@users.noreply.github.com>
2025-07-04 13:11:01 +0300

    Added additional validation and DisplayTime property to the MatchResultInputViewModel

--- MatchFixer.Core/ViewModels/MatchResults/MatchResultInputViewModel.cs
+++ MatchFixer.Core/ViewModels/MatchResults/MatchResultInputViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.Identity.Client;
 
 namespace MatchFixer.Core.ViewModels.MatchResults
 {
@@ -6,17 +7,22 @@ namespace MatchFixer.Core.ViewModels.MatchResults
 	{
 		public Guid MatchId { get; set; }
 
-		public string HomeTeam { get; set; }
-		public string AwayTeam { get; set; }
+		[Required] 
+		public string HomeTeam { get; set; } = null!;
+
+		[Required]
+		public string AwayTeam { get; set; } = null!;
 
 		public DateTime MatchDate { get; set; }
 
+		public string DisplayTime { get; set; } = null!;
+
 		[Required]
-		[Range(0, 20)]
+		[Range(0, 20, ErrorMessage = "Score must be between 0 and 20.")]
 		public int HomeScore { get; set; }
 
 		[Required]
-		[Range(0, 20)]
+		[Range(0, 20, ErrorMessage = "Score must be between 0 and 20.")]
 		public int AwayScore { get; set; }
 
 		public string? Notes { get; set; }
