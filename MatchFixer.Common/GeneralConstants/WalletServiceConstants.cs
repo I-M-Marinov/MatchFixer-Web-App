26bfd3306519d94a68173dd77b57462093bbad05
I-M-Marinov <lcfrrr@gmail.com>
2025-07-10 23:11:23 +0300

    Created the WalletServiceConstants static class to hold all the messages that the WalletService uses

---
+++ MatchFixer.Common/GeneralConstants/WalletServiceConstants.cs
@@ -0,0 +1,16 @@
+﻿namespace MatchFixer.Common.GeneralConstants
+{
+	public static class WalletServiceConstants
+	{
+		public const string MoneyAmountMustBePositive = "Amount must be positive.";
+		public const string WalletNotFound = "Wallet not found.";
+		public const string NoTransactionsToClear = "No transactions to clear.";
+		public const string TransactionHistoryCleared = "Transaction history was cleared.";
+		public const string InsufficientBalanceToPlaceBet = "Insufficient balance to place the bet.";
+
+		public const string WinAmountMustBeGreaterThanZero = "Amount won must be greater than zero.";
+		public const string AmountDeductedForTheBet = "The amount was deducted for the bet.";
+
+
+	}
+}
