c105edce2c4a684591470744a98759dd63a2a55b
I-M-Marinov <lcfrrr@gmail.com>
2025-07-14 23:07:52 +0300

    Added the following signatures to the MatchEvent Service Interface --->
    -- GetAllEventsAsync
    -- EditMatchEventAsync
    -- CancelMatchEventAsync

--- MatchFixer.Core/Contracts/IMatchEventService.cs
+++ MatchFixer.Core/Contracts/IMatchEventService.cs
@@ -1,18 +1,17 @@
 ﻿using MatchFixer.Core.ViewModels.LiveEvents;
 using Microsoft.AspNetCore.Mvc.Rendering;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace MatchFixer.Core.Contracts
 {
 	public interface IMatchEventService
 	{
+		Task<List<LiveEventViewModel>> GetAllEventsAsync();
 		Task<List<LiveEventViewModel>> GetLiveEventsAsync();
 		Task AddEventAsync(MatchEventFormModel model);
 		Task<string> GetTeamLogo(string name);
 		Task<Dictionary<string, List<SelectListItem>>> GetTeamsGroupedByLeagueAsync();
+		Task<bool> EditMatchEventAsync(Guid matchEventId, decimal homeOdds, decimal drawOdds, decimal awayOdds, DateTime kickoffTime);
+		Task<bool> CancelMatchEventAsync(Guid matchEventId);
 	}
 }
