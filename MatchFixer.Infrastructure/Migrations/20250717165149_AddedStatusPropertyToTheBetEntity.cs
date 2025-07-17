5ddc213147d68e8fcfc0dcbfc3ff46b9d32c6299
I-M-Marinov <lcfrrr@gmail.com>
2025-07-17 23:32:58 +0300

    Added the Status Property to the Bet entity, created a migration and updated the DB

---
+++ MatchFixer.Infrastructure/Migrations/20250717165149_AddedStatusPropertyToTheBetEntity.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MatchFixer.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedStatusPropertyToTheBetEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "Bets",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                comment: "Status of the bet");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Bets");
+        }
+    }
+}
