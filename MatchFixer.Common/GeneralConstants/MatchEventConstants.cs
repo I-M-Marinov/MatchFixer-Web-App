5a766bd36dbf09b930d58902450ff0c3a88cfd85
I-M-Marinov <lcfrrr@gmail.com>
2025-07-18 22:46:53 +0300

    Added const strings to the MatchEventConstants

--- MatchFixer.Common/GeneralConstants/MatchEventConstants.cs
+++ MatchFixer.Common/GeneralConstants/MatchEventConstants.cs
@@ -3,6 +3,12 @@
 	public static class MatchEventConstants
 	{
 		public const string TeamDoesNotExist = "Home or Away team does not exist!";
+		public const string FailedToGetLiveEvents = "Failed to get the live events!";
 
+		public const string MatchUpdateFailed = "Failed to edit the match. It might not exist or is already cancelled.";
+		public const string MatchUpdateSuccessful = "Match updated successfully.";
+
+		public const string EventCancellationUnsuccessful = "There was an error cancelling this event";
+		public const string EventCancellationSuccessful = "Event cancelled successfully.";
 	}
 }
