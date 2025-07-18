112e2c959da361bb39f8b70f2128bddab5451d0a
I-M-Marinov <lcfrrr@gmail.com>
2025-07-18 23:51:57 +0300

    Created the BettingServiceConstants static class to hold all the error and success message for the Betting Service

---
+++ MatchFixer.Common/GeneralConstants/BettingServiceConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace MatchFixer.Common.GeneralConstants
+{
+	public static class BettingServiceConstants
+	{
+		public const string NoBetsProvided = "No bets provided.";
+		public const string BetAmountMustBeGreaterThanZero = "Bet amount must be greater than zero.";
+		public const string EventAlreadyStartedInSlip = "An event or events in your slip already started ! Choose other events to bet on.";
+		public const string BetSlipSubmittedSuccessfully = "Your betting slip was successfully submitted! Good Luck!";
+
+	}
+}
