6ba16119faf1f442cea30ab4abc1e3ed2b5fc743
I-M-Marinov <lcfrrr@gmail.com>
2025-07-21 17:14:08 +0300

    Created the EmailMessages static class to hold all messages that would be sent to the users when an email is sent via email

---
+++ MatchFixer.Common/EmailTemplates/EmailMessages.cs
@@ -0,0 +1,9 @@
+﻿namespace MatchFixer.Common.EmailTemplates
+{
+	public static class EmailMessages
+	{
+		public const string ConfirmationEmailSent = "We have sent you a confirmation email. Please confirm your account.";
+		public const string VerificationEmailResent = "Verification email was resent. Please check your email.";
+		public const string PasswordLinkSentToSpecifiedEmail = "An email with a reset link was sent to the specified email.";
+	}
+}
