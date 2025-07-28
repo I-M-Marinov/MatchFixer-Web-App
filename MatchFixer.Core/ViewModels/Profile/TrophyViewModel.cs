ef7589122f175710dbfdffbcfbc6da7e2abf92b1
I-M-Marinov <lcfrrr@gmail.com>
2025-07-28 23:00:46 +0300

    Added the TrophyViewModel

---
+++ MatchFixer.Core/ViewModels/Profile/TrophyViewModel.cs
@@ -0,0 +1,31 @@
+﻿
+using MatchFixer.Common.Enums;
+
+namespace MatchFixer.Core.ViewModels.Profile
+{
+	public class TrophyViewModel
+	{
+		public int TrophyId { get; set; }
+
+		public string Name { get; set; } = string.Empty;
+
+		public string IconUrl { get; set; } = "";  // default fallback
+
+		public string Description { get; set; } = string.Empty;
+
+		public string Type { get; set; } = string.Empty; // Example: "Milestone", "Time-Based", etc.
+
+		public TrophyLevel Level { get; set; } = TrophyLevel.Bronze;
+
+		public bool IsEarned { get; set; } = false; // Indicates if the current user has earned it
+
+		public DateTime? AwardedOn { get; set; }  // null if not earned yet
+
+		public bool IsHiddenUntilEarned { get; set; } = false;
+
+		public DateTime? ExpirationDate { get; set; }
+
+		public string? Notes { get; set; }
+
+	}
+}
