8dec782db1df951f281914edaafb90d6775cf71c
I-M-Marinov <lcfrrr@gmail.com>
2025-07-28 23:01:43 +0300

    Added the TrophyType enum

---
+++ MatchFixer.Common/Enums/TrophyType.cs
@@ -0,0 +1,10 @@
+﻿namespace MatchFixer.Common.Enums
+{
+	public enum TrophyType
+	{
+		Milestone,      // e.g., 100 bets
+		TimeBased,      // e.g., Night Owl
+		SpecialEvent,   // e.g., Christmas Bet 2025
+		Streak          // e.g., 10 consecutive wins
+	}
+}
