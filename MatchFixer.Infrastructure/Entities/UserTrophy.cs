4931c185f2863a64ca3fb64599b2561b46518a7d
I-M-Marinov <lcfrrr@gmail.com>
2025-07-28 23:00:35 +0300

    Added the UserTrophy entity

---
+++ MatchFixer.Infrastructure/Entities/UserTrophy.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MatchFixer.Infrastructure.Entities
+{
+	public class UserTrophy
+	{
+		public int Id { get; set; }
+
+		public Guid UserId { get; set; }
+		[ForeignKey(nameof(UserId))]
+		public ApplicationUser User { get; set; }  
+
+		public int TrophyId { get; set; }
+		[ForeignKey(nameof(TrophyId))]
+		public Trophy Trophy { get; set; }  
+
+		public DateTime AwardedOn { get; set; } = DateTime.UtcNow; // set the time at now UTC 
+
+		[StringLength(500, ErrorMessage = "Notes cannot exceed 500 characters.")] 
+		public string? Notes { get; set; }
+	}
+}
