3f7965fe4535a53e4481c58294d3ed04d4039c42
I-M-Marinov <lcfrrr@gmail.com>
2025-07-29 23:13:20 +0300

    Created the SeedMilestoneTrophiesAsync static method that would seed the DB with the Milestone Trophies ( if they are not already seeded )

--- MatchFixer.Infrastructure/SeedData/SeedData.cs
+++ MatchFixer.Infrastructure/SeedData/SeedData.cs
@@ -1,17 +1,76 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.Extensions.DependencyInjection;
-
+﻿using MatchFixer.Common.Enums;
 using MatchFixer.Infrastructure.Entities;
-
-using static MatchFixer.Common.GeneralConstants.ProfilePictureConstants;
 using MatchFixer.Infrastructure.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using static MatchFixer.Common.GeneralConstants.ProfilePictureConstants;
 
 
 namespace MatchFixer.Infrastructure.SeedData
 {
 	public class SeedData
 	{
+		public static async Task SeedMilestoneTrophiesAsync(IServiceProvider serviceProvider)
+		{
+			var dbContext = serviceProvider.GetRequiredService<MatchFixerDbContext>();
+
+			var milestoneTrophies = new List<Trophy>
+			{
+				new Trophy
+				{
+					Name = "Rookie Rigger",
+					Description = "Place your first bet",
+					Type = TrophyType.Milestone,
+					Level = TrophyLevel.Bronze,
+					MilestoneTarget = 1,
+					IconUrl = "/images/trophies/rookier-rigger.png",
+					IsHiddenUntilEarned = false
+				},
+				new Trophy
+				{
+					Name = "Fixer's Dozen",
+					Description = "Place 100 total bets",
+					Type = TrophyType.Milestone,
+					Level = TrophyLevel.Silver,
+					MilestoneTarget = 100,
+					IconUrl = "/images/trophies/fixers-dozen.png",
+					IsHiddenUntilEarned = false
+				},
+				new Trophy
+				{
+					Name = "Bet Syndicate Boss",
+					Description = "Wager a total of 2000 euros",
+					Type = TrophyType.Milestone,
+					Level = TrophyLevel.Gold,
+					MilestoneTarget = 2000, 
+	                IconUrl = "/images/trophies/bet-syndicate-boss.png",
+					IsHiddenUntilEarned = false
+				},
+				new Trophy
+				{
+					Name = "Ultimate Grinder",
+					Description = "Place 1000 total bets",
+					Type = TrophyType.Milestone,
+					Level = TrophyLevel.Platinum,
+					MilestoneTarget = 1000,
+					IconUrl = "/images/trophies/ultimate-grinder.png",
+					IsHiddenUntilEarned = false
+				}
+			};
+
+			foreach (var trophy in milestoneTrophies)
+			{
+				bool exists = await dbContext.Trophies.AnyAsync(t => t.Name == trophy.Name);
+				if (!exists)
+				{
+					dbContext.Trophies.Add(trophy);
+				}
+			}
+
+			await dbContext.SaveChangesAsync();
+		}
 		public static async Task SeedDefaultProfilePicture(UserManager<ApplicationUser> userManager, IServiceProvider serviceProvider)
 		{
 			var dbContext = serviceProvider.GetRequiredService<MatchFixerDbContext>();
