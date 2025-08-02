cf9fba97f4606843f0de30519ba4658728bcb95b
I-M-Marinov <lcfrrr@gmail.com>
2025-08-02 23:43:12 +0300

    Created the TrophyNames static class to hold the names of all trophies

---
+++ MatchFixer.Common/GeneralConstants/TrophyNames.cs
@@ -0,0 +1,24 @@
+﻿namespace MatchFixer.Common.GeneralConstants
+{
+	public static class TrophyNames
+	{
+		// Milestone Trophies
+		public const string RookieRigger = "Rookie Rigger";
+		public const string FixersDozen = "Fixer's Dozen";
+		public const string BetSyndicateBoss = "Bet Syndicate Boss";
+		public const string UltimateGrinder = "Ultimate Grinder";
+
+		// Time-Based Trophies
+		public const string FixerAtDawn = "Fixer at Dawn";
+		public const string MidnightFixer = "Midnight Fixer";
+		public const string WeekendWagerWarlord = "Weekend Wager Warlord";
+		public const string LastMinuteLeak = "Last-Minute Leak";
+
+		// Special Event Trophies
+		public const string SilentBetShadyNight = "Silent Bet, Shady Night";
+		public const string FixmasMiracle = "Fixmas Miracle";
+		public const string NewYearNewFix = "New Year, New Fix";
+		public const string OneYearManyFixes = "One Year, Many Fixes";
+		public const string SummerSyndicateSlam = "Summer Syndicate Slam";
+	}
+}
