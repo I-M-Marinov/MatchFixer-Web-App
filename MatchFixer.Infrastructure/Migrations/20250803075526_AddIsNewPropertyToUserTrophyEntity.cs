0c4d161cedd0b2d7b5ca6e3d97e974dc2fcd2da5
I-M-Marinov <lcfrrr@gmail.com>
2025-08-03 21:18:40 +0300

    Added the IsNew property to the UserTrophy Entity in the DB

---
+++ MatchFixer.Infrastructure/Migrations/20250803075526_AddIsNewPropertyToUserTrophyEntity.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MatchFixer.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsNewPropertyToUserTrophyEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsNew",
+                table: "UserTrophies",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsNew",
+                table: "UserTrophies");
+        }
+    }
+}
