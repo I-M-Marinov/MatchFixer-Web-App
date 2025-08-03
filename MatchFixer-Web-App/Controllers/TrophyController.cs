4c4ac3aed1e0f0372a53a4dc7a3fda54eff51d3a
I-M-Marinov <lcfrrr@gmail.com>
2025-08-03 21:17:58 +0300

    Created the Trophy Controller

---
+++ MatchFixer-Web-App/Controllers/TrophyController.cs
@@ -0,0 +1,34 @@
+﻿using MatchFixer.Core.Contracts;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MatchFixer_Web_App.Controllers
+{
+	public class TrophyController : Controller
+	{
+		private readonly IUserContextService _userContextService;
+		private readonly ITrophyService _trophyService;
+
+
+		public TrophyController(IUserContextService userContextService, ITrophyService trophyService)
+		{
+			_userContextService = userContextService;
+			_trophyService = trophyService;
+		}
+
+
+		[Authorize]
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> MarkAsSeen(int trophyId)
+		{
+			var userId = _userContextService.GetUserId();
+			if (userId == Guid.Empty)
+				return Unauthorized();
+
+			await _trophyService.MarkTrophyAsSeenAsync(userId, trophyId);
+			return Ok();
+		}
+
+	}
+}
