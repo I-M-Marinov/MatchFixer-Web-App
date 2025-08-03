368351ec07ce0380da25218b9a54837b5a5f84ef
I-M-Marinov <lcfrrr@gmail.com>
2025-08-03 22:26:56 +0300

    Added the MarkTrophyAsSeenAsync signature to the ITrophyService interface

--- MatchFixer.Core/Contracts/ITrophyService.cs
+++ MatchFixer.Core/Contracts/ITrophyService.cs
@@ -4,5 +4,6 @@
 	{
 		Task AwardTrophyIfNotAlreadyAsync(Guid userId, int trophyId, string? notes = null);
 		Task EvaluateTrophiesAsync(Guid userId);
+		Task MarkTrophyAsSeenAsync(Guid userId, int trophyId);
 	}
 }
