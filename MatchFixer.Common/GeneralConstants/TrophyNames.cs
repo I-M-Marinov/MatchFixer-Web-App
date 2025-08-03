755d6127944afb28505814b908996d7ee76d7be9
I-M-Marinov <lcfrrr@gmail.com>
2025-08-03 22:27:48 +0300

    Added the Outcome Trophies' names  and the initial trophy name ( ShadowSyndicateRecruit ) to the TrophyNames static class

--- MatchFixer.Common/GeneralConstants/TrophyNames.cs
+++ MatchFixer.Common/GeneralConstants/TrophyNames.cs
@@ -2,6 +2,9 @@
 {
 	public static class TrophyNames
 	{
+		// Trophy for just creating an account
+		public const string ShadowSyndicateRecruit = "Shadow Syndicate Recruit";
+
 		// Milestone Trophies
 		public const string RookieRigger = "Rookie Rigger";
 		public const string FixersDozen = "Fixer's Dozen";
@@ -20,5 +23,15 @@
 		public const string NewYearNewFix = "New Year, New Fix";
 		public const string OneYearManyFixes = "One Year, Many Fixes";
 		public const string SummerSyndicateSlam = "Summer Syndicate Slam";
+
+		// Outcome Trophies
+		public const string FixersHotStreak = "Fixer's Hot Streak";
+		public const string SyndicateSharpshooter = "Syndicate Sharp Shooter";
+		public const string RiggedToWin = "Rigged To Win";
+		public const string FixGoneWrong = "Fix Gone Wrong";
+		public const string UnluckySyndicateSoldier = "Unlucky Syndicate Soldier";
+		public const string BankrollObliterator = "Bankroll Obliterator";
+		public const string ComebackKingpin = "Comeback Kingpin";
+		public const string RollercoasterRigger = "Rollercoaster Rigger";
 	}
 }
