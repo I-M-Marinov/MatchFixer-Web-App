bc7b84d5d796886bd51a8ebbf26738e04545a870
I-M-Marinov <lcfrrr@gmail.com>
2025-08-04 16:52:28 +0300

    Added the signature for the GetAllTrophiesWithUserStatusAsync method in the ITrophyService interface

--- MatchFixer.Core/Contracts/ITrophyService.cs
+++ MatchFixer.Core/Contracts/ITrophyService.cs
@@ -1,7 +1,10 @@
-﻿namespace MatchFixer.Core.Contracts
+﻿using MatchFixer.Core.ViewModels.Profile;
+
+namespace MatchFixer.Core.Contracts
 {
 	public interface ITrophyService
 	{
+		Task<List<TrophyViewModel>> GetAllTrophiesWithUserStatusAsync(Guid userId);
 		Task AwardTrophyIfNotAlreadyAsync(Guid userId, int trophyId, string? notes = null);
 		Task EvaluateTrophiesAsync(Guid userId);
 		Task MarkTrophyAsSeenAsync(Guid userId, int trophyId);
