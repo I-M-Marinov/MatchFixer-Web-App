98e8b4bd8d56a4e951ffa8871b67115cae8579cc
I-M-Marinov <lcfrrr@gmail.com>
2025-08-10 17:14:25 +0300

    Added the profileUrl to the EvaluateTrophiesAsync signature in the trophy service interface

--- MatchFixer.Core/Contracts/ITrophyService.cs
+++ MatchFixer.Core/Contracts/ITrophyService.cs
@@ -5,8 +5,8 @@ namespace MatchFixer.Core.Contracts
 	public interface ITrophyService
 	{
 		Task<List<TrophyViewModel>> GetAllTrophiesWithUserStatusAsync(Guid userId);
-		Task AwardTrophyIfNotAlreadyAsync(Guid userId, int trophyId, string? notes = null);
-		Task EvaluateTrophiesAsync(Guid userId);
+		Task AwardTrophyIfNotAlreadyAsync(Guid userId, int trophyId, string profileUrl, string? notes = null);
+		Task EvaluateTrophiesAsync(Guid userId, string profileUrl);
 		Task MarkTrophyAsSeenAsync(Guid userId, int trophyId);
 	}
 }
