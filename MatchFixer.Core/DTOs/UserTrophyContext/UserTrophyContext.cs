e126a07919ed6611afdba3527d2542b406621d05
I-M-Marinov <lcfrrr@gmail.com>
2025-08-10 17:14:44 +0300

    Created the UserTrophyContext

---
+++ MatchFixer.Core/DTOs/UserTrophyContext/UserTrophyContext.cs
@@ -0,0 +1,17 @@
+﻿using MatchFixer.Infrastructure.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace MatchFixer.Core.DTOs.UserTrophyContext
+{
+	public class UserTrophyContext
+	{
+		public List<Bet> UserBets { get; set; }
+		public Guid UserId { get; set; }
+		public int TotalBets { get; set; }
+		public decimal TotalWagered { get; set; }
+		public ApplicationUser User { get; set; }
+		public DateTime Now { get; set; }
+		public DbContext DbContext { get; set; }
+		
+	}
+}
