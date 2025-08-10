59af3d52b3fd1dfc297aceb80dd541c002a05288
I-M-Marinov <lcfrrr@gmail.com>
2025-08-10 17:11:52 +0300

    Updated the signature of the PlaceBetAsync in the betting service interface

--- MatchFixer.Core/Contracts/IBettingService.cs
+++ MatchFixer.Core/Contracts/IBettingService.cs
@@ -5,7 +5,7 @@ namespace MatchFixer.Core.Contracts
 {
 	public interface IBettingService
 	{
-		Task<(string Message, bool IsSuccess)> PlaceBetAsync(Guid userId, BetSlipDto betSlipDto);
+		Task<(string Message, bool IsSuccess)> PlaceBetAsync(Guid userId, BetSlipDto betSlipDto, string profileUrl);
 		Task<IEnumerable<UserBetSlipDTO>> GetBetsByUserAsync(Guid userId);
 		Task<bool> CancelBetsForMatchAsync(Guid matchEventId);
 		Task<bool> EvaluateBetSlipAsync(Guid betSlipId);
