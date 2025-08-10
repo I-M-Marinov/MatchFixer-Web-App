68eb96080ec597dab81817a37d060f00eb797184
I-M-Marinov <lcfrrr@gmail.com>
2025-08-10 17:11:28 +0300

    Updated the static class TrophyNames
    -- added updated image paths from Cloudinary ( cloud service )

--- MatchFixer.Common/GeneralConstants/TrophyNames.cs
+++ MatchFixer.Common/GeneralConstants/TrophyNames.cs
@@ -5,100 +5,122 @@
 		// Trophy for just creating an account
 		public const string ShadowSyndicateRecruit = "Shadow Syndicate Recruit";
 		public const string ShadowSyndicateRecruitDescription = "Welcome to the underworld — you've been initiated just by showing up.";
-		public const string ShadowSyndicateRecruitImagePath = "/images/trophies/shadow-syndicate-recruit.png";
+		// public const string ShadowSyndicateRecruitImagePath = "/images/trophies/shadow-syndicate-recruit.png";
+		public const string ShadowSyndicateRecruitImagePath = "https://res.cloudinary.com/doorb7d6i/image/upload/v1754776638/shadow-syndicate-recruit_kyrumt.png";
 
 
 		// Milestone Trophies
 		public const string RookieRigger = "Rookie Rigger";
 		public const string RookieRiggerDescription = "Every legend starts with one rigged match.";
-		public const string RookieRiggerImagePath = "/images/trophies/rookier-rigger.png";
+		// public const string RookieRiggerImagePath = "/images/trophies/rookier-rigger.png";
+		public const string RookieRiggerImagePath = "https://res.cloudinary.com/doorb7d6i/image/upload/v1754776638/rookier-rigger_zo7oqg.png";
 
 		public const string FixersDozen = "Fixer's Dozen";
 		public const string FixersDozenDescription = "You’ve hit triple digits. Fixing matches like a true pro.";
-		public const string FixersDozenImagePath = "/images/trophies/fixers-dozen.png";
+		// public const string FixersDozenImagePath = "/images/trophies/fixers-dozen.png";
+		public const string FixersDozenImagePath = "https://res.cloudinary.com/doorb7d6i/image/upload/v1754776637/fixers-dozen_klm5oh.png";
 
 		public const string BetSyndicateBoss = "Bet Syndicate Boss";
 		public const string BetSyndicateBossDescription = "Cash flows, bets grow, you run this show.";
-		public const string BetSyndicateBossImagePath = "/images/trophies/bet-syndicate-boss.png";
+		// public const string BetSyndicateBossImagePath = "/images/trophies/bet-syndicate-boss.png";
+		public const string BetSyndicateBossImagePath = "https://res.cloudinary.com/doorb7d6i/image/upload/v1754776637/bet-syndicate-boss_aeqcb8.png";
 
 		public const string UltimateGrinder = "Ultimate Grinder";
 		public const string UltimateGrinderDescription = "If placing bets were a job, you'd already be CEO of MatchFix Inc.";
-		public const string UltimateGrinderImagePath = "/images/trophies/ultimate-grinder.png";
+		// public const string UltimateGrinderImagePath = "/images/trophies/ultimate-grinder.png";
+		public const string UltimateGrinderImagePath = "https://res.cloudinary.com/doorb7d6i/image/upload/v1754776639/ultimate-grinder_i6x3m2.png";
 
 
 		// Time-Based Trophies
 		public const string FixerAtDawn = "Fixer at Dawn";
 		public const string FixerAtDawnDescription = "Up before the bookies — rigging the odds with your morning brew.";
-		public const string FixerAtDawnImagePath = "/images/trophies/fixer-at-dawn.png";
+		// public const string FixerAtDawnImagePath = "/images/trophies/fixer-at-dawn.png";
+		public const string FixerAtDawnImagePath = "https://res.cloudinary.com/doorb7d6i/image/upload/v1754776636/fixer-at-dawn_w9cu8l.png";
 
 		public const string MidnightFixer = "Midnight Fixer";
 		public const string MidnightFixerDescription = "While others sleep, you’re cooking bets in the moonlight.";
-		public const string MidnightFixerImagePath = "/images/trophies/midnight-fixer.png";
+		// public const string MidnightFixerImagePath = "/images/trophies/midnight-fixer.png";
+		public const string MidnightFixerImagePath = "https://res.cloudinary.com/doorb7d6i/image/upload/v1754776638/midnight-fixer_xmx4gf.png";
 
 		public const string WeekendWagerWarlord = "Weekend Wager Warlord";
 		public const string WeekendWagerWarlordDescription = "Weekends are for shady wagers and syndicate profits.";
-		public const string WeekendWagerWarlordImagePath = "/images/trophies/weekend-wager-warlord.png";
+		// public const string WeekendWagerWarlordImagePath = "/images/trophies/weekend-wager-warlord.png";
+		public const string WeekendWagerWarlordImagePath = "https://res.cloudinary.com/doorb7d6i/image/upload/v1754776640/weekend-wager-warlord_ljodgn.png";
 
 		public const string LastMinuteLeak = "Last-Minute Leak";
 		public const string LastMinuteLeakDescription = "Betting at the buzzer with 'insider' info in hand.";
-		public const string LastMinuteLeakImagePath = "/images/trophies/last-minute-leak.png";
+		// public const string LastMinuteLeakImagePath = "/images/trophies/last-minute-leak.png";
+		public const string LastMinuteLeakImagePath = "https://res.cloudinary.com/doorb7d6i/image/upload/v1754776637/last-minute-leak_jzly4t.png";
 
 
 		// Special Event Trophies
 		public const string SilentBetShadyNight = "Silent Bet, Shady Night";
 		public const string SilentBetShadyNightDescription = "Placed a bet on Christmas Day – even Santa can’t fix this outcome.";
-		public const string SilentBetShadyNightImagePath = "/images/trophies/silent-bet-shady-night.png";
+		// public const string SilentBetShadyNightImagePath = "/images/trophies/silent-bet-shady-night.png";
+		public const string SilentBetShadyNightImagePath = "https://res.cloudinary.com/doorb7d6i/image/upload/v1754776639/silent-bet-shady-night_zkluoq.png";
 
 		public const string FixmasMiracle = "Fixmas Miracle";
 		public const string FixmasMiracleDescription = "Placed 3 bets on Christmas Day – triple the cheer, triple the rigging.";
-		public const string FixmasMiracleImagePath = "/images/trophies/fixmas-miracle.png";
+		// public const string FixmasMiracleImagePath = "/images/trophies/fixmas-miracle.png";
+		public const string FixmasMiracleImagePath = "https://res.cloudinary.com/doorb7d6i/image/upload/v1754776637/fixmas-miracle_jfe4lh.png";
 
 		public const string NewYearNewFix = "New Year, New Fix";
 		public const string NewYearNewFixDescription = "Started the year with a wager – resolutions are temporary, betting is forever.";
-		public const string NewYearNewFixImagePath = "/images/trophies/new-year-new-fix.png";
+		// public const string NewYearNewFixImagePath = "/images/trophies/new-year-new-fix.png";
+		public const string NewYearNewFixImagePath = "https://res.cloudinary.com/doorb7d6i/image/upload/v1754776638/new-year-new-fix_knnbkk.png";
 
 		public const string OneYearManyFixes = "One Year, Many Fixes";
 		public const string OneYearManyFixesDescription = "Your betting career turns 1 – we see you, Fixer-in-Chief.";
-		public const string OneYearManyFixesImagePath = "/images/trophies/one-year-many-fixes.png";
+		// public const string OneYearManyFixesImagePath = "/images/trophies/one-year-many-fixes.png";
+		public const string OneYearManyFixesImagePath = "https://res.cloudinary.com/doorb7d6i/image/upload/v1754776638/one-year-many-fixes_s3petx.png";
 
 		public const string SummerSyndicateSlam = "Summer Syndicate Slam";
 		public const string SummerSyndicateSlamDescription = "10 shady bets in July – when the sun’s out, the fixes come out to play.";
-		public const string SummerSyndicateSlamImagePath = "/images/trophies/summer-syndicate-slam.png";
+		// public const string SummerSyndicateSlamImagePath = "/images/trophies/summer-syndicate-slam.png";
+		public const string SummerSyndicateSlamImagePath = "https://res.cloudinary.com/doorb7d6i/image/upload/v1754776639/summer-syndicate-slam_avm3ou.png";
 
 		// Outcome Trophies
 		public const string FixersHotStreak = "Fixer's Hot Streak";
 		public const string FixersHotStreakDescription = "3 winning bets in a row — you’re heating up.";
-		public const string FixersHotStreakImagePath = "/images/trophies/fixers-hot-streak.png";
+		// public const string FixersHotStreakImagePath = "/images/trophies/fixers-hot-streak.png";
+		public const string FixersHotStreakImagePath = "https://res.cloudinary.com/doorb7d6i/image/upload/v1754776637/fixers-hot-streak_duvsvb.png";
 
 		public const string SyndicateSharpshooter = "Syndicate Sharp Shooter";
 		public const string SyndicateSharpshooterDescription = "5 winning bets in a single day — you’re making bookies sweat.";
-		public const string SyndicateSharpshooterImagePath = "/images/trophies/syndicate-sharpshooter.png";
+		// public const string SyndicateSharpshooterImagePath = "/images/trophies/syndicate-sharpshooter.png";
+		public const string SyndicateSharpshooterImagePath = "https://res.cloudinary.com/doorb7d6i/image/upload/v1754776641/syndicate-sharpshooter_vhtxir.png";
 
 		public const string RiggedToWin = "Rigged To Win";
 		public const string RiggedToWinDescription = "20 total winning bets — statistically suspicious, impressively efficient.";
-		public const string RiggedToWinImagePath = "/images/trophies/rigged-to-win.png";
+		// public const string RiggedToWinImagePath = "/images/trophies/rigged-to-win.png";
+		public const string RiggedToWinImagePath = "https://res.cloudinary.com/doorb7d6i/image/upload/v1754776638/rigged-to-win_oyx2v2.png";
 
 		public const string FixGoneWrong = "Fix Gone Wrong";
 		public const string FixGoneWrongDescription = "Lost 3 bets in a row — even the best plans fall apart sometimes.";
-		public const string FixGoneWrongImagePath = "/images/trophies/fix-gone-wrong.png";
+		// public const string FixGoneWrongImagePath = "/images/trophies/fix-gone-wrong.png";
+		public const string FixGoneWrongImagePath = "https://res.cloudinary.com/doorb7d6i/image/upload/v1754776636/fix-gone-wrong_ofamps.png";
 
 
 		public const string UnluckySyndicateSoldier = "Unlucky Syndicate Soldier";
 		public const string UnluckySyndicateSoldierDescription = "Lost 10 bets total — it’s not fixing, it’s just bad luck… right?";
-		public const string UnluckySyndicateSoldierImagePath = "/images/trophies/unlucky-syndicate-soldier.png";
+		// public const string UnluckySyndicateSoldierImagePath = "/images/trophies/unlucky-syndicate-soldier.png";
+		public const string UnluckySyndicateSoldierImagePath = "https://res.cloudinary.com/doorb7d6i/image/upload/v1754776639/unlucky-syndicate-soldier_fephlt.png";
 
 		public const string BankrollObliterator = "Bankroll Obliterator";
 		public const string BankrollObliteratorDescription = "Lost 5 bets in a single day — did you even try?";
-		public const string BankrollObliteratorImagePath = "/images/trophies/bankroll-obliterator.png";
+		// public const string BankrollObliteratorImagePath = "/images/trophies/bankroll-obliterator.png";
+		public const string BankrollObliteratorImagePath = "https://res.cloudinary.com/doorb7d6i/image/upload/v1754776638/bankroll-obliterator_jyduwd.png";
 
 
 		public const string ComebackKingpin = "Comeback Kingpin";
 		public const string ComebackKingpinDescription = "Won a bet after 5 straight losses — resilience or divine intervention?";
-		public const string ComebackKingpinImagePath = "/images/trophies/comeback-kingpin.png";
+		// public const string ComebackKingpinImagePath = "/images/trophies/comeback-kingpin.png";
+		public const string ComebackKingpinImagePath = "https://res.cloudinary.com/doorb7d6i/image/upload/v1754776636/comeback-kingpin_ptzakd.png";
 
 		public const string RollercoasterRigger = "Rollercoaster Rigger";
 		public const string RollercoasterRiggerDescription = "Alternated between win and loss 4 times in a row — your luck swings like a pendulum.";
-		public const string RollercoasterRiggerImagePath = "/images/trophies/rollercoaster-rigger.png";
+		// public const string RollercoasterRiggerImagePath = "/images/trophies/rollercoaster-rigger.png";
+		public const string RollercoasterRiggerImagePath = "https://res.cloudinary.com/doorb7d6i/image/upload/v1754776638/rollercoaster-rigger_msfupe.png";
 
 	}
 }
