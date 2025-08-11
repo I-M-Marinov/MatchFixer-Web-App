eb22c90919748ed526d00d254592151f20738c34
I-M-Marinov <lcfrrr@gmail.com>
2025-08-11 22:21:43 +0300

    Created the MatchEventLog entity:
    --> it will hold information on all changes made per event with the new and old values of the property changed, time of the change ( in UTC ), name of the user that changed it ( holding references to the MatchEvent and Application User tables )

---
+++ MatchFixer.Infrastructure/Entities/MatchEventLog.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MatchFixer.Infrastructure.Entities
+{
+	public class MatchEventLog
+	{
+		[Key]
+		public Guid Id { get; set; }
+
+		[Required]
+		public Guid MatchEventId { get; set; }
+
+		[ForeignKey(nameof(MatchEventId))]
+		public MatchEvent MatchEvent { get; set; }
+
+		[Required]
+		[MaxLength(100)]
+		public string PropertyName { get; set; } // e.g. "MatchDate", "HomeOdds"
+
+		[MaxLength(500)]
+		public string? OldValue { get; set; }
+
+		[MaxLength(500)]
+		public string? NewValue { get; set; }
+
+		public DateTime ChangedAt { get; set; } = DateTime.UtcNow;
+
+		[Required]
+		public Guid ChangedByUserId { get; set; }
+
+		[ForeignKey(nameof(ChangedByUserId))]
+		public ApplicationUser ChangedBy { get; set; }
+	}
+}
