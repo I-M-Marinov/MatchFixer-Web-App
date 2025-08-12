01c8b74e02f5192e404c772304725aec6a6ba229
I-M-Marinov <lcfrrr@gmail.com>
2025-08-12 23:45:32 +0300

    Created the IMatchEventNotifier interface in the Core project

---
+++ MatchFixer.Core/Contracts/IMatchEventNotifier.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace MatchFixer.Core.Contracts
+{
+	public interface IMatchEventNotifier
+	{
+		Task NotifyMatchEventUpdatedAsync(Guid matchEventId, decimal homeOdds, decimal drawOdds, decimal awayOdds);
+	}
+}
