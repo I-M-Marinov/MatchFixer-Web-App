8c566d76ede63c8d8665a5b1768b6d6717a6d772
I-M-Marinov <lcfrrr@gmail.com>
2025-08-12 23:46:01 +0300

    Created the MatchEventNotifier ( implements the IMatchEventNotifier interface from the core project )

---
+++ MatchFixer-Web-App/Hubs/MatchEventNotifier.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using MatchFixer.Core.Contracts;
+
+namespace MatchFixer_Web_App.Hubs
+{
+	public class MatchEventNotifier : IMatchEventNotifier
+	{
+		private readonly IHubContext<MatchEventHub> _hubContext;
+
+		public MatchEventNotifier(IHubContext<MatchEventHub> hubContext)
+		{
+			_hubContext = hubContext;
+		}
+
+		public Task NotifyMatchEventUpdatedAsync(Guid matchEventId, decimal homeOdds, decimal drawOdds, decimal awayOdds)
+		{
+			return _hubContext.Clients
+				.Group(matchEventId.ToString())
+				.SendAsync("MatchEventUpdated", new
+				{
+					matchEventId,
+					homeOdds,
+					drawOdds,
+					awayOdds
+				});
+		}
+	}
+
+}
