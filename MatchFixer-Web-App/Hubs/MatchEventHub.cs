0777e49763800494f199d442938d23560d032d43
I-M-Marinov <lcfrrr@gmail.com>
2025-08-12 23:45:09 +0300

    Created the MatchEventHub

---
+++ MatchFixer-Web-App/Hubs/MatchEventHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace MatchFixer_Web_App.Hubs
+{
+	public class MatchEventHub: Hub
+	{
+		public async Task SubscribeToEvent(Guid matchEventId)
+		{
+			await Groups.AddToGroupAsync(Context.ConnectionId, matchEventId.ToString());
+		}
+	}
+}
