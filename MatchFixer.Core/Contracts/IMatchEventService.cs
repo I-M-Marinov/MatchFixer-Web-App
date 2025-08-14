64a41b1372cc9594cde9d3224c481f17368f3abd
I-M-Marinov <lcfrrr@gmail.com>
2025-08-14 23:56:32 +0300

    Added the GetOddsForMatchesAsync signature to the IMatchEventService interface

--- MatchFixer.Core/Contracts/IMatchEventService.cs
+++ MatchFixer.Core/Contracts/IMatchEventService.cs
@@ -1,4 +1,5 @@
-﻿using MatchFixer.Core.ViewModels.LiveEvents;
+﻿using MatchFixer.Core.DTOs.Bets;
+using MatchFixer.Core.ViewModels.LiveEvents;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Threading.Tasks;
 
@@ -13,5 +14,6 @@ namespace MatchFixer.Core.Contracts
 		Task<Dictionary<string, List<SelectListItem>>> GetTeamsGroupedByLeagueAsync();
 		Task<bool> EditMatchEventAsync(Guid matchEventId, decimal homeOdds, decimal drawOdds, decimal awayOdds, DateTime kickoffTime);
 		Task<bool> CancelMatchEventAsync(Guid matchEventId);
+		Task<Dictionary<Guid, OddsDTO>> GetOddsForMatchesAsync(Guid[] matchIds);
 	}
 }
