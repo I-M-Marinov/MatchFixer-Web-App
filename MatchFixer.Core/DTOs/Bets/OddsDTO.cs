e4ded9f26a5e18374d77210aac46120fc188ef48
I-M-Marinov <lcfrrr@gmail.com>
2025-08-14 23:56:42 +0300

    Created the OddsDTO

---
+++ MatchFixer.Core/DTOs/Bets/OddsDTO.cs
@@ -0,0 +1,11 @@
+﻿
+namespace MatchFixer.Core.DTOs.Bets
+{
+	public class OddsDTO
+	{ 
+		public Guid Id { get; set; }
+		public decimal? HomeOdds { get; set; }
+		public decimal? DrawOdds { get; set; }
+		public decimal? AwayOdds { get; set; }
+	}
+}
