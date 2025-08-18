5ca3054785843de43740d236f1f8c5f1596f4c6e
I-M-Marinov <lcfrrr@gmail.com>
2025-08-18 18:17:54 +0300

    Added some constants for the Wallet Controller

--- MatchFixer.Common/GeneralConstants/WalletServiceConstants.cs
+++ MatchFixer.Common/GeneralConstants/WalletServiceConstants.cs
@@ -3,7 +3,7 @@
 	public static class WalletServiceConstants
 	{
 		public const string MoneyAmountMustBePositive = "Amount must be positive.";
-		public const string WalletNotFound = "Wallet not found.";
+		public const string WalletNotFound = "Wallet was not found.";
 		public const string NoTransactionsToClear = "No transactions to clear.";
 		public const string TransactionHistoryCleared = "Transaction history was cleared.";
 		public const string InsufficientBalanceToPlaceBet = "Insufficient balance to place the bet.";
@@ -11,6 +11,18 @@
 		public const string WinAmountMustBeGreaterThanZero = "Amount won must be greater than zero.";
 		public const string AmountDeductedForTheBet = "The amount was deducted for the bet.";
 
+		// Wallet Controller
+
+		public const string WalletCreatedSuccessfully = "Wallet created successfully.";
+		public const string InsufficientBalanceForWithdrawal = "Insufficient balance for this withdrawal.";
+		public static string SuccessfullyDeposited(decimal amount)
+		{
+			return $"Successfully deposited {amount:0.00} EUR.";
+		}
+		public static string SuccessfullyWithdrew(decimal amount)
+		{
+			return $"Successfully withdrew {amount:0.00} EUR.";
+		}
 
 	}
 }
