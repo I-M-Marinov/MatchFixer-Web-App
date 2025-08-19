ee8e50f7631d2fd9802f47cce1ba5e04fff1b5c4
Ivan Marinov <134225981+I-M-Marinov@users.noreply.github.com>
2025-08-19 22:03:11 +0300

    Created the UserBetsConstants static class

---
+++ MatchFixer.Common/GeneralConstants/UserBetsConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace MatchFixer.Common.GeneralConstants
+{
+	public static class UserBetsConstants
+	{
+		public const string BetSlipEvaluatedSuccessfully = "Bet slip evaluated successfully.";
+		public const string CouldNotEvaluateBetslip = "Could not evaluate this bet slip. It may still be pending or already settled.";
+
+
+	}
+}
