e1132868b5a1c87d113e7342d9fe33687c78234f
Ivan Marinov <134225981+I-M-Marinov@users.noreply.github.com>
2025-08-19 22:03:00 +0300

    Created the MatchResultConstants static class

---
+++ MatchFixer.Common/GeneralConstants/MatchResultConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace MatchFixer.Common.GeneralConstants
+{
+	public static class MatchResultConstants
+	{
+		public const string MatchResultSavedSuccessfully = "Match result saved successfully and winnings awarded.";
+		public const string MatchResultSaveFailed = "Could not save result. It might already be recorded.";
+	}
+}
