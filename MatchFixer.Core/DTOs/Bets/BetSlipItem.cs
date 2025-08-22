f825db6105803dbf077811992e5207811b1655c2
Ivan Marinov <134225981+I-M-Marinov@users.noreply.github.com>
2025-08-22 23:29:59 +0300

    Updated the BetSlipItem DTO

--- MatchFixer.Core/DTOs/Bets/BetSlipItem.cs
+++ MatchFixer.Core/DTOs/Bets/BetSlipItem.cs
@@ -6,14 +6,17 @@ namespace MatchFixer.Core.DTOs.Bets
 	{
 		[JsonPropertyName("MatchId")]
 		public Guid MatchId { get; set; }
-		public string HomeTeam { get; set; }
-		public string AwayTeam { get; set; }
-		public string HomeLogoUrl { get; set; }
-		public string AwayLogoUrl { get; set; }
+
+		public string? HomeTeam { get; set; }
+		public string? AwayTeam { get; set; }
+		public string? HomeLogoUrl { get; set; }
+		public string? AwayLogoUrl { get; set; }
+
 		[JsonPropertyName("SelectedOption")]
 		public string SelectedOption { get; set; }
-		public decimal Odds { get; set; }
-		public DateTime StartTimeUtc { get; set; }
 
+		public decimal Odds { get; set; }
+		public DateTime? StartTimeUtc { get; set; }
 	}
+
 }
