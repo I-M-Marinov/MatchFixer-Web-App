4e4da179a081c36b5bc059e0454c49c429589ce0
Ivan Marinov <134225981+I-M-Marinov@users.noreply.github.com>
2025-08-24 15:41:01 +0300

    Created the BoostViewModel

---
+++ MatchFixer.Core/ViewModels/LiveEvents/BoostViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace MatchFixer.Core.ViewModels.LiveEvents
+{
+	public class BoostViewModel
+	{
+		public decimal BoostValue { get; set; }
+		public DateTime StartUtc { get; set; }
+		public DateTime EndUtc { get; set; }
+		public decimal? MaxStakePerBet { get; set; }
+		public int? MaxUsesPerUser { get; set; }
+		public string? Note { get; set; }
+	}
+
+}
