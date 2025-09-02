69ccf44c735693183f8dcdf4e3ce56c3ab803c73
I-M-Marinov <lcfrrr@gmail.com>
2025-09-02 22:22:11 +0300

    Created the static class Permissions

---
+++ MatchFixer.Common/Identity/Permissions.cs
@@ -0,0 +1,10 @@
+﻿namespace MatchFixer.Common.Identity
+{
+	public static class Permissions
+	{
+		public const string ManageUsers = "perm.manage_users";
+		public const string ManageWallets = "perm.manage_wallets";
+		public const string ManageMatchEvents = "perm.manage_match_events";
+		
+	}
+}
