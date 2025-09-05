197dd57bdf089f6b4c9073d82aeeffebd9cdf1ad
Ivan Marinov <134225981+I-M-Marinov@users.noreply.github.com>
2025-09-05 23:57:50 +0300

    Created the IAdminDashboardService interface

---
+++ MatchFixer-Web-App/Areas/Admin/Services/IAdminDashboardService.cs
@@ -0,0 +1,9 @@
+﻿using MatchFixer_Web_App.Areas.Admin.ViewModels;
+
+namespace MatchFixer_Web_App.Areas.Admin.Services
+{
+	public interface IAdminDashboardService
+	{
+		Task<AdminDashboardViewModel> GetDashboardAsync();
+	}
+}
