e5a6d346a7528aac2385214d413653de3e7e453f
Ivan Marinov <134225981+I-M-Marinov@users.noreply.github.com>
2025-09-05 23:58:10 +0300

    Created the DashboardController

---
+++ MatchFixer-Web-App/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,26 @@
+﻿using MatchFixer.Infrastructure.Security;
+using MatchFixer_Web_App.Areas.Admin.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MatchFixer_Web_App.Areas.Admin.Controllers
+{
+	
+	[Area("Admin")]
+	[AdminOnly]
+	public class DashboardController : Controller
+	{
+		private readonly IAdminDashboardService _dashboardService;
+
+		public DashboardController(IAdminDashboardService dashboardService)
+		{
+			_dashboardService = dashboardService;
+		}
+
+		public async Task<IActionResult> Index()
+		{
+			var model = await _dashboardService.GetDashboardAsync();
+			return View(model);
+		}
+	}
+	
+}
