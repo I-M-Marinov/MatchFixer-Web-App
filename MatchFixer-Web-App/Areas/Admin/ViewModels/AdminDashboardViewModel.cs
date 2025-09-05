bc5c37b0146af058732da747cd40388c313182fc
Ivan Marinov <134225981+I-M-Marinov@users.noreply.github.com>
2025-09-05 23:57:38 +0300

    Created the AdminDashboardViewModel

---
+++ MatchFixer-Web-App/Areas/Admin/ViewModels/AdminDashboardViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace MatchFixer_Web_App.Areas.Admin.ViewModels
+{
+	public class AdminDashboardViewModel
+	{
+		public int TotalUsers { get; set; }
+		public int ActiveUsers { get; set; }
+
+		public decimal TotalWalletBalance { get; set; }
+		public decimal TotalTransactions { get; set; }
+
+		public int TotalBets { get; set; }
+		public int PendingBets { get; set; }
+		public int WonBets { get; set; }
+		public int LostBets { get; set; }
+	}
+}
