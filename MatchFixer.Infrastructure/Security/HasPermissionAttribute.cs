50c99c6b353a6cd19b7f7490425d94715bfba305
Ivan Marinov <134225981+I-M-Marinov@users.noreply.github.com>
2025-09-05 23:56:45 +0300

    Added the HasPermissionAttribute

---
+++ MatchFixer.Infrastructure/Security/HasPermissionAttribute.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace MatchFixer.Infrastructure.Security
+{
+	public class HasPermissionAttribute : AuthorizeAttribute
+	{
+		public HasPermissionAttribute(string permission)
+		{
+			Policy = permission; // uses the policies you registered in Program.cs
+		}
+	}
+}
