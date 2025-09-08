5ac63e879fafd072115cef8a5634fdc4248a35ca
Ivan Marinov <134225981+I-M-Marinov@users.noreply.github.com>
2025-09-08 23:55:54 +0300

    Moved the interface in a dedicated folder ( separate from the services folder )

--- MatchFixer-Web-App/Areas/Admin/Services/IAdminDashboardService.cs
+++
@@ -1,9 +0,0 @@
-﻿using MatchFixer_Web_App.Areas.Admin.ViewModels;
-
-namespace MatchFixer_Web_App.Areas.Admin.Services
-{
-	public interface IAdminDashboardService
-	{
-		Task<AdminDashboardViewModel> GetDashboardAsync();
-	}
-}
