c546790ea544d886a260157b5064639a8139b45e
Ivan Marinov <134225981+I-M-Marinov@users.noreply.github.com>
2025-09-08 23:56:24 +0300

    Created the AdminUsersListViewModel

---
+++ MatchFixer-Web-App/Areas/Admin/ViewModels/AdminUsersListViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace MatchFixer_Web_App.Areas.Admin.ViewModels
+{
+	public class AdminUsersListViewModel
+	{
+		public string? Query { get; set; }
+		public int Page { get; set; }
+		public int PageSize { get; set; }
+		public int Total { get; set; }
+		public List<AdminUserRow> Users { get; set; } = new();
+	}
+}
