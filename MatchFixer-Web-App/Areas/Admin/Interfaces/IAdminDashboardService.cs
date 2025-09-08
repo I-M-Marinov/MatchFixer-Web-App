5dd17ca0ae060bc45fb0675db8383063f7693222
Ivan Marinov <134225981+I-M-Marinov@users.noreply.github.com>
2025-09-08 23:50:39 +0300

    Created the Interfaces folder and added the IAdminUserService

---
+++ MatchFixer-Web-App/Areas/Admin/Interfaces/IAdminDashboardService.cs
@@ -0,0 +1,9 @@
+﻿using MatchFixer_Web_App.Areas.Admin.ViewModels;
+
+namespace MatchFixer_Web_App.Areas.Admin.Interfaces
+{
+	public interface IAdminDashboardService
+	{
+		Task<AdminDashboardViewModel> GetDashboardAsync();
+	}
+}
---
+++ MatchFixer-Web-App/Areas/Admin/Interfaces/IAdminUserService.cs
@@ -0,0 +1,18 @@
+﻿using MatchFixer_Web_App.Areas.Admin.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MatchFixer_Web_App.Areas.Admin.Interfaces
+{
+	public interface IAdminUserService
+	{
+		Task<AdminUsersListViewModel> GetUsersAsync(string? query, int page, int pageSize);
+
+		Task<bool> LockUserAsync(Guid userId);
+		Task<bool> UnlockUserAsync(Guid userId);
+		Task<bool> MarkEmailConfirmedAsync(Guid userId);
+
+		Task<(bool ok, string? resetLink)> GenerateResetPasswordLinkAsync(Guid userId, IUrlHelper url);
+		Task<bool> AddRoleAsync(Guid userId, string role);
+		Task<bool> RemoveRoleAsync(Guid userId, string role);
+	}
+}
