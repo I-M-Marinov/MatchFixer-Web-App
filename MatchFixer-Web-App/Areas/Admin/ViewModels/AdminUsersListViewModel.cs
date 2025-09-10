57268325cfff2c4f5a84a453b9b5579a7e8e2809
I-M-Marinov <lcfrrr@gmail.com>
2025-09-10 23:54:48 +0300

    Added the Status property to the AdminUsersListViewModel

--- MatchFixer-Web-App/Areas/Admin/ViewModels/AdminUsersListViewModel.cs
+++ MatchFixer-Web-App/Areas/Admin/ViewModels/AdminUsersListViewModel.cs
@@ -3,6 +3,7 @@
 	public class AdminUsersListViewModel
 	{
 		public string? Query { get; set; }
+		public string? Status { get; set; }   
 		public int Page { get; set; }
 		public int PageSize { get; set; }
 		public int Total { get; set; }
