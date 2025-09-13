40d785f5b43ef6aaba1584739f3c9d73068940f8
I-M-Marinov <lcfrrr@gmail.com>
2025-09-13 23:58:47 +0300

    Created the AdminWalletViewModel

---
+++ MatchFixer-Web-App/Areas/Admin/ViewModels/Wallet/AdminWalletViewModel.cs
@@ -0,0 +1,15 @@
+﻿using MatchFixer.Core.ViewModels.Wallet;
+
+namespace MatchFixer_Web_App.Areas.Admin.ViewModels.Wallet
+{
+	public class AdminWalletViewModel
+	{
+		public Guid UserId { get; set; }
+		public string? UserName { get; set; }
+		public string? Email { get; set; }
+		public bool HasWallet { get; set; }
+		public decimal Balance { get; set; }
+		public string Currency { get; set; } = "EUR";
+		public List<WalletTransactionViewModel> Transactions { get; set; } = new();
+	}
+}
