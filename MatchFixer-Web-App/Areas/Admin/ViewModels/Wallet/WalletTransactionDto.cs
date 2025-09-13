4877ac6762c05ae13490082a22c92b988295e80c
I-M-Marinov <lcfrrr@gmail.com>
2025-09-13 23:58:30 +0300

    Created the WalletTransactionDto

---
+++ MatchFixer-Web-App/Areas/Admin/ViewModels/Wallet/WalletTransactionDto.cs
@@ -0,0 +1,11 @@
+﻿namespace MatchFixer_Web_App.Areas.Admin.ViewModels.Wallet
+{
+	public class WalletTransactionDto
+	{
+		public DateTime CreatedUtc { get; set; }
+		public string Type { get; set; } = "";
+		public decimal Amount { get; set; }
+		public decimal BalanceAfter { get; set; }
+		public string? Note { get; set; }
+	}
+}
