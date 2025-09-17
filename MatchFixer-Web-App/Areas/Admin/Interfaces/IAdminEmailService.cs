1dc553a456820e6e3547a38887751b3c029ea9ec
I-M-Marinov <lcfrrr@gmail.com>
2025-09-17 23:18:17 +0300

    Created the IAdminEmailService ( admin area )

---
+++ MatchFixer-Web-App/Areas/Admin/Interfaces/IAdminEmailService.cs
@@ -0,0 +1,10 @@
+﻿using MatchFixer_Web_App.Areas.Admin.ViewModels.Email;
+
+namespace MatchFixer_Web_App.Areas.Admin.Interfaces
+{
+	public interface IAdminEmailService
+	{
+		Task<int> CountRecipientsAsync(EmailBlastCommand cmd);
+		Task<EmailBlastResult> SendAsync(EmailBlastCommand cmd);
+	}
+}
