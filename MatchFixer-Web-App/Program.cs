1c6a0a01659bc0b2ab0fbbaf0ee7af97506c1cf2
I-M-Marinov <lcfrrr@gmail.com>
2025-09-17 23:19:53 +0300

    Registered the AdminEmailService in the pipeline

--- MatchFixer-Web-App/Program.cs
+++ MatchFixer-Web-App/Program.cs
@@ -49,7 +49,8 @@ builder.Services.AddHttpClient();                                               
 
 builder.Services.AddScoped<IAdminDashboardService, AdminDashboardService>();    // Add the Admin Dashboard Service
 builder.Services.AddScoped<IAdminUserService, AdminUserService>();              // Add the Admin User Service
-builder.Services.AddScoped<IAdminWalletService, AdminWalletService>();
+builder.Services.AddScoped<IAdminWalletService, AdminWalletService>();			// Add the Admin Wallet Service
+builder.Services.AddScoped<IAdminEmailService, AdminEmailService>();           // Add the Admin Email Service
 
 
 builder.Services.AddHttpClient<WikipediaService>();                             // Add the Wikipedia Service ( HTTP Client ) 
