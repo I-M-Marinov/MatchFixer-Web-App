497e5743baac1e5dfedaccccd9ff7c386588793e
I-M-Marinov <lcfrrr@gmail.com>
2025-09-29 23:48:39 +0300

    Created the IAdminUserBetsService Interface

---
+++ MatchFixer-Web-App/Areas/Admin/Interfaces/IAdminUserBetsService.cs
@@ -0,0 +1,12 @@
+﻿using MatchFixer_Web_App.Areas.Admin.ViewModels.Bets;
+
+namespace MatchFixer_Web_App.Areas.Admin.Interfaces
+{
+	public interface IAdminUserBetsService
+	{
+		Task<AdminUserBetsColumnsViewModel?> GetColumnsAsync(
+			Guid userId,
+			string timeZoneId,
+			int maxPerColumn = 200);
+	}
+}
