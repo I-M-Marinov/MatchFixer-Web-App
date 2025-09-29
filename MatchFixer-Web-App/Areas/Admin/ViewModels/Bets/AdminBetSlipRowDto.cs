688feaef154372753dc673f2d4047d5b3c724f25
I-M-Marinov <lcfrrr@gmail.com>
2025-09-29 23:52:25 +0300

    Created the AdminBetSlipRowDto

---
+++ MatchFixer-Web-App/Areas/Admin/ViewModels/Bets/AdminBetSlipRowDto.cs
@@ -0,0 +1,21 @@
+﻿using MatchFixer.Common.Enums;
+
+namespace MatchFixer_Web_App.Areas.Admin.ViewModels.Bets
+{
+	public class AdminBetSlipRowDto
+	{
+		public Guid Id { get; set; }
+		public BetStatus SlipStatus { get; set; }
+		public decimal Stake { get; set; }             
+		public decimal? WinAmount { get; set; }       
+		public decimal? PotentialReturn { get; set; } 
+
+		public int Selections { get; set; }
+
+		public DateTime CreatedUtc { get; set; }
+		public string CreatedDisplay { get; set; } = "";
+		public DateTime? SettledUtc { get; set; }
+		public string? SettledDisplay { get; set; }
+	}
+
+}
