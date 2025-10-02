26253b5f34bfdfc5bd3970e9ad1714fdd2c8c423
I-M-Marinov <lcfrrr@gmail.com>
2025-10-02 23:33:47 +0300

    Created the AdminEventRow

---
+++ MatchFixer-Web-App/Areas/Admin/ViewModels/MatchEvents/AdminEventRow.cs
@@ -0,0 +1,13 @@
+﻿namespace MatchFixer_Web_App.Areas.Admin.ViewModels.MatchEvents
+{
+	public class AdminEventRow
+	{
+		public Guid Id { get; set; }
+		public string? League { get; set; } // for later use ... 
+		public string HomeTeam { get; set; } = "";
+		public string AwayTeam { get; set; } = "";
+		public DateTime MatchUtc { get; set; }
+		public string Status { get; set; } = "";
+		public DateTime? UpdatedUtc { get; set; }
+	}
+}
