928cc8a2e3522260ff1190da445372777d9f1e57
I-M-Marinov <lcfrrr@gmail.com>
2025-10-02 23:33:16 +0300

    Added the BuildEventsSummaryAsync signature to the IAdminDashboardService

--- MatchFixer-Web-App/Areas/Admin/Interfaces/IAdminDashboardService.cs
+++ MatchFixer-Web-App/Areas/Admin/Interfaces/IAdminDashboardService.cs
@@ -1,9 +1,11 @@
 ﻿using MatchFixer_Web_App.Areas.Admin.ViewModels;
+using MatchFixer_Web_App.Areas.Admin.ViewModels.MatchEvents;
 
 namespace MatchFixer_Web_App.Areas.Admin.Interfaces
 {
 	public interface IAdminDashboardService
 	{
 		Task<AdminDashboardViewModel> GetDashboardAsync();
+		Task<AdminEventsSummaryViewModel> BuildEventsSummaryAsync(int takeRecent);
 	}
 }
