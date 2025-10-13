81f03fcde3ccae3bf88917a1cafae5d4d442dd78
I-M-Marinov <lcfrrr@gmail.com>
2025-10-13 23:06:31 +0300

    Added the HomeTeamLogo and the AwayTeamLogo properties to the AdminEventRow

--- MatchFixer-Web-App/Areas/Admin/ViewModels/MatchEvents/AdminEventRow.cs
+++ MatchFixer-Web-App/Areas/Admin/ViewModels/MatchEvents/AdminEventRow.cs
@@ -5,7 +5,9 @@
 		public Guid Id { get; set; }
 		public string? League { get; set; } // for later use ... 
 		public string HomeTeam { get; set; } = "";
+		public string HomeTeamLogo { get; set; } = "";
 		public string AwayTeam { get; set; } = "";
+		public string AwayTeamLogo { get; set; } = "";
 		public DateTime MatchUtc { get; set; }
 		public string Status { get; set; } = "";
 		public DateTime? UpdatedUtc { get; set; }
