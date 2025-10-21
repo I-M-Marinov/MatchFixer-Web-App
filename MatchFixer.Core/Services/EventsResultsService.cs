37a668ec7b350d7fa516f35b90bbce8356bf5276
I-M-Marinov <lcfrrr@gmail.com>
2025-10-21 14:17:22 +0300

    Created the EventsResultsService

---
+++ MatchFixer.Core/Services/EventsResultsService.cs
@@ -0,0 +1,43 @@
+﻿using MatchFixer_Web_App.Areas.Admin.Interfaces;
+using MatchFixer.Infrastructure;
+using MatchFixer.Core.ViewModels.EventsResults;
+using Microsoft.EntityFrameworkCore;
+
+namespace MatchFixer.Core.Services
+{
+	public class EventsResultsService: IEventsResultsService
+	{
+
+		private readonly MatchFixerDbContext _dbContext;
+
+		public EventsResultsService(MatchFixerDbContext dbContext) => _dbContext = dbContext;
+
+		public async Task<IReadOnlyList<EventsResults>> GetLatestAsync(int count = 10)
+		{
+			// Not cancelled + has result
+			var query = _dbContext.MatchEvents
+				.AsNoTracking()
+				.Include(e => e.HomeTeam)
+				.Include(e => e.AwayTeam)
+				.Include(e => e.LiveResult)
+				.Where(e => !e.IsCancelled && e.LiveResult != null);
+
+			var items = await query
+				.OrderByDescending(e => e.MatchDate)
+				.Take(count)
+				.Select(e => new EventsResults
+				{
+					MatchEventId = e.Id,
+					HomeTeam = e.HomeTeam!.Name,
+					AwayTeam = e.AwayTeam!.Name,
+					HomeScore = e.LiveResult!.HomeScore,
+					AwayScore = e.LiveResult!.AwayScore,
+					HomeLogoUrl = e.HomeTeam!.LogoUrl,
+					AwayLogoUrl = e.AwayTeam!.LogoUrl
+				})
+				.ToListAsync();
+
+			return items;
+		}
+	}
+}
