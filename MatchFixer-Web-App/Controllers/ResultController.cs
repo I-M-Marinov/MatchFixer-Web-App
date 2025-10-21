023d9f341b5505b0316c27f02adbd758db3b82ee
I-M-Marinov <lcfrrr@gmail.com>
2025-10-21 14:21:25 +0300

    Created a HttpGet method EventsResults that would call the service ( eventsResultsService, injected through the constructor ), get the last 10 matches with results and return them to the controller and the view in turn

--- MatchFixer-Web-App/Controllers/ResultController.cs
+++ MatchFixer-Web-App/Controllers/ResultController.cs
@@ -1,5 +1,6 @@
 ﻿using MatchFixer.Core.Contracts;
 using MatchFixer.Core.ViewModels.MatchResults;
+using MatchFixer_Web_App.Areas.Admin.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 using static MatchFixer.Common.GeneralConstants.MatchResultConstants;
@@ -9,10 +10,13 @@ namespace MatchFixer_Web_App.Controllers
 	public class ResultController : Controller
 	{
 		private readonly ILiveMatchResultService _resultService;
+		private readonly IEventsResultsService _eventsResultsService;
 
-		public ResultController(ILiveMatchResultService resultService)
+		public ResultController(ILiveMatchResultService resultService, IEventsResultsService eventsResultsService)
 		{
 			_resultService = resultService;
+			_eventsResultsService = eventsResultsService;
+
 		}
 
 		[HttpGet]
@@ -45,6 +49,13 @@ namespace MatchFixer_Web_App.Controllers
 				TempData["ErrorMessage"] = MatchResultSaveFailed;
 
 			return RedirectToAction(nameof(LiveMatchResults));
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> EventsResults(int count = 10)
+		{
+			var model = await _eventsResultsService.GetLatestAsync(count);
+			return View(model);
 		}
 
 
