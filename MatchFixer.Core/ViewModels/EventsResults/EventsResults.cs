56a22f867f95ba43483a34290b3ace49847f3ef5
I-M-Marinov <lcfrrr@gmail.com>
2025-10-21 14:16:41 +0300

    Created the EventsResults View model

---
+++ MatchFixer.Core/ViewModels/EventsResults/EventsResults.cs
@@ -0,0 +1,16 @@
+﻿namespace MatchFixer.Core.ViewModels.EventsResults
+{
+	public class EventsResults
+	{
+		public Guid MatchEventId { get; set; }
+		public string HomeTeam { get; set; } = string.Empty;
+		public string AwayTeam { get; set; } = string.Empty;
+		public int HomeScore { get; set; }
+		public int AwayScore { get; set; }
+		public string? HomeLogoUrl { get; set; }
+		public string? AwayLogoUrl { get; set; }
+	}
+
+}
+
+
