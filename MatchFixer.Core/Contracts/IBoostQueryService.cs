cfac9d510f1f12fe1452fa5126af8de33a0b6ca5
I-M-Marinov <lcfrrr@gmail.com>
2025-10-24 23:54:57 +0300

    Created the IBoostQueryService interface

---
+++ MatchFixer.Core/Contracts/IBoostQueryService.cs
@@ -0,0 +1,22 @@
+﻿namespace MatchFixer.Core.Contracts
+{
+	public interface IBoostQueryService
+	{
+		Task<IReadOnlyList<ActiveBoost>> GetActiveBoostsAsync(IEnumerable<Guid> matchEventIds);
+
+		public class ActiveBoost
+		{
+			public Guid MatchEventId { get; init; }
+			public decimal EffectiveHomeOdds { get; init; }
+			public decimal EffectiveDrawOdds { get; init; }
+			public decimal EffectiveAwayOdds { get; init; }
+			public DateTime StartUtc { get; init; }
+			public DateTime EndUtc { get; init; }
+			public decimal? MaxStake { get; init; }
+			public int? MaxUses { get; init; }
+			public string? Label { get; init; }
+			public string? HomeTeamName { get; init; }
+			public string? AwayTeamName { get; init; }
+		}
+	}
+}
