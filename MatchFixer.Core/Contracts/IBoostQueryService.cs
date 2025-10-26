46abf89e9a7b48d201e9269a9a960bd7fe66aa8c
I-M-Marinov <lcfrrr@gmail.com>
2025-10-26 23:36:02 +0200

    Updated the IBoostQueryService
    -- moved the ActiveBoost out of the interface

--- MatchFixer.Core/Contracts/IBoostQueryService.cs
+++ MatchFixer.Core/Contracts/IBoostQueryService.cs
@@ -1,22 +1,10 @@
-﻿namespace MatchFixer.Core.Contracts
+﻿using MatchFixer.Core.ViewModels.DTO;
+
+namespace MatchFixer.Core.Contracts
 {
 	public interface IBoostQueryService
 	{
 		Task<IReadOnlyList<ActiveBoost>> GetActiveBoostsAsync(IEnumerable<Guid> matchEventIds);
-
-		public class ActiveBoost
-		{
-			public Guid MatchEventId { get; init; }
-			public decimal EffectiveHomeOdds { get; init; }
-			public decimal EffectiveDrawOdds { get; init; }
-			public decimal EffectiveAwayOdds { get; init; }
-			public DateTime StartUtc { get; init; }
-			public DateTime EndUtc { get; init; }
-			public decimal? MaxStake { get; init; }
-			public int? MaxUses { get; init; }
-			public string? Label { get; init; }
-			public string? HomeTeamName { get; init; }
-			public string? AwayTeamName { get; init; }
-		}
+		Task<IReadOnlyList<ActiveBoost>> GetAllActiveBoostsAsync(int limit);
 	}
 }
