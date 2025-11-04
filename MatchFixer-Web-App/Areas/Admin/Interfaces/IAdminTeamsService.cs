dabda1cebe4f0ab0fbe46bc9cb407d20e61e49d9
I-M-Marinov <lcfrrr@gmail.com>
2025-11-04 23:21:45 +0200

    Created the IAdminTeamsService interface

---
+++ MatchFixer-Web-App/Areas/Admin/Interfaces/IAdminTeamsService.cs
@@ -0,0 +1,11 @@
+﻿using MatchFixer_Web_App.Areas.Admin.ViewModels.Teams;
+
+namespace MatchFixer_Web_App.Areas.Admin.Interfaces
+{
+	public interface IAdminTeamsService
+	{
+		Task<IReadOnlyList<TeamSearchResult>> SearchTeamsAsync(string name, CancellationToken ct = default);
+		Task<bool> AddTeamFromSearchAsync(int apiTeamId, string name, string logoUrl, int leagueId, CancellationToken ct = default);
+		Task<PaginatedTeamsList<TeamListRow>> GetTeamsPageAsync(int page, int pageSize, CancellationToken ct = default);
+	}
+}
