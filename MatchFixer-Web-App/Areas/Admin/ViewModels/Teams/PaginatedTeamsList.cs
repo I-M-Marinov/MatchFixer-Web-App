9939970a7655911b7fc5462af0a4fbdbd134f6e1
I-M-Marinov <lcfrrr@gmail.com>
2025-11-04 23:19:27 +0200

    Created the PaginatedTeamsList<T>

---
+++ MatchFixer-Web-App/Areas/Admin/ViewModels/Teams/PaginatedTeamsList.cs
@@ -0,0 +1,11 @@
+﻿namespace MatchFixer_Web_App.Areas.Admin.ViewModels.Teams
+{
+	public class PaginatedTeamsList<T>
+	{
+		public IReadOnlyList<T> Items { get; init; } = Array.Empty<T>();
+		public int Page { get; init; }
+		public int PageSize { get; init; }
+		public int TotalCount { get; init; }
+		public int TotalPages => (int)Math.Ceiling((double)TotalCount / PageSize);
+	}
+}
