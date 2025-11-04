72eb039c156b62403e54f0253d0a062632af8bd4
I-M-Marinov <lcfrrr@gmail.com>
2025-11-04 23:19:51 +0200

    Created the TeamSearchVm

---
+++ MatchFixer-Web-App/Areas/Admin/ViewModels/Teams/TeamSearchVm.cs
@@ -0,0 +1,12 @@
+﻿namespace MatchFixer_Web_App.Areas.Admin.ViewModels.Teams
+{
+	public class TeamSearchVm
+	{
+		public string? Query { get; set; }
+		public int LeagueId { get; set; }
+		public int Season { get; set; }
+		public IReadOnlyDictionary<int, string>? Leagues { get; set; }
+		public IReadOnlyList<TeamSearchResult> Results { get; set; } = Array.Empty<TeamSearchResult>();
+	}
+}
+
