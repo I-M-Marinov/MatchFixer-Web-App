43f370361a35de24d6b9f92ecd9df6a4bbe834b9
I-M-Marinov <lcfrrr@gmail.com>
2025-11-04 23:18:47 +0200

    Created the SearchTeamListApiResponse

---
+++ MatchFixer-Web-App/Areas/Admin/ViewModels/Teams/DTO/SearchTeamListApiResponseTeamListApiResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace MatchFixer_Web_App.Areas.Admin.ViewModels.Teams.DTO
+{
+	public class SearchTeamListApiResponse
+	{
+		public List<SearchTeamResponseItem> Response { get; set; } = new();
+	}
+}
