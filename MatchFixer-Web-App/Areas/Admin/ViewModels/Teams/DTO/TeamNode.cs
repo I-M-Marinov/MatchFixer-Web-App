de354fc9e7789fa1445c13986c110ae8406e9088
I-M-Marinov <lcfrrr@gmail.com>
2025-11-04 23:19:11 +0200

    Created the TeamNode

---
+++ MatchFixer-Web-App/Areas/Admin/ViewModels/Teams/DTO/TeamNode.cs
@@ -0,0 +1,9 @@
+﻿namespace MatchFixer_Web_App.Areas.Admin.ViewModels.Teams.DTO
+{
+	public class TeamNode
+	{
+		public int Id { get; set; }
+		public string Name { get; set; } = "";
+		public string Logo { get; set; } = "";
+	}
+}
