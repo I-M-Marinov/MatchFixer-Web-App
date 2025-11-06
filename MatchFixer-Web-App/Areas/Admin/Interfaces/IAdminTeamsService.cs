ab3cc80752f85205adfecdf1be2c2228d3da84dc
I-M-Marinov <lcfrrr@gmail.com>
2025-11-06 23:41:48 +0200

    UPdated the IAdminTeamsService interface adding the leagueId and leagueIds int array in the AddTeamFromSearchAsync and GetTeamsPageAsync signatures

--- MatchFixer-Web-App/Areas/Admin/Interfaces/IAdminTeamsService.cs
+++ MatchFixer-Web-App/Areas/Admin/Interfaces/IAdminTeamsService.cs
@@ -5,7 +5,20 @@ namespace MatchFixer_Web_App.Areas.Admin.Interfaces
 	public interface IAdminTeamsService
 	{
 		Task<IReadOnlyList<TeamSearchResult>> SearchTeamsAsync(string name, CancellationToken ct = default);
-		Task<bool> AddTeamFromSearchAsync(int apiTeamId, string name, string logoUrl, int leagueId, CancellationToken ct = default);
-		Task<PaginatedTeamsList<TeamListRow>> GetTeamsPageAsync(int page, int pageSize, CancellationToken ct = default);
+
+		Task<bool> AddTeamFromSearchAsync(
+			int apiTeamId,
+			string name,
+			string logoUrl,
+			int leagueId,
+			CancellationToken ct = default);
+
+		Task<PaginatedTeamsList<TeamListRow>> GetTeamsPageAsync(
+			int page,
+			int pageSize,
+			int[]? leagueIds = null,
+			CancellationToken ct = default);
+
+		Task<Dictionary<int, string>> GetAllLeaguesAsync(CancellationToken ct = default);
 	}
 }
