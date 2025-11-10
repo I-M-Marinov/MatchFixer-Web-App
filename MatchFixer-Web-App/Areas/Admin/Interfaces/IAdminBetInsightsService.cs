4c89bd66945c0b0b99d98ce3d3017a2aa70c0f6e
I-M-Marinov <lcfrrr@gmail.com>
2025-11-10 22:42:09 +0200

    Created the IAdminBetInsightsService interface

---
+++ MatchFixer-Web-App/Areas/Admin/Interfaces/IAdminBetInsightsService.cs
@@ -0,0 +1,10 @@
+﻿using MatchFixer_Web_App.Areas.Admin.ViewModels.Events;
+
+namespace MatchFixer_Web_App.Areas.Admin.Interfaces
+{
+	public interface IAdminBetInsightsService
+	{
+		Task<PaginatedEventList<EventBetStatsRow>> GetUpcomingEventBetStatsAsync(
+			int page, int pageSize, CancellationToken ct = default);
+	}
+}
