8c589be90d41a956a45fc338f4c6a725ab903b3e
I-M-Marinov <lcfrrr@gmail.com>
2025-11-10 22:41:44 +0200

    Createad the EventBetStatsRow

---
+++ MatchFixer-Web-App/Areas/Admin/ViewModels/Events/EventBetStatsRow.cs
@@ -0,0 +1,28 @@
+﻿namespace MatchFixer_Web_App.Areas.Admin.ViewModels.Events
+{
+	public class EventBetStatsRow
+	{
+		public Guid EventId { get; set; }
+		public string HomeTeam { get; set; } = "";
+		public string AwayTeam { get; set; } = "";
+		public string LeagueName { get; set; } = "";
+		public DateTime KickoffUtc { get; set; }
+
+		// amounts
+		public decimal TotalStake { get; set; }
+		public decimal HomeStake { get; set; }
+		public decimal DrawStake { get; set; }
+		public decimal AwayStake { get; set; }
+
+		// counts 
+		public int TotalBets { get; set; }
+		public int HomeBets { get; set; }
+		public int DrawBets { get; set; }
+		public int AwayBets { get; set; }
+
+		// % (0..100)
+		public decimal HomePct { get; set; }
+		public decimal DrawPct { get; set; }
+		public decimal AwayPct { get; set; }
+	}
+}
