3a19a962d9cdd491936cb1b28110532930f99b80
I-M-Marinov <lcfrrr@gmail.com>
2025-11-10 23:48:52 +0200

    Created the AdminInsightsController

---
+++ MatchFixer-Web-App/Areas/Admin/Controllers/AdminInsightsController.cs
@@ -0,0 +1,23 @@
+﻿using MatchFixer.Infrastructure.Security;
+using MatchFixer_Web_App.Areas.Admin.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MatchFixer_Web_App.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+	[AdminOnly]
+	public sealed class AdminInsightsController : Controller
+	{
+		private readonly IAdminBetInsightsService _svc;
+		private const int PageSize = 20;
+
+		public AdminInsightsController(IAdminBetInsightsService svc) => _svc = svc;
+
+		[HttpGet]
+		public async Task<IActionResult> EventsSpread(int page = 1, CancellationToken ct = default)
+		{
+			var model = await _svc.GetUpcomingEventBetStatsAsync(page, PageSize, ct);
+			return View(model);
+		}
+	}
+}
