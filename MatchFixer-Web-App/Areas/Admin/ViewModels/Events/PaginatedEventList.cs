ba251d444352ccdf1c301bacb591c9f3038d8efe
I-M-Marinov <lcfrrr@gmail.com>
2025-11-10 22:41:55 +0200

    Added the PaginatedEventList

---
+++ MatchFixer-Web-App/Areas/Admin/ViewModels/Events/PaginatedEventList.cs
@@ -0,0 +1,11 @@
+﻿namespace MatchFixer_Web_App.Areas.Admin.ViewModels.Events
+{
+	public sealed class PaginatedEventList<T>
+	{
+		public IReadOnlyList<T> Items { get; init; } = Array.Empty<T>();
+		public int Page { get; init; }
+		public int PageSize { get; init; }
+		public int TotalCount { get; init; }
+		public int TotalPages => (int)Math.Ceiling((double)TotalCount / Math.Max(PageSize, 1));
+	}
+}
