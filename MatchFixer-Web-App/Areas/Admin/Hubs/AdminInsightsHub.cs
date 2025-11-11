ceab15d86a7aec443abc77091d46f3c02907653d
I-M-Marinov <lcfrrr@gmail.com>
2025-11-11 21:41:01 +0200

    Created the AdminInsightsHub

---
+++ MatchFixer-Web-App/Areas/Admin/Hubs/AdminInsightsHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using MatchFixer.Infrastructure.Security;
+
+namespace MatchFixer_Web_App.Areas.Admin.Hubs
+{
+	[AdminOnly]
+	public class AdminInsightsHub : Hub
+	{
+		public Task JoinEvent(Guid eventId) => Groups.AddToGroupAsync(Context.ConnectionId, $"evt:{eventId}");
+		public Task LeaveEvent(Guid eventId) => Groups.RemoveFromGroupAsync(Context.ConnectionId, $"evt:{eventId}");
+	}
+}
