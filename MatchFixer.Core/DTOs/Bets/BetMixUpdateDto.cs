48e103e1796753781f1d10d68057f68db73ddab1
I-M-Marinov <lcfrrr@gmail.com>
2025-11-11 21:50:30 +0200

    Created the BetMixUpdateDto

---
+++ MatchFixer.Core/DTOs/Bets/BetMixUpdateDto.cs
@@ -0,0 +1,22 @@
+﻿namespace MatchFixer.Core.DTOs.Bets
+{
+	public class BetMixUpdateDto
+	{
+		public Guid EventId { get; set; }
+		public int TotalBets { get; set; }
+		public decimal HomePct { get; set; }
+		public decimal DrawPct { get; set; }
+		public decimal AwayPct { get; set; }
+
+		public BetMixUpdateDto() { }
+
+		public BetMixUpdateDto(Guid eventId, int totalBets, decimal homePct, decimal drawPct, decimal awayPct)
+		{
+			EventId = eventId;
+			TotalBets = totalBets;
+			HomePct = homePct;
+			DrawPct = drawPct;
+			AwayPct = awayPct;
+		}
+	}
+}
