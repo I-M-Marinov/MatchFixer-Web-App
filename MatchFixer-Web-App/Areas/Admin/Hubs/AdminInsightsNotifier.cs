8e54a864dcb589ce1fcc3cfc92840ddda4bb1250
I-M-Marinov <lcfrrr@gmail.com>
2025-11-11 21:41:19 +0200

    Created the AdminInsightsNotifier

---
+++ MatchFixer-Web-App/Areas/Admin/Hubs/AdminInsightsNotifier.cs
@@ -0,0 +1,24 @@
+﻿using MatchFixer_Web_App.Areas.Admin.Hubs;
+using MatchFixer.Core.Contracts;
+using MatchFixer.Core.DTOs.Bets;
+using Microsoft.AspNetCore.SignalR;
+
+namespace MatchFixer.Core.Services
+{
+	public sealed class AdminInsightsNotifier : IAdminInsightsNotifier
+	{
+		private readonly IHubContext<AdminInsightsHub> _hub;
+		public AdminInsightsNotifier(IHubContext<AdminInsightsHub> hub) => _hub = hub;
+
+		public Task PublishBetMixAsync(BetMixUpdateDto m, CancellationToken ct = default)
+			=> _hub.Clients.Group($"evt:{m.EventId}")
+				.SendAsync("BetMixUpdated", new
+				{
+					eventId = m.EventId,
+					totalBets = m.TotalBets,
+					homePct = m.HomePct,
+					drawPct = m.DrawPct,
+					awayPct = m.AwayPct
+				}, ct);
+	}
+}
