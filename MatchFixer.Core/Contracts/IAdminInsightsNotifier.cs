95656c12d8b3ab8a5c4be80a08342062ab686f14
I-M-Marinov <lcfrrr@gmail.com>
2025-11-11 21:41:32 +0200

    Created the IAdminInsightsNotifier interface

---
+++ MatchFixer.Core/Contracts/IAdminInsightsNotifier.cs
@@ -0,0 +1,9 @@
+﻿using MatchFixer.Core.DTOs.Bets;
+
+namespace MatchFixer.Core.Contracts
+{
+	public interface IAdminInsightsNotifier
+	{
+		Task PublishBetMixAsync(BetMixUpdateDto payload, CancellationToken ct = default);
+	}
+}
