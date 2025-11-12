9b5202bcd9bb54b77f10f0c2371bf65effa216dd
I-M-Marinov <lcfrrr@gmail.com>
2025-11-12 23:32:51 +0200

    Added the TotalStake property to the BetMixUpdateDto

--- MatchFixer.Core/DTOs/Bets/BetMixUpdateDto.cs
+++ MatchFixer.Core/DTOs/Bets/BetMixUpdateDto.cs
@@ -3,6 +3,7 @@
 	public class BetMixUpdateDto
 	{
 		public Guid EventId { get; set; }
+		public decimal TotalStake { get; set; } 
 		public int TotalBets { get; set; }
 		public decimal HomePct { get; set; }
 		public decimal DrawPct { get; set; }
@@ -10,10 +11,11 @@
 
 		public BetMixUpdateDto() { }
 
-		public BetMixUpdateDto(Guid eventId, int totalBets, decimal homePct, decimal drawPct, decimal awayPct)
+		public BetMixUpdateDto(Guid eventId, int totalBets, decimal totalStake, decimal homePct, decimal drawPct, decimal awayPct)
 		{
 			EventId = eventId;
 			TotalBets = totalBets;
+			TotalStake = totalStake;
 			HomePct = homePct;
 			DrawPct = drawPct;
 			AwayPct = awayPct;
