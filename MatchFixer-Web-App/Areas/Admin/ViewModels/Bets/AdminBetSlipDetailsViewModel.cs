2549d840ef6ed5683045ca11185d4f514f5bf814
I-M-Marinov <lcfrrr@gmail.com>
2025-11-30 23:06:09 +0200

    Created the AdminBetSlipDetailsViewModel

---
+++ MatchFixer-Web-App/Areas/Admin/ViewModels/Bets/AdminBetSlipDetailsViewModel.cs
@@ -0,0 +1,26 @@
+﻿using MatchFixer.Common.Enums;
+
+namespace MatchFixer_Web_App.Areas.Admin.ViewModels.Bets
+{
+	public class AdminBetSlipDetailsViewModel
+	{
+		public Guid Id { get; set; }
+		public BetStatus SlipStatus { get; set; }
+
+		public decimal Stake { get; set; }
+		public decimal? WinAmount { get; set; }
+		public decimal? PotentialReturn { get; set; }
+
+		public string StatusBadge =>
+			SlipStatus switch
+			{
+				BetStatus.Won => "bg-success",
+				BetStatus.Lost => "bg-danger",
+				BetStatus.Voided => "bg-secondary",
+				BetStatus.Pending => "bg-warning text-dark",
+				_ => "bg-light text-dark"
+			};
+
+		public List<AdminBetSlipSelectionDto> Selections { get; set; } = new();
+	}
+}
