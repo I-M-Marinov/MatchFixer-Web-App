d89a7bd0a6e6ec6f5070d6a7391da0edd233ff7d
I-M-Marinov <lcfrrr@gmail.com>
2025-11-30 23:06:21 +0200

    Created the AdminBetSlipSelectionDto

---
+++ MatchFixer-Web-App/Areas/Admin/ViewModels/Bets/AdminBetSlipSelectionDto.cs
@@ -0,0 +1,11 @@
+﻿namespace MatchFixer_Web_App.Areas.Admin.ViewModels.Bets
+{
+	public class AdminBetSlipSelectionDto
+	{
+		public string MatchName { get; set; }
+		public string Pick { get; set; }
+		public decimal Odds { get; set; }
+		public string Status { get; set; }
+		public string StatusBadge { get; set; } = "bg-light";
+	}
+}
