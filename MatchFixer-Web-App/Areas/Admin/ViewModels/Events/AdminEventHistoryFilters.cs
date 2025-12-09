310a7c257eb6d3e6e2712860f54e0c4dba8a4c37
I-M-Marinov <lcfrrr@gmail.com>
2025-12-09 22:59:30 +0200

    Created the AdminEventHistoryFilters

---
+++ MatchFixer-Web-App/Areas/Admin/ViewModels/Events/AdminEventHistoryFilters.cs
@@ -0,0 +1,10 @@
+﻿namespace MatchFixer_Web_App.Areas.Admin.ViewModels.Events
+{
+	public class AdminEventHistoryFilters
+	{
+		public DateTime? FromDate { get; set; }
+		public DateTime? ToDate { get; set; }
+		public string? League { get; set; }
+		public Guid? TeamId { get; set; }
+	}
+}
