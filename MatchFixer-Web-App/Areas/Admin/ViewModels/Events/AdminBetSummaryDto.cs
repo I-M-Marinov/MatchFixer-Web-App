fde9bc29f7da45a04ca75794ee06e11d2041114c
I-M-Marinov <lcfrrr@gmail.com>
2025-12-09 22:58:41 +0200

    Created the AdminBetSummaryDto

---
+++ MatchFixer-Web-App/Areas/Admin/ViewModels/Events/AdminBetSummaryDto.cs
@@ -0,0 +1,17 @@
+﻿namespace MatchFixer_Web_App.Areas.Admin.ViewModels.Events
+{
+	public class AdminBetSummaryDto
+	{
+		public Guid BetId { get; set; }
+		public string Username { get; set; }
+		public string FullName { get; set; }
+
+		public string Pick { get; set; }
+		public decimal Odds { get; set; }
+		public decimal Stake { get; set; }
+
+		public string Status { get; set; }
+		public decimal? Payout { get; set; }
+	}
+
+}
