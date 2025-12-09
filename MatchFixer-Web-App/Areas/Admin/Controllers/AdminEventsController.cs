6fbaab85d5847a68715e01b6979a9e2ef61c8ccc
I-M-Marinov <lcfrrr@gmail.com>
2025-12-09 23:00:44 +0200

    Created the AdminEventsController

---
+++ MatchFixer-Web-App/Areas/Admin/Controllers/AdminEventsController.cs
@@ -0,0 +1,27 @@
+﻿using MatchFixer.Infrastructure.Security;
+using MatchFixer_Web_App.Areas.Admin.Interfaces;
+using MatchFixer_Web_App.Areas.Admin.ViewModels.Events;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MatchFixer_Web_App.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+	[AdminOnly]
+	public class AdminEventsController : Controller
+	{
+		private readonly IAdminEventsService _adminEventsService;
+
+		public AdminEventsController(IAdminEventsService adminEventsService)
+		{
+			_adminEventsService = adminEventsService;
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> History(AdminEventHistoryFilters filters)
+		{
+			var model = await _adminEventsService.GetFinishedEventsAsync(filters);
+
+			return View(model);
+		}
+	}
+}
