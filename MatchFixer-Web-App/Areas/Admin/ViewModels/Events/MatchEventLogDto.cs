f66130b0c696a5334af1de89c1a23141524e07a7
I-M-Marinov <lcfrrr@gmail.com>
2025-12-09 22:58:58 +0200

    Created the MatchEventLogDto

---
+++ MatchFixer-Web-App/Areas/Admin/ViewModels/Events/MatchEventLogDto.cs
@@ -0,0 +1,15 @@
+﻿namespace MatchFixer_Web_App.Areas.Admin.ViewModels.Events
+{
+	public class MatchEventLogDto
+	{
+		public DateTime ChangedAt { get; set; }
+		public string PropertyName { get; set; }
+		public string? OldValue { get; set; }
+		public string? NewValue { get; set; }
+
+		public string ChangedByFullName { get; set; }
+		public string ChangedByUserName { get; set; }
+
+		public string ChangedBy => $"{ChangedByFullName} ({ChangedByUserName})";
+	}
+}
