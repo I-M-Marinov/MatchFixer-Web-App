ea1828c1d34dde9aaab14063df58fc68aa1c4d73
I-M-Marinov <lcfrrr@gmail.com>
2025-12-09 22:59:19 +0200

    Created the AdminEventOverviewDto

---
+++ MatchFixer-Web-App/Areas/Admin/ViewModels/Events/AdminEventOverviewDto.cs
@@ -0,0 +1,22 @@
+﻿namespace MatchFixer_Web_App.Areas.Admin.ViewModels.Events
+{
+	public class AdminEventOverviewDto
+	{
+		public Guid EventId { get; set; }
+		public string MatchName { get; set; }
+		public string LeagueName { get; set; }
+		public DateTime MatchDate { get; set; }
+		public int? HomeScore { get; set; }
+		public int? AwayScore { get; set; }
+		public bool IsCancelled { get; set; }
+
+		// Betting Summary
+		public int TotalBets { get; set; }
+		public decimal TotalStake { get; set; }
+		public decimal TotalPayout { get; set; }
+		public decimal Profit => TotalStake - TotalPayout;
+
+		public List<AdminBetSummaryDto> Bets { get; set; } = new();
+		public List<MatchEventLogDto> Logs { get; set; } = new();
+	}
+}
