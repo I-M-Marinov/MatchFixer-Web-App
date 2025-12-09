045b55640c605a9c3583a2be71422f1b629e7fc1
I-M-Marinov <lcfrrr@gmail.com>
2025-12-09 22:59:51 +0200

    Created the IAdminEventsService interface

---
+++ MatchFixer-Web-App/Areas/Admin/Interfaces/IAdminEventsService .cs
@@ -0,0 +1,9 @@
+﻿using MatchFixer_Web_App.Areas.Admin.ViewModels.Events;
+
+namespace MatchFixer_Web_App.Areas.Admin.Interfaces
+{
+	public interface IAdminEventsService
+	{
+		Task<List<AdminEventOverviewDto>> GetFinishedEventsAsync(AdminEventHistoryFilters filters);
+	}
+}
