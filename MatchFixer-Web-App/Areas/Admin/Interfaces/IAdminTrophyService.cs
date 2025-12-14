5f8ed95226e239ddf0b8a8911e5e8c80d748cd8a
I-M-Marinov <lcfrrr@gmail.com>
2025-12-14 23:06:37 +0200

    Created the IAdminTrophyService

---
+++ MatchFixer-Web-App/Areas/Admin/Interfaces/IAdminTrophyService.cs
@@ -0,0 +1,9 @@
+﻿namespace MatchFixer_Web_App.Areas.Admin.Interfaces
+{
+	public interface IAdminTrophyService
+	{
+		Task ReevaluateUserTrophiesAsync(Guid userId);
+		Task ReevaluateAllUsersAsync();
+	}
+
+}
