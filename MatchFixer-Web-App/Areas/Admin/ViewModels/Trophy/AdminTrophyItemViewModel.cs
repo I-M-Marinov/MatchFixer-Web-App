a6daf8728a0068875c47657cc9b0ff6ea655b591
I-M-Marinov <lcfrrr@gmail.com>
2025-12-15 23:55:49 +0200

    Created the AdminTrophyItemViewModel

---
+++ MatchFixer-Web-App/Areas/Admin/ViewModels/Trophy/AdminTrophyItemViewModel.cs
@@ -0,0 +1,15 @@
+﻿using MatchFixer.Common.Enums;
+
+namespace MatchFixer_Web_App.Areas.Admin.ViewModels.Trophy
+{
+	public class AdminTrophyItemViewModel
+	{
+		public int TrophyId { get; set; }
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public string IconUrl { get; set; }
+		public TrophyLevel Level { get; set; }
+		public DateTime AwardedOn { get; set; }
+	}
+
+}
