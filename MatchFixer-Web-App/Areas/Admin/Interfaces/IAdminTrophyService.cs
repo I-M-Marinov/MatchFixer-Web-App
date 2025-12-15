aabf6a4638bfc5533a35e499665880dbe6e2e67b
I-M-Marinov <lcfrrr@gmail.com>
2025-12-15 23:22:33 +0200

    Updated the IAdminTrophyService

--- MatchFixer-Web-App/Areas/Admin/Interfaces/IAdminTrophyService.cs
+++ MatchFixer-Web-App/Areas/Admin/Interfaces/IAdminTrophyService.cs
@@ -1,9 +1,15 @@
-﻿namespace MatchFixer_Web_App.Areas.Admin.Interfaces
+﻿using MatchFixer.Core.DTOs.UserTrophyContext;
+using MatchFixer_Web_App.Areas.Admin.ViewModels.Trophy;
+
+namespace MatchFixer_Web_App.Areas.Admin.Interfaces
 {
 	public interface IAdminTrophyService
 	{
 		Task ReevaluateUserTrophiesAsync(Guid userId);
 		Task ReevaluateAllUsersAsync();
+		Task<List<AdminUserTrophyViewModel>> GetUsersWithTrophiesAsync();
+		Task<AdminUserTrophyViewModel?> GetUserWithTrophiesAsync(Guid userId);
+
 	}
 
 }
