eb281aa519d2ff0f5c4332211a1c23b100033cbf
I-M-Marinov <lcfrrr@gmail.com>
2025-12-16 21:14:06 +0200

    Updated the IMatchEventService:
    -- added an overload for AddEventAsync ( + apiFixtureId )
    
    -- added the GetApiLeaguesAsync, MatchExistsByApiFixtureAsync and AddEventFromUpcomingAsync method signatures

--- MatchFixer.Core/Contracts/IMatchEventService.cs
+++ MatchFixer.Core/Contracts/IMatchEventService.cs
@@ -1,5 +1,6 @@
 ﻿using MatchFixer.Core.DTOs.Bets;
 using MatchFixer.Core.ViewModels.LiveEvents;
+using MatchFixer.Infrastructure.Models.FootballAPI;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace MatchFixer.Core.Contracts
@@ -9,10 +10,14 @@ namespace MatchFixer.Core.Contracts
 		Task<List<LiveEventViewModel>> GetAllEventsAsync();
 		Task<List<LiveEventViewModel>> GetLiveEventsAsync();
 		Task AddEventAsync(MatchEventFormModel model);
+		Task AddEventAsync(MatchEventFormModel model, int apiFixtureId);
 		Task<string> GetTeamLogo(string name);
 		Task<Dictionary<string, List<SelectListItem>>> GetTeamsGroupedByLeagueAsync();
 		Task<bool> EditMatchEventAsync(Guid matchEventId, decimal homeOdds, decimal drawOdds, decimal awayOdds, DateTime kickoffTime);
 		Task<bool> CancelMatchEventAsync(Guid matchEventId);
 		Task<Dictionary<Guid, OddsDTO>> GetOddsForMatchesAsync(Guid[] matchIds);
+		Task<List<ApiLeagueSelectViewModel>> GetApiLeaguesAsync();
+		Task<bool> MatchExistsByApiFixtureAsync(int apiFixtureId);
+		Task AddEventFromUpcomingAsync(UpcomingMatchDto dto);
 	}
 }
