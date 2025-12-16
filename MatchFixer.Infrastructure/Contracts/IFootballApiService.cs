e8bc8832e0577c857ef4ee6430c5ac650d626bca
I-M-Marinov <lcfrrr@gmail.com>
2025-12-16 21:14:24 +0200

    Creatred the IFootballApiService

---
+++ MatchFixer.Infrastructure/Contracts/IFootballApiService.cs
@@ -0,0 +1,12 @@
+﻿using MatchFixer.Infrastructure.Models.FootballAPI;
+
+namespace MatchFixer.Infrastructure.Contracts
+{
+	public interface IFootballApiService
+	{
+		Task FetchAndSaveFixturesAsync();
+		Task FetchAndSaveTeamsAsync();
+		Task<List<UpcomingMatchDto>> GetUpcomingFromApiAsync(int leagueId);
+		int? ExtractTeamIdFromLogoUrl(string logoUrl);
+	}
+}
