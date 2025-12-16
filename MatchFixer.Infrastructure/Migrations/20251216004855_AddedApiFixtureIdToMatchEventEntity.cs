545a16a05d52caf94fd7f12d8f8a76c49728bdfe
I-M-Marinov <lcfrrr@gmail.com>
2025-12-16 20:14:04 +0200

    Added the ApiFixtureId to the MatchEvent Entity and updated the DB

---
+++ MatchFixer.Infrastructure/Migrations/20251216004855_AddedApiFixtureIdToMatchEventEntity.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MatchFixer.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedApiFixtureIdToMatchEventEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ApiFixtureId",
+                table: "MatchEvents",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ApiFixtureId",
+                table: "MatchEvents");
+        }
+    }
+}
