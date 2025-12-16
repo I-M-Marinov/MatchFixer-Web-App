d281b433d459513cdcd50f40fadca2b75bbc9f52
I-M-Marinov <lcfrrr@gmail.com>
2025-12-16 22:25:49 +0200

    Created the SupportedApiLeagues static class and the Football static dictionary

---
+++ MatchFixer.Common/FootballLeagues/SupportedApiLeagues.cs
@@ -0,0 +1,24 @@
+﻿namespace MatchFixer.Common.FootballLeagues
+{
+	public static class SupportedApiLeagues
+	{
+		public static readonly Dictionary<int, string> Football = new()
+		{
+			// Top 5
+			{ 39,  "Premier League (England)" },
+			{ 140, "La Liga (Spain)" },
+			{ 135, "Serie A (Italy)" },
+			{ 78,  "Bundesliga (Germany)" },
+			{ 61,  "Ligue 1 (France)" },
+
+			// European leagues
+			{ 88,  "Eredivisie (Netherlands)" },
+			{ 94,  "Primeira Liga (Portugal)" },
+			{ 106, "Ekstraklasa (Poland)" },
+			{ 207, "Super League (Switzerland)" },
+
+			// Regional
+			{ 172, "First Professional League (Bulgaria)" }
+		};
+	}
+}
