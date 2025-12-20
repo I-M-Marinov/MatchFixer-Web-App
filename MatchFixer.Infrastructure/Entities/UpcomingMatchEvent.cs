c60bdbb92a64d359222a242f6ea40b91960a37ca
I-M-Marinov <lcfrrr@gmail.com>
2025-12-20 23:00:52 +0200

    Created the UpcomingMatchEvent Entity

---
+++ MatchFixer.Infrastructure/Entities/UpcomingMatchEvent.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MatchFixer.Infrastructure.Entities
+{
+	public class UpcomingMatchEvent
+	{
+		[Key]
+		public Guid Id { get; set; }
+
+		// API identity
+		[Required]
+		public int ApiFixtureId { get; set; }
+
+		[Required]
+		public int ApiLeagueId { get; set; }
+
+		[Required]
+		public DateTime MatchDateUtc { get; set; }
+
+		// Teams
+		[Required]
+		public Guid HomeTeamId { get; set; }
+
+		[Required]
+		public Guid AwayTeamId { get; set; }
+
+		[ForeignKey(nameof(HomeTeamId))]
+		public Team HomeTeam { get; set; } = null!;
+
+		[ForeignKey(nameof(AwayTeamId))]
+		public Team AwayTeam { get; set; } = null!;
+
+		// State
+		public bool IsCancelled { get; set; }
+		public bool IsImported { get; set; } 
+
+		// Audit
+		public DateTime ImportedAtUtc { get; set; }
+	}
+
+}
