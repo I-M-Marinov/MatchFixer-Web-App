745f330fce2b040ca9543b4b59d93c65177cc2ba
I-M-Marinov <lcfrrr@gmail.com>
2025-12-20 23:01:20 +0200

    Created the new UpcomingMatchEvent entity and updated the DB

---
+++ MatchFixer.Infrastructure/Migrations/20251220201859_AddedTheUpcomingMatchEventEntityToTheDB.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MatchFixer.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedTheUpcomingMatchEventEntityToTheDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UpcomingMatchEvents",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ApiFixtureId = table.Column<int>(type: "int", nullable: false),
+                    ApiLeagueId = table.Column<int>(type: "int", nullable: false),
+                    MatchDateUtc = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    HomeTeamId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    AwayTeamId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    IsCancelled = table.Column<bool>(type: "bit", nullable: false),
+                    IsImported = table.Column<bool>(type: "bit", nullable: false),
+                    ImportedAtUtc = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UpcomingMatchEvents", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UpcomingMatchEvents_Teams_AwayTeamId",
+                        column: x => x.AwayTeamId,
+                        principalTable: "Teams",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_UpcomingMatchEvents_Teams_HomeTeamId",
+                        column: x => x.HomeTeamId,
+                        principalTable: "Teams",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UpcomingMatchEvents_ApiFixtureId",
+                table: "UpcomingMatchEvents",
+                column: "ApiFixtureId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UpcomingMatchEvents_AwayTeamId",
+                table: "UpcomingMatchEvents",
+                column: "AwayTeamId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UpcomingMatchEvents_HomeTeamId",
+                table: "UpcomingMatchEvents",
+                column: "HomeTeamId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UpcomingMatchEvents");
+        }
+    }
+}
