881889738086436cb4e62499ec29fd09df395602
I-M-Marinov <lcfrrr@gmail.com>
2025-12-22 04:44:55 +0200

    Created the UpcomingMatchesSelectionViewModel

---
+++ MatchFixer.Infrastructure/Models/FootballAPI/UpcomingMatchesSelectionViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace MatchFixer.Infrastructure.Models.FootballAPI
+{
+	public class UpcomingMatchesSelectionViewModel
+	{
+		public List<UpcomingMatchDto> Selected { get; set; } = new();
+	}
+
+}
