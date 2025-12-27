e1a1e591c53a972332863eba6a6824c3e728d542
I-M-Marinov <lcfrrr@gmail.com>
2025-12-27 23:31:53 +0200

    Created the OddsBoostConstants

---
+++ MatchFixer.Common/GeneralConstants/OddsBoostConstants.cs
@@ -0,0 +1,7 @@
+﻿namespace MatchFixer.Common.GeneralConstants
+{
+	public static class OddsBoostConstants
+	{
+		public const string OddsBoostCreatedSuccessfully = "The odds boost for this event was created successfully!";
+	}
+}
