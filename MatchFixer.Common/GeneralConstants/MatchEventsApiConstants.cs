9cfb639dc5586362fb1f47d45a2817023811ec3e
I-M-Marinov <lcfrrr@gmail.com>
2025-12-27 23:32:05 +0200

    Created the MatchEventsApiConstants

---
+++ MatchFixer.Common/GeneralConstants/MatchEventsApiConstants.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.RegularExpressions;
+
+namespace MatchFixer.Common.GeneralConstants
+{
+	public static class MatchEventsApiConstants
+	{
+		public const string NoMatchesWereSelected = "Please select at least one match event to add!";
+		public const string MatchEventsWereAddedSuccessfully = "Match events selected were added successfully!";
+		public const string MatchEventWasAddedSuccessfully = "This match event was added successfully!";
+	}
+}
