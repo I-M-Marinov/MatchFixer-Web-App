c71148013f53d94af6eb2c1cec83ac47569a0d7c
I-M-Marinov <lcfrrr@gmail.com>
2025-12-28 23:01:44 +0200

    Updated the database with the new adjustment of the Date property in the MatchResult

---
+++ MatchFixer.Infrastructure/Migrations/20251228203231_UpdateDatePropertyInTheMatchResultEntity.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MatchFixer.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateDatePropertyInTheMatchResultEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "Date",
+                table: "MatchResults",
+                type: "datetimeoffset",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Date",
+                table: "MatchResults",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "datetimeoffset");
+        }
+    }
+}
