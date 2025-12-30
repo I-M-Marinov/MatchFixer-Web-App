58913a83088717dbc2fd838dc9d8c817ac51d9a5
I-M-Marinov <lcfrrr@gmail.com>
2025-12-30 11:38:23 +0200

    Updated the signature of the GetUpcomingEventBetStatsAsync in the interface to take league as a parameter

--- MatchFixer-Web-App/Areas/Admin/Interfaces/IAdminBetInsightsService.cs
+++ MatchFixer-Web-App/Areas/Admin/Interfaces/IAdminBetInsightsService.cs
@@ -5,6 +5,9 @@ namespace MatchFixer_Web_App.Areas.Admin.Interfaces
 	public interface IAdminBetInsightsService
 	{
 		Task<PaginatedEventList<EventBetStatsRow>> GetUpcomingEventBetStatsAsync(
-			int page, int pageSize, CancellationToken ct = default);
+				string? league,
+				int page,
+				int pageSize,
+				CancellationToken ct);
 	}
 }
