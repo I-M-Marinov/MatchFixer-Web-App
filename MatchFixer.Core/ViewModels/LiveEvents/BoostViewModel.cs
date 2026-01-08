77eff7c00818548e60e1fb07e39e53b00ae4b488
I-M-Marinov <lcfrrr@gmail.com>
2026-01-08 23:08:26 +0200

    Added the ID property to the BoostViewModel

--- MatchFixer.Core/ViewModels/LiveEvents/BoostViewModel.cs
+++ MatchFixer.Core/ViewModels/LiveEvents/BoostViewModel.cs
@@ -2,6 +2,7 @@
 {
 	public class BoostViewModel
 	{
+		public Guid Id { get; set; }
 		public decimal BoostValue { get; set; }
 		public DateTime StartUtc { get; set; }
 		public DateTime EndUtc { get; set; }
