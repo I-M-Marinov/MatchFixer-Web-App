5c1f08244a5d93924163b9e814e08cb375a80165
I-M-Marinov <lcfrrr@gmail.com>
2026-01-11 22:29:56 +0200

    Created the BirthdayEmailLogMessages to hold the constant strings used in the BirthdayEmailService

---
+++ MatchFixer.Common/GeneralConstants/BirthdayEmailLogMessages.cs
@@ -0,0 +1,21 @@
+﻿namespace MatchFixer.Common.GeneralConstants
+{
+	public static class BirthdayEmailLogMessages
+	{
+		// Service lifecycle
+		public const string ServiceStarted = "BirthdayEmailService started.";
+		public const string ServiceStopping = "BirthdayEmailService stopping.";
+
+		// Processing
+		public const string NoBirthdaysToday = "No birthdays today.";
+
+		// Email
+		public const string BirthdayEmailSent = "Sent birthday email to {Email}.";
+
+		// Wallet
+		public const string BirthdayBonusAwarded = "Awarded €10 birthday bonus to {Email}.";
+
+		// Errors
+		public const string ProcessingError = "Error occurred while sending birthday emails.";
+	}
+}
