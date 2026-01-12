dc66704e3fbdc1a85aa1514ca0fab3a52548f149
I-M-Marinov <lcfrrr@gmail.com>
2026-01-12 22:56:36 +0200

    Scrapped the original message EventOrEventsCancelledInSlip and created a custom message depending of the reason why a bet would be blocked all to be used with the new enum BettingBlockReason and in the Betting Service

--- MatchFixer.Common/GeneralConstants/BettingServiceConstants.cs
+++ MatchFixer.Common/GeneralConstants/BettingServiceConstants.cs
@@ -4,10 +4,15 @@
 	{
 		public const string NoBetsProvided = "No bets provided.";
 		public const string BetAmountMustBeGreaterThanZero = "Bet amount must be greater than zero.";
-		public const string EventAlreadyStartedInSlip = "An event or events in your slip already started ! Choose other events to bet on.";
 		public const string EventOrEventsCancelledInSlip = "An event or events in your slip was cancelled ! Choose other events to bet on.";
-		public const string BetSlipSubmittedSuccessfully = "Your betting slip was successfully submitted! Good Luck!";
 
+		public const string EventCancelledInSlip = "One or more events in your slip were cancelled.";
+		public const string EventPostponedInSlip = "One or more events in your slip were postponed.";
+		public const string EventAlreadyStartedInSlip = "One or more events in your slip have already started.";
+		public const string EventAlreadyFinishedInSlip = "One or more events in your slip have already finished.";
+		public const string EventNotOpenForBetting = "One or more events are not open for betting.";
+
+		public const string BetSlipSubmittedSuccessfully = "Your betting slip was successfully submitted! Good Luck!";
 		public const string BoostAlreadyUsedMaximumTimesForEvent = "Boost already used maximum times for this event.";
 
 		public static string MaxStakePerBetIs(decimal? maxStakePerBet)
