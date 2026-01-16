9302bfd9e36a0a865b7097a60b0e688ad6ecff1a
I-M-Marinov <lcfrrr@gmail.com>
2026-01-16 23:05:14 +0200

    Created the AdminTeamBettingStatsDto

---
+++ MatchFixer-Web-App/Areas/Admin/ViewModels/Events/AdminTeamBettingStatsDto.cs
@@ -0,0 +1,15 @@
+﻿namespace MatchFixer_Web_App.Areas.Admin.ViewModels.Events
+{
+	public class AdminTeamBettingStatsDto
+	{
+		public Guid TeamId { get; set; }   
+		public string TeamName { get; set; }
+		public string LogoUrl { get; set; }
+
+		public int TotalBets { get; set; }
+		public decimal TotalStake { get; set; }
+		public decimal TotalPayout { get; set; }
+		public decimal Profit => TotalStake - TotalPayout;
+	}
+
+}
