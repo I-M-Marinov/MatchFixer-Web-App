b66174fc37d8e96d08ab078fab8099233bf7bb1c
I-M-Marinov <lcfrrr@gmail.com>
2026-01-22 22:34:40 +0200

    Updated the DB after the addition of the CompetitionName property to the MatchEvent Entity and the not mapped bool property IsCompetitionMatch

---
+++ MatchFixer.Infrastructure/Migrations/20260122172901_Added_CompetitionName_Property_To_MatchEvent_Entity.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MatchFixer.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Added_CompetitionName_Property_To_MatchEvent_Entity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CompetitionName",
+                table: "MatchEvents",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CompetitionName",
+                table: "MatchEvents");
+        }
+    }
+}
