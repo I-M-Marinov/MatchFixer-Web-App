3b96010da44c440b6233a46295e1ae2568a40bf8
I-M-Marinov <lcfrrr@gmail.com>
2026-01-22 22:37:02 +0200

    Created the static FootballCompetitions to hold the names of all the competitions that would be offered in the application

---
+++ MatchFixer.Common/FootballCompetitions/FootballCompetitions.cs
@@ -0,0 +1,9 @@
+﻿namespace MatchFixer.Common.FootballCompetitions
+{
+	public static class FootballCompetitions
+	{
+		public const string ChampionsLeague = "UEFA Champions League";
+		public const string EuropaLeague = "UEFA Europa League";
+		public const string ConferenceLeague = "UEFA Europa Conference League";
+	}
+}
