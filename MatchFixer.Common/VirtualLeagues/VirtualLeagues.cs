121fd5f184477342e70d4ec6e842293b702c6229
I-M-Marinov <lcfrrr@gmail.com>
2026-01-23 18:28:46 +0200

    Created the VirtualLeagues static class

---
+++ MatchFixer.Common/VirtualLeagues/VirtualLeagues.cs
@@ -0,0 +1,8 @@
+﻿namespace MatchFixer.Common.VirtualLeagues
+{
+	public static class VirtualLeagues
+	{
+		public const int RestOfWorldId = -1;
+		public const string RestOfWorldName = "Rest of World";
+	}
+}
