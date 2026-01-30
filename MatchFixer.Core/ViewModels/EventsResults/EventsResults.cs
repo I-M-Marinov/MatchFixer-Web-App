5fd77f3f913ed4d3710d7bc660e79f06c2ae7149
I-M-Marinov <lcfrrr@gmail.com>
2026-01-30 23:45:54 +0200

    Added the CompetitionName and the LeagueName to the EventsResults Model

--- MatchFixer.Core/ViewModels/EventsResults/EventsResults.cs
+++ MatchFixer.Core/ViewModels/EventsResults/EventsResults.cs
@@ -5,8 +5,11 @@
 		public Guid MatchEventId { get; set; }
 		public string HomeTeam { get; set; } = string.Empty;
 		public string AwayTeam { get; set; } = string.Empty;
+		public string? CompetitionName { get; set; }
+		public string LeagueName { get; set; } = null!; 
 		public int HomeScore { get; set; }
 		public int AwayScore { get; set; }
+
 		public string? HomeLogoUrl { get; set; }
 		public string? AwayLogoUrl { get; set; }
 	}
