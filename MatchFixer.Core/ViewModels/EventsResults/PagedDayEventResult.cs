1e62f8da4ee0736dabb8c494f5d4dd932c1efaf9
I-M-Marinov <lcfrrr@gmail.com>
2026-01-30 23:45:29 +0200

    Created the PagedDayEventResult<T> model

---
+++ MatchFixer.Core/ViewModels/EventsResults/PagedDayEventResult.cs
@@ -0,0 +1,15 @@
+﻿namespace MatchFixer.Core.ViewModels.EventsResults
+{
+	public class PagedDayEventResult<T>
+	{
+		public DateOnly Day { get; init; }
+		public IReadOnlyList<T> Items { get; init; } = [];
+
+		public int DayIndex { get; init; }          
+		public int TotalDays { get; init; }
+
+		public bool HasPreviousDay => DayIndex < TotalDays - 1;
+		public bool HasNextDay => DayIndex > 0;
+	}
+
+}
