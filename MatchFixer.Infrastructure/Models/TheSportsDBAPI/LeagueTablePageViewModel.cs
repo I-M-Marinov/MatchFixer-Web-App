5befe25f12339341727615583cfb3769c553143a
I-M-Marinov <lcfrrr@gmail.com>
2026-02-01 13:44:29 +0200

    Created the LeagueTablePageViewModel

---
+++ MatchFixer.Infrastructure/Models/TheSportsDBAPI/LeagueTablePageViewModel.cs
@@ -0,0 +1,11 @@
+﻿using MatchFixer.Common.Enums;
+
+namespace MatchFixer.Infrastructure.Models.TheSportsDBAPI
+{
+	public class LeagueTablePageViewModel
+	{
+		public InternalLeague SelectedLeague { get; set; }
+		public List<LeagueTableRowApiDto> Table { get; set; } = new();
+	}
+
+}
