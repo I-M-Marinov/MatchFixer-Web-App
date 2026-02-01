bb1c208bdbe5220f350f73ca23275b3ff02c7837
I-M-Marinov <lcfrrr@gmail.com>
2026-02-01 13:42:09 +0200

    Created the LeagueTableApiResponse

---
+++ MatchFixer.Infrastructure/Models/TheSportsDBAPI/LeagueTableApiResponse.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+namespace MatchFixer.Infrastructure.Models.TheSportsDBAPI
+{
+	public class LeagueTableApiResponse
+	{
+		[JsonPropertyName("table")]
+		public List<LeagueTableRowApiDto> Table { get; set; }
+	}
+}
