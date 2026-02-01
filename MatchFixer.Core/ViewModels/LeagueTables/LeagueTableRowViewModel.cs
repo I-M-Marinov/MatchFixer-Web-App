06a9ca9dac780798a6b461f661e28395c4e5a2f6
I-M-Marinov <lcfrrr@gmail.com>
2026-02-01 13:44:38 +0200

    Created the LeagueTableRowViewModel

---
+++ MatchFixer.Core/ViewModels/LeagueTables/LeagueTableRowViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace MatchFixer.Core.ViewModels.LeagueTables
+{
+	public class LeagueTableRowViewModel
+	{
+		public int Rank { get; set; }
+		public string Team { get; set; }
+		public string Badge { get; set; }
+		public int Played { get; set; }
+		public int Wins { get; set; }
+		public int Draws { get; set; }
+		public int Losses { get; set; }
+		public int GoalDiff { get; set; }
+		public int Points { get; set; }
+	}
+
+}
