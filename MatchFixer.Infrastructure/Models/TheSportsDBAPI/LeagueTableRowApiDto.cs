ef486f0614f3f3b16e608606c39238ce16c2aeae
I-M-Marinov <lcfrrr@gmail.com>
2026-02-01 13:41:38 +0200

    Created the LeagueTableRowApiDto

---
+++ MatchFixer.Infrastructure/Models/TheSportsDBAPI/LeagueTableRowApiDto.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.Json.Serialization;
+
+public class LeagueTableRowApiDto
+{
+	[JsonPropertyName("intRank")]
+	public string Rank { get; set; } = "0";
+
+	[JsonPropertyName("strTeam")]
+	public string Team { get; set; } = string.Empty;
+
+	[JsonPropertyName("strBadge")]
+	public string Badge { get; set; } = string.Empty;
+
+	[JsonPropertyName("intPlayed")]
+	public string Played { get; set; } = "0";
+
+	[JsonPropertyName("intWin")]
+	public string Wins { get; set; } = "0";
+
+	[JsonPropertyName("intDraw")]
+	public string Draws { get; set; } = "0";
+
+	[JsonPropertyName("intLoss")]
+	public string Losses { get; set; } = "0";
+
+	[JsonPropertyName("intGoalDifference")]
+	public string GoalDifference { get; set; } = "0";
+
+	[JsonPropertyName("intPoints")]
+	public string Points { get; set; } = "0";
+}
