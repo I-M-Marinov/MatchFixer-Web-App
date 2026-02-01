7a8914c32373854e8b4ad68a2b90138f2bb5a977
I-M-Marinov <lcfrrr@gmail.com>
2026-02-01 13:43:27 +0200

    Created the ILeagueTableService interface

---
+++ MatchFixer.Core/Contracts/ILeagueTableService.cs
@@ -0,0 +1,13 @@
+﻿using MatchFixer.Common.Enums;
+using MatchFixer.Core.ViewModels.LeagueTables;
+
+namespace MatchFixer.Core.Contracts
+{
+	public interface ILeagueTableService
+	{
+		Task<List<LeagueTableRowViewModel>> GetLeagueTableAsync(
+			InternalLeague league,
+			string? season = null,
+			CancellationToken ct = default);
+	}
+}
