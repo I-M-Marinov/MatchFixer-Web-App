0794bff84c25fe30e2074bd6340ac235907a8f1d
I-M-Marinov <lcfrrr@gmail.com>
2026-02-01 13:39:48 +0200

    Created the InternalLeague enum

---
+++ MatchFixer.Common/Enums/InternalLeague.cs
@@ -0,0 +1,17 @@
+﻿namespace MatchFixer.Common.Enums
+{
+	public enum InternalLeague
+	{
+		PremierLeague,
+		ParvaLiga,
+		Bundesliga,
+		LaLiga,
+		Eredivisie,
+		Ligue1,
+		SerieA,
+		LigaPortugal,
+		SwissLeague,
+		Ekstraklasa
+	}
+
+}
