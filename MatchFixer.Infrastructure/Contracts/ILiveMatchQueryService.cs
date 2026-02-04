7890c39079a2d36f739891a10e8db6f25d7be418
I-M-Marinov <lcfrrr@gmail.com>
2026-02-04 22:36:26 +0200

    Created the interface ILiveMatchQueryService

---
+++ MatchFixer.Infrastructure/Contracts/ILiveMatchQueryService.cs
@@ -0,0 +1,12 @@
+﻿using MatchFixer.Common.Enums;
+using MatchFixer.Infrastructure.Models.TheSportsDBAPI;
+
+namespace MatchFixer.Infrastructure.Contracts
+{
+	public interface ILiveMatchQueryService
+	{
+		Task<IReadOnlyList<LiveTeamMatchInfo>> GetLiveMatchesForLeagueAsync(
+			InternalLeague league,
+			CancellationToken ct = default);
+	}
+}
