0d92d6e3efa94f55b30903168384625948ffddda
I-M-Marinov <lcfrrr@gmail.com>
2026-02-04 22:38:59 +0200

    Created the LiveTeamMatchInfo

---
+++ MatchFixer.Infrastructure/Models/TheSportsDBAPI/LiveTeamMatchInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace MatchFixer.Infrastructure.Models.TheSportsDBAPI
+{
+	public class LiveTeamMatchInfo
+	{
+		public string TeamName { get; set; } = null!;
+		public string Opponent { get; set; } = null!;
+		public int GoalsFor { get; set; }
+		public int GoalsAgainst { get; set; }
+	}
+}
