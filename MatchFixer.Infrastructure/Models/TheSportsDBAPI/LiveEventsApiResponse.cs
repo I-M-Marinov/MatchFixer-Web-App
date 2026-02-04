755d679706b3510e0a9f5ef926b8c3e4461fb12c
I-M-Marinov <lcfrrr@gmail.com>
2026-02-04 22:39:08 +0200

    Created the LiveEventsApiResponse

---
+++ MatchFixer.Infrastructure/Models/TheSportsDBAPI/LiveEventsApiResponse.cs
@@ -0,0 +1,12 @@
+﻿
+using System.Text.Json.Serialization;
+
+
+namespace MatchFixer.Infrastructure.Models.TheSportsDBAPI
+{
+	public class LiveEventsApiResponse
+	{
+		[JsonPropertyName("events")]
+		public List<LiveEventApiDto>? Events { get; set; }
+	}
+}
