9caf8535c7be38ad924719f5682fc2a37f1f9659
I-M-Marinov <lcfrrr@gmail.com>
2026-02-04 22:39:18 +0200

    Created the LiveEventApiDto

---
+++ MatchFixer.Infrastructure/Models/TheSportsDBAPI/LiveEventApiDto.cs
@@ -0,0 +1,14 @@
+﻿
+namespace MatchFixer.Infrastructure.Models.TheSportsDBAPI
+{
+	public class LiveEventApiDto
+	{
+		public string? strHomeTeam { get; set; }
+		public string? strAwayTeam { get; set; }
+
+		public string? intHomeScore { get; set; }
+		public string? intAwayScore { get; set; }
+
+		public string? strStatus { get; set; } 
+	}
+}
