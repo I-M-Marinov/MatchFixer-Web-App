387ea114dd87e678245826bdaff794ba6bc8a653
I-M-Marinov <lcfrrr@gmail.com>
2026-02-06 23:45:23 +0200

    Added the idEvent / idLeague  and the strLeague to the LiveEventApiDto ( for later use if need be )

--- MatchFixer.Infrastructure/Models/TheSportsDBAPI/LiveEventApiDto.cs
+++ MatchFixer.Infrastructure/Models/TheSportsDBAPI/LiveEventApiDto.cs
@@ -3,12 +3,17 @@ namespace MatchFixer.Infrastructure.Models.TheSportsDBAPI
 {
 	public class LiveEventApiDto
 	{
+		public string? idEvent { get; set; }
+		public string? idLeague { get; set; }
+		public string? strLeague { get; set; }
+
 		public string? strHomeTeam { get; set; }
 		public string? strAwayTeam { get; set; }
 
 		public string? intHomeScore { get; set; }
 		public string? intAwayScore { get; set; }
 
-		public string? strStatus { get; set; } 
+		public string? strStatus { get; set; }
 	}
+
 }
