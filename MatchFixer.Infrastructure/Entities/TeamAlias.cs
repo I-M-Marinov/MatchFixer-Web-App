179bb24626a9caf44f2cefea9d522c7e0d138006
I-M-Marinov <lcfrrr@gmail.com>
2026-02-07 23:40:30 +0200

    Created the TeamAlias Entity

---
+++ MatchFixer.Infrastructure/Entities/TeamAlias.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace MatchFixer.Infrastructure.Entities
+{
+	public class TeamAlias
+	{
+		[Key]
+		public int Id { get; set; }
+
+		[Required]
+		[StringLength(150)]
+		public string Alias { get; set; } = null!;
+
+		public Guid TeamId { get; set; }
+		public Team Team { get; set; } = null!;
+
+		public string? Source { get; set; } 
+	}
+
+}
