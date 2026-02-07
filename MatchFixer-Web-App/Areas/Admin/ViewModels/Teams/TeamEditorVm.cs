2f3d10b1eed98986d5bf2045bd42ea8116c6683f
I-M-Marinov <lcfrrr@gmail.com>
2026-02-07 23:51:10 +0200

    Added the LeagueId and Leagues to the TeamEditorVm

---
+++ MatchFixer-Web-App/Areas/Admin/ViewModels/Teams/TeamEditorVm.cs
@@ -0,0 +1,21 @@
+﻿using MatchFixer_Web_App.Areas.Admin.ViewModels.Teams.DTO;
+
+namespace MatchFixer_Web_App.Areas.Admin.ViewModels.Teams
+{
+	public class TeamEditorVm
+	{
+		public Guid TeamId { get; set; }
+
+		// Team fields
+		public string Name { get; set; } = "";
+		public string LogoUrl { get; set; } = "";
+		public string LeagueName { get; set; } = "";
+		public int? ApiTeamId { get; set; }
+
+		public int LeagueId { get; set; }
+		public IReadOnlyList<LeagueOptionVm> Leagues { get; set; } = [];
+
+		// Aliases
+		public List<TeamAliasVm> Aliases { get; set; } = new();
+	}
+}
