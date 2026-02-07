07dd0421df763a500bd45a6592db3d1281a6fda3
I-M-Marinov <lcfrrr@gmail.com>
2026-02-07 23:41:39 +0200

    Created the TeamNameResolver service

---
+++ MatchFixer.Infrastructure/Services/TeamNameResolver.cs
@@ -0,0 +1,31 @@
+﻿using MatchFixer.Infrastructure.Contracts;
+using MatchFixer.Infrastructure.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace MatchFixer.Infrastructure.Services
+{
+	public class TeamNameResolver : ITeamNameResolver
+	{
+		private readonly MatchFixerDbContext _db;
+
+		public TeamNameResolver(MatchFixerDbContext db)
+		{
+			_db = db;
+		}
+
+		public async Task<Team?> ResolveTeamAsync(string inputName)
+		{
+			if (string.IsNullOrWhiteSpace(inputName))
+				return null;
+
+			var alias = await _db.TeamAliases
+				.AsNoTracking()
+				.Include(a => a.Team)
+				.FirstOrDefaultAsync(a => a.Alias.ToLower() == inputName.ToLower());
+
+			return alias?.Team;
+
+		}
+	}
+
+}
