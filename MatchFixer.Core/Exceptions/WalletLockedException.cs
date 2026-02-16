17a5bb1371f5a4a2f886d71f1ac79cfc72857bee
I-M-Marinov <lcfrrr@gmail.com>
2026-02-16 22:58:25 +0200

    Created the custom exception ---> WalletLockedException

---
+++ MatchFixer.Core/Exceptions/WalletLockedException.cs
@@ -0,0 +1,10 @@
+﻿namespace MatchFixer.Core.Exceptions;
+
+public class WalletLockedException : Exception
+{
+	public WalletLockedException(string? message = null)
+		: base(message ?? "Your wallet is currently locked.")
+	{
+	}
+}
+
