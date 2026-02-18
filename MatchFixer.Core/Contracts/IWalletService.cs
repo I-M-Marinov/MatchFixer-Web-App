a3e51588dc0207710bd9b81b12d0c11e07993e04
I-M-Marinov <lcfrrr@gmail.com>
2026-02-18 23:53:35 +0200

    Updated the signatures for the DeductForBetAsync and the AwardWinningsAsync methods in the IWalletService interface

--- MatchFixer.Core/Contracts/IWalletService.cs
+++ MatchFixer.Core/Contracts/IWalletService.cs
@@ -13,8 +13,8 @@ namespace MatchFixer.Core.Contracts
 		Task DepositAsync(decimal amount, string description = null);
 		Task<bool> WithdrawAsync(decimal amount, string description = null);
 		Task<(bool Success, string Message)> ClearTransactionHistoryAsync();
-		Task<(bool Success, string Message)> DeductForBetAsync(Guid userId, decimal amount);
-		Task AwardWinningsAsync(Guid userId, decimal amount, string matchDescription);
+		Task<(bool Success, string Message)> DeductForBetAsync(Guid userId, decimal amount, Guid betslipId);
+		Task AwardWinningsAsync(Guid userId, decimal amount, string betslipDescription);
 		Task<bool> AwardBirthdayBonusAsync(Guid userId);
 		Task<bool> RefundBetAsync(Guid userId, decimal amount, Guid betSlipId);
 		Task<bool> HasTransactionForSlipAsync(Guid userId, WalletTransactionType type, Guid betSlipId);
