8bf5d3e059a0f4ade55f633eb09d1514996ad6d2
I-M-Marinov <lcfrrr@gmail.com>
2026-02-20 23:56:55 +0200

    Created the AdminUserBetsConstants

---
+++ MatchFixer.Common/Admin/AdminUserBetsConstants.cs
@@ -0,0 +1,12 @@
+﻿namespace MatchFixer.Common.Admin
+{
+	public static class AdminUserBetsConstants
+	{
+		public const string UserBetWonStatusBadge = "bg-success";
+		public const string UserBetLostStatusBadge = "bg-danger";
+		public const string UserBetVoidedStatusBadge = "bg-secondary text-white";
+		public const string UserBetPendingStatusBadge = "bg-warning text-dark";
+		public const string UserBetStatusBadgeFallback = "bg-light text-dark";
+
+	}
+}
