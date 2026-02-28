9780674a252cac11532420474ed2032d17d4e4e6
I-M-Marinov <lcfrrr@gmail.com>
2026-02-28 20:24:42 +0200

    Updated the DB with the new properties in the MatchEvent Entity

---
+++ MatchFixer.Infrastructure/Migrations/20260228130801_Added_The_Status_Property_To_The_MatchEvent_Entity.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MatchFixer.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Added_The_Status_Property_To_The_MatchEvent_Entity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "FinishedAtUtc",
+                table: "MatchEvents",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "MatchEvents",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FinishedAtUtc",
+                table: "MatchEvents");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "MatchEvents");
+        }
+    }
+}
