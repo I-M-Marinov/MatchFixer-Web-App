f453c23228fa81285821f8a5918ce966e726a83a
I-M-Marinov <lcfrrr@gmail.com>
2026-03-01 22:14:55 +0200

    Added all strings from the Odds Boost Service to the OddsBoostConstants

--- MatchFixer.Common/GeneralConstants/OddsBoostConstants.cs
+++ MatchFixer.Common/GeneralConstants/OddsBoostConstants.cs
@@ -3,5 +3,10 @@
 	public static class OddsBoostConstants
 	{
 		public const string OddsBoostCreatedSuccessfully = "The odds boost for this event was created successfully!";
+		public const string BoostValueMustBePositive = "Boost value must be greater than zero.";
+		public const string DurationMustBePositive = "Duration must be greater than zero.";
+		public const string MatchEventNotFound = "Match event not found.";
+		public const string OverlappingBoostExists = "An active boost already overlaps with this timeframe.";
+		public const string BoostNotFound = "Odds boost not found.";
 	}
 }
