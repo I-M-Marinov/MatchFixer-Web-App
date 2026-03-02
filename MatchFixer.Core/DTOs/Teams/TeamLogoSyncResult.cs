05d8c1829cd8f10d67ca398b983edaac26aa632b
I-M-Marinov <lcfrrr@gmail.com>
2026-03-02 22:28:19 +0200

    Created the TeamLogoSyncResult

---
+++ MatchFixer.Core/DTOs/Teams/TeamLogoSyncResult.cs
@@ -0,0 +1,12 @@
+﻿namespace MatchFixer.Core.DTOs.Teams
+{
+	public class TeamLogoSyncResult
+	{
+		public int TotalProcessed { get; set; }
+		public int Downloaded { get; set; }
+		public int Skipped { get; set; }
+		public int Failed { get; set; }
+
+		public List<string> Errors { get; set; } = new();
+	}
+}
