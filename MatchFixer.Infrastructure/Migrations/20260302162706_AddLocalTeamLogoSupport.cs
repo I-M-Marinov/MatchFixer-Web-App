4a3bfc96e9ddc855e75efd00d3583a9e639535ba
I-M-Marinov <lcfrrr@gmail.com>
2026-03-02 22:25:26 +0200

    Updated the DB with the new Team properties added --->LocalLogoUrl and LogoLastSyncedUtc

---
+++ MatchFixer.Infrastructure/Migrations/20260302162706_AddLocalTeamLogoSupport.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MatchFixer.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLocalTeamLogoSupport : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "LocalLogoUrl",
+                table: "Teams",
+                type: "nvarchar(250)",
+                maxLength: 250,
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LogoLastSyncedUtc",
+                table: "Teams",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LocalLogoUrl",
+                table: "Teams");
+
+            migrationBuilder.DropColumn(
+                name: "LogoLastSyncedUtc",
+                table: "Teams");
+        }
+    }
+}
