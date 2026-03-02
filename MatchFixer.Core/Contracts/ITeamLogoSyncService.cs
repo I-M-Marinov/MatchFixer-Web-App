13eaec4961eb1160943da445290b8b0cbfeea09d
I-M-Marinov <lcfrrr@gmail.com>
2026-03-02 22:26:30 +0200

    Created the ITeamLogoSyncService

---
+++ MatchFixer.Core/Contracts/ITeamLogoSyncService.cs
@@ -0,0 +1,18 @@
+﻿
+using MatchFixer.Core.DTOs.Teams;
+
+namespace MatchFixer.Core.Contracts
+{
+	public interface ITeamLogoSyncService
+	{
+		
+		Task<TeamLogoSyncResult> SyncAllTeamLogosAsync(
+			bool forceRedownload = false,
+			CancellationToken ct = default);
+
+		Task<TeamLogoSyncResult> SyncTeamLogoAsync(
+			Guid teamId,
+			bool forceRedownload = false,
+			CancellationToken ct = default);
+	}
+}
