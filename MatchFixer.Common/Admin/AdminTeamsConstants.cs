9a00c13979f63024cbaa4b17889c6662b34ba9db
I-M-Marinov <lcfrrr@gmail.com>
2026-03-05 22:49:06 +0200

    Created the AdminTeamsConstants

---
+++ MatchFixer.Common/Admin/AdminTeamsConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace MatchFixer.Common.Admin
+{
+	public static class AdminTeamsConstants
+	{
+		public const string InvalidTeamData = "Invalid team data.";
+		public const string TeamUpdateFailed = "Failed to update team.";
+
+		public const string TeamAddedSuccessfully = "Team added successfully.";
+		public const string TeamAlreadyExists = "This team already exists.";
+	}
+}
