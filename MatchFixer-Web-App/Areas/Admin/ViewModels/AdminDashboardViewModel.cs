81ad6bdad3cf17ab2321efa8b2841fbe82832734
I-M-Marinov <lcfrrr@gmail.com>
2026-03-06 23:43:10 +0200

    Updated the AdminDashboardViewModel. Added the following properties ---->
    -- Exposure
    -- BetsActivity
    -- WalletActivity
    --  TopWinners
    -- TopLosers
    -- HotMatches

--- MatchFixer-Web-App/Areas/Admin/ViewModels/AdminDashboardViewModel.cs
+++ MatchFixer-Web-App/Areas/Admin/ViewModels/AdminDashboardViewModel.cs
@@ -1,4 +1,5 @@
-﻿using MatchFixer_Web_App.Areas.Admin.ViewModels.MatchEvents;
+﻿using MatchFixer_Web_App.Areas.Admin.ViewModels.Dashboard;
+using MatchFixer_Web_App.Areas.Admin.ViewModels.MatchEvents;
 
 namespace MatchFixer_Web_App.Areas.Admin.ViewModels
 {
@@ -11,13 +12,21 @@ namespace MatchFixer_Web_App.Areas.Admin.ViewModels
 		public int BannedUsers { get; set; }
 
 		public decimal TotalWalletBalance { get; set; }
-		public decimal TotalTransactions { get; set; }
+		public int TotalTransactions { get; set; }
 
 		public int TotalBets { get; set; }
 		public int PendingBets { get; set; }
 		public int WonBets { get; set; }
 		public int LostBets { get; set; }
-		public AdminEventsSummaryViewModel Events { get; set; } = new();
+
+		public PlatformExposureViewModel Exposure { get; set; }
+		public BetsActivityViewModel BetsActivity { get; set; }
+		public WalletActivityViewModel WalletActivity { get; set; }
+
+		public List<TopUserWinLossRow> TopWinners { get; set; } = new();
+		public List<TopUserWinLossRow> TopLosers { get; set; } = new();
+
+		public List<HotMatchRow> HotMatches { get; set; } = new();
 
 	}
 }
