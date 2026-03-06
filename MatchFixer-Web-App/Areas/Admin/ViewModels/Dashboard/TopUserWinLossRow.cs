750f6aa502108c68355054043b60f7e38573648c
I-M-Marinov <lcfrrr@gmail.com>
2026-03-06 23:41:51 +0200

    Created the TopUserWinLossRow

---
+++ MatchFixer-Web-App/Areas/Admin/ViewModels/Dashboard/TopUserWinLossRow.cs
@@ -0,0 +1,10 @@
+﻿namespace MatchFixer_Web_App.Areas.Admin.ViewModels.Dashboard
+{
+	public class TopUserWinLossRow
+	{
+		public Guid UserId { get; set; }
+		public string Username { get; set; }
+		public decimal Profit { get; set; }
+		public string Email { get; set; }
+	}
+}
