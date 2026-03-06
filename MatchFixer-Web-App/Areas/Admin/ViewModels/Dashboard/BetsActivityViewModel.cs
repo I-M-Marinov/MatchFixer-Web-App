9bd0245f3dae2f50cfbd748f44b8ecb364237719
I-M-Marinov <lcfrrr@gmail.com>
2026-03-06 23:41:23 +0200

    Created the BetsActivityViewModel

---
+++ MatchFixer-Web-App/Areas/Admin/ViewModels/Dashboard/BetsActivityViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MatchFixer_Web_App.Areas.Admin.ViewModels.Dashboard
+{
+	public class BetsActivityViewModel
+	{
+		public int BetsToday { get; set; }
+		public int BetsYesterday { get; set; }
+		public int BetsThisWeek { get; set; }
+	}
+}
