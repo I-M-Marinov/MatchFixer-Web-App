bc00bc35dd1e8179c286f7360d4bf3ef451e596c
I-M-Marinov <lcfrrr@gmail.com>
2026-03-06 23:41:41 +0200

    Created the PlatformExposureViewModel

---
+++ MatchFixer-Web-App/Areas/Admin/ViewModels/Dashboard/PlatformExposureViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MatchFixer_Web_App.Areas.Admin.ViewModels.Dashboard
+{
+	public class PlatformExposureViewModel
+	{
+		public decimal TotalStakesToday { get; set; }
+		public decimal PotentialPayout { get; set; }
+		public decimal Exposure => PotentialPayout - TotalStakesToday;
+	}
+}
