4e3f6e63104c337ae654c0065f3a2e31f4537166
I-M-Marinov <lcfrrr@gmail.com>
2026-03-06 23:41:32 +0200

    Created the HotMatchRow

---
+++ MatchFixer-Web-App/Areas/Admin/ViewModels/Dashboard/HotMatchRow.cs
@@ -0,0 +1,15 @@
+﻿namespace MatchFixer_Web_App.Areas.Admin.ViewModels.Dashboard
+{
+	public class HotMatchRow
+	{
+		public Guid MatchId { get; set; }
+
+		public string HomeTeam { get; set; }
+		public string AwayTeam { get; set; }
+
+		public string HomeLogo { get; set; }
+		public string AwayLogo { get; set; }
+
+		public int BetsCount { get; set; }
+	}
+}
