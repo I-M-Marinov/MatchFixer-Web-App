c0603c3c5943e49a6913d0835ceef48ad6bf55dc
I-M-Marinov <lcfrrr@gmail.com>
2026-03-06 23:42:02 +0200

    Created the WalletActivityViewModel

---
+++ MatchFixer-Web-App/Areas/Admin/ViewModels/Dashboard/WalletActivityViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MatchFixer_Web_App.Areas.Admin.ViewModels.Dashboard
+{
+	public class WalletActivityViewModel
+	{
+		public decimal DepositsToday { get; set; }
+		public decimal WithdrawalsToday { get; set; }
+		public decimal NetFlow => DepositsToday - WithdrawalsToday;
+	}
+}
