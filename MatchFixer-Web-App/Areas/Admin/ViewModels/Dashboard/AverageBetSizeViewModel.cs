4e72ddcff5c80d90e88f84b8360f89c9e12b5ccf
I-M-Marinov <lcfrrr@gmail.com>
2026-03-08 22:04:29 +0200

    Created the AverageBetSizeViewModel

---
+++ MatchFixer-Web-App/Areas/Admin/ViewModels/Dashboard/AverageBetSizeViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace MatchFixer_Web_App.Areas.Admin.ViewModels.Dashboard
+{
+	public class AverageBetSizeViewModel
+	{
+		public string Period { get; set; }
+		public decimal AverageAmount { get; set; }
+	}
+}
