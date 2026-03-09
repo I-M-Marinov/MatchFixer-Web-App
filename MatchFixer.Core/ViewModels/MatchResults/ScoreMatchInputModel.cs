f4a6e7ed6a2daabd6a75c8bfd8442ba5f4170c69
I-M-Marinov <lcfrrr@gmail.com>
2026-03-09 22:02:15 +0200

    Created the ScoreMatchInputModel

---
+++ MatchFixer.Core/ViewModels/MatchResults/ScoreMatchInputModel.cs
@@ -0,0 +1,21 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+
+namespace MatchFixer.Core.ViewModels.MatchResults
+{
+	public class ScoreMatchInputModel
+	{
+		public Guid MatchId { get; set; }
+
+		[Required]
+		[Range(0, 20, ErrorMessage = "Score must be between 0 and 20.")]
+		public int HomeScore { get; set; }
+
+		[Required]
+		[Range(0, 20, ErrorMessage = "Score must be between 0 and 20.")]
+		public int AwayScore { get; set; }
+
+		public string? Notes { get; set; }
+	}
+}
