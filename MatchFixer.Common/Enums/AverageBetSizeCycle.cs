98153720e18599ef65c31f20df72b628afe986d7
I-M-Marinov <lcfrrr@gmail.com>
2026-03-16 23:02:58 +0200

    Created the AverageBetSizeCycle

---
+++ MatchFixer.Common/Enums/AverageBetSizeCycle.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MatchFixer.Common.Enums
+{
+	public enum AverageBetSizeCycle
+	{
+		[Display(Name = "Today")]
+		Today = 0,
+		[Display(Name = "Yesterday")]
+		Yesterday = 1,
+		[Display(Name = "This Week")]
+		ThisWeek = 2,
+	}
+}
