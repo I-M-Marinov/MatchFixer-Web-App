b6ca7210b2c747af7606e05fb3a671418e96ccfa
I-M-Marinov <lcfrrr@gmail.com>
2026-03-16 23:03:33 +0200

    Updated the AverageBetSizeViewModel's Period to be AverageBetSizeCycle instead of the string

--- MatchFixer-Web-App/Areas/Admin/ViewModels/Dashboard/AverageBetSizeViewModel.cs
+++ MatchFixer-Web-App/Areas/Admin/ViewModels/Dashboard/AverageBetSizeViewModel.cs
@@ -1,8 +1,10 @@
-﻿namespace MatchFixer_Web_App.Areas.Admin.ViewModels.Dashboard
+﻿using MatchFixer.Common.Enums;
+
+namespace MatchFixer_Web_App.Areas.Admin.ViewModels.Dashboard
 {
 	public class AverageBetSizeViewModel
 	{
-		public string Period { get; set; }
+		public AverageBetSizeCycle Period { get; set; }
 		public decimal AverageAmount { get; set; }
 	}
 }
