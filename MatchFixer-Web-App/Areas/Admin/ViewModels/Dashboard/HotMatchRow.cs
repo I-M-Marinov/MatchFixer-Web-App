829b677808daead4856fb94adcdfe981f889eca9
I-M-Marinov <lcfrrr@gmail.com>
2026-03-19 22:12:05 +0200

    Added the local logo url properties for the home and away teams and the EffectiveHomeTeamLogo + EffectiveAwayTeamLogo to the HotMatchRow View Model

--- MatchFixer-Web-App/Areas/Admin/ViewModels/Dashboard/HotMatchRow.cs
+++ MatchFixer-Web-App/Areas/Admin/ViewModels/Dashboard/HotMatchRow.cs
@@ -7,8 +7,19 @@
 		public string HomeTeam { get; set; }
 		public string AwayTeam { get; set; }
 
-		public string HomeLogo { get; set; }
-		public string AwayLogo { get; set; }
+		public string HomeLogo { get; set; } // API
+		public string AwayLogo { get; set; } // API
+		public string? HomeTeamLocalLogoUrl { get; set; } // LOCAL
+		public string? AwayTeamLocalLogoUrl { get; set; } // LOCAL
+		public string? EffectiveHomeTeamLogo =>
+			!string.IsNullOrWhiteSpace(HomeTeamLocalLogoUrl)
+				? HomeTeamLocalLogoUrl
+				: HomeLogo;
+
+		public string? EffectiveAwayTeamLogo =>
+			!string.IsNullOrWhiteSpace(AwayTeamLocalLogoUrl)
+				? AwayTeamLocalLogoUrl
+				: AwayLogo;
 
 		public int BetsCount { get; set; }
 	}
