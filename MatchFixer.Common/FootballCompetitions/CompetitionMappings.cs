c757dde7e8cb32d8c276ed623a7433b9fe74ffdb
I-M-Marinov <lcfrrr@gmail.com>
2026-03-21 23:28:52 +0200

    Created the CompetitionMappings

---
+++ MatchFixer.Common/FootballCompetitions/CompetitionMappings.cs
@@ -0,0 +1,19 @@
+﻿namespace MatchFixer.Common.FootballCompetitions
+{
+	using MatchFixer.Common.VirtualLeagues;
+	public static class CompetitionMappings
+	{
+		public static readonly Dictionary<string, int> NameToId = new()
+		{
+			// UEFA
+			{ FootballCompetitions.ChampionsLeague, 1001 },
+			{ FootballCompetitions.EuropaLeague, 1002 },
+			{ FootballCompetitions.ConferenceLeague, 1003 },
+
+			// International
+			{ VirtualLeagues.InternationalName, VirtualLeagues.InternationalId },
+			{ VirtualLeagues.WorldCupName, VirtualLeagues.WorldCupId },
+			{ VirtualLeagues.EuroName, VirtualLeagues.EuroId }
+		};
+	}
+}
