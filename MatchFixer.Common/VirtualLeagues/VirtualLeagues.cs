71c8e5138f7f34bbce5bfd5a2fc77dbb8230a1de
I-M-Marinov <lcfrrr@gmail.com>
2026-03-21 23:28:42 +0200

    Added the International / World Cup and Euro to the VirtualLeagues

--- MatchFixer.Common/VirtualLeagues/VirtualLeagues.cs
+++ MatchFixer.Common/VirtualLeagues/VirtualLeagues.cs
@@ -4,5 +4,15 @@
 	{
 		public const int RestOfWorldId = -1;
 		public const string RestOfWorldName = "Rest of World";
+
+		public const int InternationalId = -2;
+		public const string InternationalName = "International";
+
+		public const int WorldCupId = -4;
+		public const string WorldCupName = "FIFA World Cup";
+
+		public const int EuroId = -5;
+		public const string EuroName = "UEFA Euro";
+
 	}
 }
