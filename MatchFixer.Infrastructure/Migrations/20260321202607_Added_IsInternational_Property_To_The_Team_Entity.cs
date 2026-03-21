f53c42daa19a6e3654b2095ea2526e38142d291f
I-M-Marinov <lcfrrr@gmail.com>
2026-03-21 23:28:15 +0200

    Updated the DB with the new IsNationalTeam bool property that was added to the Team Entity

---
+++ MatchFixer.Infrastructure/Migrations/20260321202607_Added_IsInternational_Property_To_The_Team_Entity.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MatchFixer.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Added_IsInternational_Property_To_The_Team_Entity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsNationalTeam",
+                table: "Teams",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsNationalTeam",
+                table: "Teams");
+        }
+    }
+}
