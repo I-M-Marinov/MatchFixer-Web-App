75ac933f828b5e785e1a4c4de9840b8a87471102
I-M-Marinov <lcfrrr@gmail.com>
2026-04-06 23:23:27 +0300

    Created the UpcomingMatchRowViewModel

---
+++ MatchFixer.Infrastructure/Models/FootballAPI/UpcomingMatchRowViewModel.cs
@@ -0,0 +1,25 @@
+﻿namespace MatchFixer.Infrastructure.Models.FootballAPI
+{
+	public class UpcomingMatchRowViewModel
+	{
+		public bool Selected { get; set; }
+
+		public int ApiFixtureId { get; set; }
+
+		public Guid? HomeTeamId { get; set; }
+		public Guid? AwayTeamId { get; set; }
+
+		public string HomeName { get; set; } = null!;
+		public string AwayName { get; set; } = null!;
+		public string HomeLogo { get; set; } = null!;
+		public string AwayLogo { get; set; } = null!;
+		public string KickoffInput { get; set; } = null!;
+
+		public decimal HomeOdds { get; set; }
+		public decimal DrawOdds { get; set; }
+		public decimal AwayOdds { get; set; }
+
+		public bool IsAlreadyImported { get; set; }
+		public bool? IsManualDuplicate { get; set; }
+	}
+}
