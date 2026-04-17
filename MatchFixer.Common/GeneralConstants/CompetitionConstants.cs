a7838fcba1594d1849b7a1c9cb13fb398072b865
I-M-Marinov <lcfrrr@gmail.com>
2026-04-17 22:48:50 +0300

    Created the CompetitionConstants

---
+++ MatchFixer.Common/GeneralConstants/CompetitionConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace MatchFixer.Common.GeneralConstants
+{
+	public static class CompetitionConstants
+	{
+
+		public const string FailedToUpdateCompetition = "Failed to update competition.";
+		public const string CompetitonUpdatedSuccessfully = "Competition updated successfully.";
+
+	}
+}
