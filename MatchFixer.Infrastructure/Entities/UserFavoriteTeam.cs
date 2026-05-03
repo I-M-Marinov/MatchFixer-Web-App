a55da217174f7e9e7bc9258d28ab475af9b60cc8
I-M-Marinov <lcfrrr@gmail.com>
2026-05-03 22:12:51 +0300

    Created the UserFavoriteTeam Entity

---
+++ MatchFixer.Infrastructure/Entities/UserFavoriteTeam.cs
@@ -0,0 +1,11 @@
+﻿namespace MatchFixer.Infrastructure.Entities
+{
+	public class UserFavoriteTeam
+	{
+		public Guid UserId { get; set; }
+		public Guid TeamId { get; set; }
+
+		public ApplicationUser User { get; set; }
+		public Team Team { get; set; }
+	}
+}
