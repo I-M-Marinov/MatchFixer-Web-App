c935bc5fa3622aa5b3134908a69112f9d6611baf
I-M-Marinov <lcfrrr@gmail.com>
2026-05-03 22:52:34 +0300

    Created the TeamDto

---
+++ MatchFixer.Core/ViewModels/Profile/TeamDto.cs
@@ -0,0 +1,13 @@
+﻿namespace MatchFixer.Core.ViewModels.Profile
+{
+	public class TeamDto
+	{
+		public Guid Id { get; set; }
+
+		public string Name { get; set; }
+
+		public string LogoUrl { get; set; }
+
+		public bool IsFavorite { get; set; } 
+	}
+}
