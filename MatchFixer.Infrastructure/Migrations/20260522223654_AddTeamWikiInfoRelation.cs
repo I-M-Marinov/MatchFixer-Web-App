9f3abb31cdf30495e2218523c6c42c005d342e1f
I-M-Marinov <lcfrrr@gmail.com>
2026-05-23 14:09:54 +0300

    Added amigration and updated the DB with the new WikiInfo property in the Team Entity

---
+++ MatchFixer.Infrastructure/Migrations/20260522223654_AddTeamWikiInfoRelation.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MatchFixer.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTeamWikiInfoRelation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_TeamWikiInfos_TeamId",
+                table: "TeamWikiInfos");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TeamWikiInfos_TeamId",
+                table: "TeamWikiInfos",
+                column: "TeamId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_TeamWikiInfos_TeamId",
+                table: "TeamWikiInfos");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TeamWikiInfos_TeamId",
+                table: "TeamWikiInfos",
+                column: "TeamId");
+        }
+    }
+}
