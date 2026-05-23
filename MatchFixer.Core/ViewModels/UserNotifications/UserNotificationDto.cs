76cc87c9c0f62b008f12c2bb19a0279f6d90a773
I-M-Marinov <lcfrrr@gmail.com>
2026-05-23 14:10:30 +0300

    Created the UserNotificationDto

---
+++ MatchFixer.Core/ViewModels/UserNotifications/UserNotificationDto.cs
@@ -0,0 +1,11 @@
+﻿namespace MatchFixer.Core.ViewModels.UserNotifications
+{
+	public class UserNotificationDto
+	{
+		public string Email { get; set; }
+		public Guid TeamId { get; set; }
+		public string TimeZone { get; set; }
+
+	}
+}
+
