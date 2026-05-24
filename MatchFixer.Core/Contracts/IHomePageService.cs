711e746e722bf96c34adce3603c2b6cdf1257889
I-M-Marinov <lcfrrr@gmail.com>
2026-05-24 23:30:48 +0300

    Created the IHomePageService interface

---
+++ MatchFixer.Core/Contracts/IHomePageService.cs
@@ -0,0 +1,9 @@
+﻿using MatchFixer.Core.ViewModels.Index;
+
+namespace MatchFixer.Core.Contracts
+{
+	public interface IHomePageService
+	{
+		Task<List<RecentBigWinViewModel>> GetRecentBigWinsAsync();
+	}
+}
