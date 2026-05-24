eb58bb46c59983647df9fad1aba67b3d7eb06088
I-M-Marinov <lcfrrr@gmail.com>
2026-05-24 23:30:23 +0300

    Created the RecentBigWinBetViewModel

---
+++ MatchFixer.Core/ViewModels/Index/RecentBigWinBetViewModel.cs
@@ -0,0 +1,23 @@
+﻿using MatchFixer.Common.Enums;
+
+namespace MatchFixer.Core.ViewModels.Index
+{
+	public class RecentBigWinBetViewModel
+	{
+		public string HomeTeam { get; set; } = null!;
+
+		public string AwayTeam { get; set; } = null!;
+		public string? HomeTeamImageUrl { get; set; }
+		public string? AwayTeamImageUrl { get; set; }
+
+		public MatchPick Pick { get; set; }
+
+		public decimal Odds { get; set; }
+
+		public BetStatus Status { get; set; }
+
+		public int? HomeScore { get; set; }
+
+		public int? AwayScore { get; set; }
+	}
+}
