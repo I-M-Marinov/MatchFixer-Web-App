5a25942bdce5c77320c0e7b8d030dba8c02d96e6
I-M-Marinov <lcfrrr@gmail.com>
2026-05-24 23:30:13 +0300

    Created the RecentBigWinViewModel

---
+++ MatchFixer.Core/ViewModels/Index/RecentBigWinViewModel.cs
@@ -0,0 +1,23 @@
+﻿namespace MatchFixer.Core.ViewModels.Index
+{
+	public class RecentBigWinViewModel
+	{
+		public Guid BetSlipId { get; set; }
+
+		public string Username { get; set; } = null!;
+
+		public decimal WinAmount { get; set; }
+
+		public decimal StakeAmount { get; set; }
+
+		public int LegsCount { get; set; }
+
+		public decimal TotalOdds { get; set; }
+
+		public List<decimal> Odds { get; set; } = new();
+
+		public DateTime BetTime { get; set; }
+
+		public List<RecentBigWinBetViewModel> Bets { get; set; } = new();
+	}
+}
