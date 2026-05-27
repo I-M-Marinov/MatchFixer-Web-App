b64b97088a89ef78bbb697f4319c71c03328dc05
I-M-Marinov <lcfrrr@gmail.com>
2026-05-27 23:38:26 +0300

    Created the WorldCupPageViewModel

---
+++ MatchFixer.Core/ViewModels/WordCup/WorldCupPageViewModel.cs
@@ -0,0 +1,17 @@
+﻿using MatchFixer.Infrastructure.Models.TheSportsDBAPI;
+
+namespace MatchFixer.Core.ViewModels.WordCup
+{
+	public class WorldCupPageViewModel
+	{
+		public List<WorldCupStageViewModel> Stages { get; set; } = new();
+
+		public KnockoutBracketViewModel Bracket { get; set; } = null!;
+
+		public List<WorldCupGroupStandingViewModel> GroupStandings
+		{ get; set; } = new();
+
+
+	}
+}
+
