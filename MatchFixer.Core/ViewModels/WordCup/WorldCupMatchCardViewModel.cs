c167194e05564a7ca3bf195e9145a5a84a756d2e
I-M-Marinov <lcfrrr@gmail.com>
2026-05-27 23:37:47 +0300

    Created the WorldCupMatchCardViewModel

---
+++ MatchFixer.Core/ViewModels/WordCup/WorldCupMatchCardViewModel.cs
@@ -0,0 +1,31 @@
+﻿using MatchFixer.Common.Enums;
+
+namespace MatchFixer.Core.ViewModels.WordCup
+{
+	public class WorldCupMatchCardViewModel
+	{
+		public int MatchId { get; set; }
+
+		public string HomeTeam { get; set; } = null!;
+
+		public string AwayTeam { get; set; } = null!;
+
+		public string HomeLogo { get; set; } = null!;
+
+		public string AwayLogo { get; set; } = null!;
+
+		public DateTime? MatchDate { get; set; }
+
+		public int? HomeScore { get; set; }
+
+		public int? AwayScore { get; set; }
+
+		public bool IsFinished { get; set; }
+
+		public bool IsLive { get; set; }
+
+		public string? Winner { get; set; }
+
+		public WorldCupStage Stage { get; set; }
+	}
+}
