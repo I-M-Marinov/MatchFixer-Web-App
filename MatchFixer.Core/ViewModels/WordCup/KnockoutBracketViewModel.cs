2a5b74283949509b4551c33785de6a3a7de6dcc4
I-M-Marinov <lcfrrr@gmail.com>
2026-05-27 23:37:37 +0300

    Created the KnockoutBracketViewModel

---
+++ MatchFixer.Core/ViewModels/WordCup/KnockoutBracketViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace MatchFixer.Core.ViewModels.WordCup
+{
+	public class KnockoutBracketViewModel
+	{
+		public List<WorldCupMatchCardViewModel> RoundOf16 { get; set; } = new();
+
+		public List<WorldCupMatchCardViewModel> QuarterFinals { get; set; } = new();
+
+		public List<WorldCupMatchCardViewModel> SemiFinals { get; set; } = new();
+
+		public List<WorldCupMatchCardViewModel> ThirdPlace { get; set; } = new();
+
+		public List<WorldCupMatchCardViewModel> Final { get; set; } = new();
+	}
+}
