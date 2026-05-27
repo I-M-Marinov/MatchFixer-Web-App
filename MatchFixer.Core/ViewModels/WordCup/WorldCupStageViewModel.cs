e14eef2bdc4fa74ad4f3a6c49e670a19eb08cc26
I-M-Marinov <lcfrrr@gmail.com>
2026-05-27 23:38:33 +0300

    Created the WorldCupStageViewModel

---
+++ MatchFixer.Core/ViewModels/WordCup/WorldCupStageViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MatchFixer.Core.ViewModels.WordCup
+{
+	public class WorldCupStageViewModel
+	{
+		public string StageName { get; set; } = null!;
+
+		public List<WorldCupMatchCardViewModel> Matches { get; set; } = new();
+	}
+}
