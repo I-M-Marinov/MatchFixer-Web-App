104d4aae3cc23cc639111e818ec04052bd372d49
I-M-Marinov <lcfrrr@gmail.com>
2026-05-28 00:19:44 +0300

    Created the IWorldCupService

---
+++ MatchFixer.Core/Contracts/IWorldCupService.cs
@@ -0,0 +1,12 @@
+﻿using MatchFixer.Core.ViewModels.WordCup;
+using MatchFixer.Infrastructure.Models.TheSportsDBAPI;
+
+namespace MatchFixer.Core.Contracts
+{
+	public interface IWorldCupService
+	{
+		Task<WorldCupPageViewModel> GetWorldCupPageAsync();
+		Task<List<WorldCupGroupStandingViewModel>>
+			GetGroupStandingsAsync();
+	}
+}
