93997955b9d140c1399f34fff8a4ae36c9bcd998
I-M-Marinov <lcfrrr@gmail.com>
2026-06-23 19:35:01 +0300

    Updated the DB with the new property added to the BetSlip entity

---
+++ MatchFixer.Infrastructure/Migrations/20260622192552_AddSettledAtToBetSlip.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MatchFixer.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSettledAtToBetSlip : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "SettledAt",
+                table: "BetSlips",
+                type: "datetime2",
+                nullable: true,
+                comment: "UTC timestamp when the bet slip was settled (Won, Lost, or Voided)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SettledAt",
+                table: "BetSlips");
+        }
+    }
+}
