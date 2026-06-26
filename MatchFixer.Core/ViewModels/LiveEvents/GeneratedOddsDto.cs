47931ccc80d033823ddc1899bee3ab2e70eaa3b0
Ivan Marinov <134225981+I-M-Marinov@users.noreply.github.com>
2026-06-26 15:03:32 +0300

    Created the GeneratedOddsDto

---
+++ MatchFixer.Core/ViewModels/LiveEvents/GeneratedOddsDto.cs
@@ -0,0 +1,9 @@
+namespace MatchFixer.Core.ViewModels.LiveEvents
+{
+	public class GeneratedOddsDto
+	{
+		public decimal HomeOdds { get; init; }
+		public decimal DrawOdds { get; init; }
+		public decimal AwayOdds { get; init; }
+	}
+}
