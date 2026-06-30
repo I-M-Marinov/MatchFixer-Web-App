5f3fd0db14aab65585b27622a4e76e09f32faf6c
Ivan Marinov <134225981+I-M-Marinov@users.noreply.github.com>
2026-06-30 12:37:33 +0300

    Added a migration to the DB

---
+++ MatchFixer.Infrastructure/Migrations/20260630083010_AddHomeWonOnPenaltiesToLiveMatchResult.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MatchFixer.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddHomeWonOnPenaltiesToLiveMatchResult : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "HomeWonOnPenalties",
+                table: "LiveMatchResults",
+                type: "bit",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HomeWonOnPenalties",
+                table: "LiveMatchResults");
+        }
+    }
+}
