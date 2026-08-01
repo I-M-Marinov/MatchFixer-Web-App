e7f45870bee11b555f1468f3409889df8ab9b356
Ivan Marinov <134225981+I-M-Marinov@users.noreply.github.com>
2026-08-01 20:48:48 +0300

    Created a migration and updated the DB with the new PrePostponeStatus property that was added to the MatchEvent Entity

---
+++ MatchFixer.Infrastructure/Migrations/20260801173603_AddPrePostponeStatusToMatchEvent.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MatchFixer.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPrePostponeStatusToMatchEvent : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PrePostponeStatus",
+                table: "MatchEvents",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PrePostponeStatus",
+                table: "MatchEvents");
+        }
+    }
+}
