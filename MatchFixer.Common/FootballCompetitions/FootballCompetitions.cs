a4bf803180756f353dc5a54f9adbf70de8a2622f
I-M-Marinov <lcfrrr@gmail.com>
2026-08-13 23:53:47 +0300

    Added the static ByApiLeagueId

--- MatchFixer.Common/FootballCompetitions/FootballCompetitions.cs
+++ MatchFixer.Common/FootballCompetitions/FootballCompetitions.cs
@@ -5,5 +5,14 @@
 		public const string ChampionsLeague = "UEFA Champions League";
 		public const string EuropaLeague = "UEFA Europa League";
 		public const string ConferenceLeague = "UEFA Europa Conference League";
+
+		// Maps an API-Football league id to the internal competition tag.
+		// Domestic leagues are intentionally absent (they stay untagged).
+		public static readonly Dictionary<int, string> ByApiLeagueId = new()
+		{
+			{ 2,   ChampionsLeague },
+			{ 3,   EuropaLeague },
+			{ 848, ConferenceLeague }
+		};
 	}
 }
