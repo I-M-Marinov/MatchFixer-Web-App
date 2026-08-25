f4cf9a542facccebb4dd297c2c0be0afc8d0ee2b
I-M-Marinov <lcfrrr@gmail.com>
2026-08-25 23:14:33 +0300

    Created the TempDataKeys static class

---
+++ MatchFixer.Common/GeneralConstants/TempDataKeys.cs
@@ -0,0 +1,9 @@
+﻿namespace MatchFixer.Common.GeneralConstants
+{
+	public static class TempDataKeys
+	{
+		public const string SuccessMessage = "SuccessMessage";
+		public const string ErrorMessage = "ErrorMessage";
+		public const string SkippedMatches = "SkippedMatches";
+	}
+}
